06192028f1128acf646ee4195a2a02bb0f54e1e5
Alexander Diaz <35619574+aliensanderdiaz@users.noreply.github.com>
2024-07-25 18:33:32 -0500

    Add files via upload

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,5 +1,5 @@
 let APUESTAS_ABIERTAS = [
-	[[11800,"Corinthians SP","Gremio RS","local",2.15],[11645,"Atletico Lanus","CA Belgrano","visitante",2.9],[11645,"Platense","Velez Sarsfield","visitante",2.7],[11700,"Cuiaba MT","Palestino","visitante",3.35],"563,956"],
-	[[11600,"Gualaceo SC","Guayaquil City FC","over",2.55],[11645,"Atletico Lanus","CA Belgrano","over",2.55],[11645,"Platense","Velez Sarsfield","over",3.7],[11700,"Cuiaba MT","Palestino","over",2.5],[11800,"Corinthians SP","Gremio RS","over",2.5],"1,503,703"],
-	[[11645,"Atletico Lanus","CA Belgrano","mitad",1.55],[11645,"Platense","Velez Sarsfield","mitad",1.8],[11700,"Cuiaba MT","Palestino","mitad",1.55],[11800,"Corinthians SP","Gremio RS","mitad",1.55],[11600,"Gualaceo SC","Guayaquil City FC","mitad",1.533],"102,779"],
+	[[11900,"Atletico Tucuman","Instituto AC Cordoba","local",2.4],[11900,"CA Tigre","Central Cordoba","local",2.1],[11930,"Goias GO","CRB Maceio AL","visitante",3.2],[11930,"Always Ready","LDU Quito","visitante",2.15],"346,752"],
+	[[11900,"Atletico Tucuman","Instituto AC Cordoba","over",2.9],[11900,"CA Tigre","Central Cordoba","over",2.55],[11930,"Athletico PR","Cerro Porteño","over",2.1],[11930,"Ceara CE","Botafogo SP","over",2.25],[11930,"Goias GO","CRB Maceio AL","over",2.8],"978,359"],
+	[[11900,"Atletico Tucuman","Instituto AC Cordoba","mitad",1.65],[11900,"CA Tigre","Central Cordoba","mitad",1.571],[11930,"Athletico PR","Cerro Porteño","mitad",1.45],[11930,"Ceara CE","Botafogo SP","mitad",1.48],[11930,"Goias GO","CRB Maceio AL","mitad",1.615],"89,884"],
 ]
