5a8734a589c0e92062d677f2522bf348638a949d
Alexander Diaz <35619574+aliensanderdiaz@users.noreply.github.com>
2024-07-25 20:38:55 -0500

    Add files via upload

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,5 +1,2 @@
 let APUESTAS_ABIERTAS = [
-	[[11900,"Atletico Tucuman","Instituto AC Cordoba","local",2.4],[11900,"CA Tigre","Central Cordoba","local",2.1],[11930,"Goias GO","CRB Maceio AL","visitante",3.2],[11930,"Always Ready","LDU Quito","visitante",2.15],"346,752"],
-	[[11900,"Atletico Tucuman","Instituto AC Cordoba","over",2.9],[11900,"CA Tigre","Central Cordoba","over",2.55],[11930,"Athletico PR","Cerro Porteño","over",2.1],[11930,"Ceara CE","Botafogo SP","over",2.25],[11930,"Goias GO","CRB Maceio AL","over",2.8],"978,359"],
-	[[11900,"Atletico Tucuman","Instituto AC Cordoba","mitad",1.65],[11900,"CA Tigre","Central Cordoba","mitad",1.571],[11930,"Athletico PR","Cerro Porteño","mitad",1.45],[11930,"Ceara CE","Botafogo SP","mitad",1.48],[11930,"Goias GO","CRB Maceio AL","mitad",1.615],"89,884"],
 ]
