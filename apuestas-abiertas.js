f2cebcf24c16de379d7df808e23a8bfb6d3b1fa7
Alexander Diaz <35619574+aliensanderdiaz@users.noreply.github.com>
2024-07-27 08:35:41 -0500

    Add files via upload

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,6 +1,5 @@
 let APUESTAS_ABIERTAS = [
-	[[10745,"Stal Mielec","GKS Katowice","local",2],[10830,"Karlsruher SC","Strasbourg","local",1.95],[10730,"Dukla Praha","Bohemians 1905","visitante",1.85],[10800,"Miramar Misiones","Cerro Largo FC","visitante",2.3],[10830,"Hannover 96","Wolfsburg","visitante",1.769],[10830,"NEC Nijmegen","Olympiacos","visitante",1.85],[10900,"Clyde","Montrose","visitante",2.1],[10900,"Ipswich Town","Fortuna Dusseldorf","visitante",2.2],"2,509,356"],
+	[[10830,"Karlsruher SC","Strasbourg","local",1.95],[10830,"Hannover 96","Wolfsburg","visitante",1.9],[10830,"NEC Nijmegen","Olympiacos","visitante",1.9],[10900,"Clyde","Montrose","visitante",2.15],[10900,"Ipswich Town","Fortuna Dusseldorf","visitante",2.15],"325,401"],
 	[[10800,"Miramar Misiones","Cerro Largo FC","over",2.15],"21.500"],
-	[[10745,"Stal Mielec","GKS Katowice","mitad",1.42],[10800,"Miramar Misiones","Cerro Largo FC","mitad",1.45],"20,590"],
-	[[10530,"Daegu FC","Daejeon Citizen","over",2],[10530,"Incheon United","FC Seoul","over",2],[10800,"Miramar Misiones","Cerro Largo FC","over",2],[10700,"FC Nizhny Novgorod","CSKA Moscow","over",2.15],"172,000"],
+	[[10800,"Miramar Misiones","Cerro Largo FC","mitad",1.45],"20,590"],
 ]
