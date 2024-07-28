d6d06e0b1f88537e88aeffbbf6200380b84c3a48
Alexander Diaz <35619574+aliensanderdiaz@users.noreply.github.com>
2024-07-28 09:58:30 -0500

    Add files via upload

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,5 +1,6 @@
 let APUESTAS_ABIERTAS = [
-	[[10800,"CA Atenas De San Carlos","Institucion Atletica Sud America","over",2.75],[10800,"Uruguay Montevideo FC","Albion FC","over",2.7],[10800,"Kaizer Chiefs","Young Africans","over",2.45],[10900,"Bragantino SP","Fluminense RJ","over",2.15],"391,112"],
-	[[10800,"CA Atenas De San Carlos","Institucion Atletica Sud America","mitad",1.571],[10800,"Uruguay Montevideo FC","Albion FC","mitad",1.571],[10800,"Kaizer Chiefs","Young Africans","mitad",1.5],[10900,"Bragantino SP","Fluminense RJ","mitad",1.45],"53,709"],
-	[[10730,"Slovan Liberec","Sigma Olomouc","local",1.75],[10745,"Zaglebie Lubin","Pogon Szczecin","local",2.05],[10900,"Udinese","Konyaspor","local",2],[10900,"Bragantino SP","Fluminense RJ","visitante",2.375],[10930,"FC Zurich","FC Winterthur","visitante",2.25],"383,414"],
+	[[11000,"FK Mlada Boleslav","FC Slovacko","local",1.909],[11030,"GKS Piast Gliwice","Slask Wroclaw","local",1.95],[11030,"CA Cerro","Rampla Juniors","local",2.1],[11000,"Banik Ostrava","FK Baumit Jablonec","visitante",2.2],"171,990"],
+	[[11030,"GKS Piast Gliwice","Slask Wroclaw","over",2.55],[11030,"CA Cerro","Rampla Juniors","over",2.2],[11030,"AC Arezzo","Juve Stabia","over",2.45],[11030,"GFK Tikvesh Kavadarci","FK Shkendija 79","over",2],"274,890"],
+	[[11030,"GKS Piast Gliwice","Slask Wroclaw","mitad",1.55],[11030,"CA Cerro","Rampla Juniors","mitad",1.48],[11030,"AC Arezzo","Juve Stabia","mitad",1.48],"33,951"],
+	[[10900,"Bragantino SP","Fluminense RJ","over",2.2],"22.000"],
 ]
