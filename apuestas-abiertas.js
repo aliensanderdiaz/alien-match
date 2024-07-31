e0217a3301cfc0656b090f874982b86a6edbce16
Alexander Diaz <35619574+aliensanderdiaz@users.noreply.github.com>
2024-07-31 09:33:01 -0500

    Add files via upload

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,5 +1,5 @@
 let APUESTAS_ABIERTAS = [
-	[[10400,"Moreirense","Pacos Ferreira","local",1.769],[11000,"Brasil (F)","España (F)","local",2.55],[11200,"AZ Alkmaar","AC Le Havre","local",1.833],[11200,"Valencia","Levante","local",1.85],[11200,"Real Sociedad","Osasuna","local",1.909],[10400,"Girona","Toulouse","visitante",2.375],[11100,"Montpellier","Saint Etienne","visitante",2.2],[11200,"Getafe","Real Zaragoza","visitante",2.8],"4,273,746"],
-	[[10300,"Real Oviedo","Al Ettifaq","mitad",1.42],[11200,"Getafe","Real Zaragoza","mitad",1.42],[11200,"Real Sociedad","Osasuna","mitad",1.48],[11200,"Valencia","Levante","mitad",1.444],[11200,"Elche","CD Castellon","mitad",1.42],[11200,"Espanyol","SD Huesca","mitad",1.45],[11230,"Sanluqueno","Real Linense","mitad",1.48],"131,358"],
-	[[10300,"Real Oviedo","Al Ettifaq","over",2.05],[11200,"Getafe","Real Zaragoza","over",2.05],[11200,"Real Sociedad","Osasuna","over",2.15],[11200,"Valencia","Levante","over",2.1],[11200,"Elche","CD Castellon","over",2],[11200,"Espanyol","SD Huesca","over",2.15],[11230,"Sanluqueno","Real Linense","over",2.2],"1,794,968"],
+	[[11000,"Brasil (F)","España (F)","local",2.25],[11100,"Montpellier","Saint Etienne","visitante",2.15],"48,375"],
+	[[11200,"Getafe","Real Zaragoza","mitad",1.42],[11200,"Real Sociedad","Osasuna","mitad",1.48],[11200,"Valencia","Levante","mitad",1.444],[11200,"Elche","CD Castellon","mitad",1.42],[11200,"Espanyol","SD Huesca","mitad",1.45],[11230,"Sanluqueno","Real Linense","mitad",1.48],"131,358"],
+	[[11200,"Getafe","Real Zaragoza","over",2.05],[11200,"Real Sociedad","Osasuna","over",2.15],[11200,"Valencia","Levante","over",2.1],[11200,"Elche","CD Castellon","over",2],[11200,"Espanyol","SD Huesca","over",2.15],[11230,"Sanluqueno","Real Linense","over",2.2],"1,794,968"],
 ]
