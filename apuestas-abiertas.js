78d99122b04d319ddbf693c97535f7b133fe6269
Alexander Diaz <35619574+aliensanderdiaz@users.noreply.github.com>
2024-07-31 22:26:07 -0500

    Add files via upload

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,6 +1,5 @@
 let APUESTAS_ABIERTAS = [
-	[[12000,"FC Dallas","FC Juarez","local",1.909],[12000,"Nashville SC","Mazatlan FC","local",1.8],[12100,"Manchester United","Real Betis","local",1.75],"60,136"],
-	[[12000,"FBC Melgar","Universitario de Deportes","over",2.25],[12010,"Independiente Medellin","Boyaca Chico FC","over",2.2],"49,500"],
-	[[12010,"Independiente Medellin","Boyaca Chico FC","mitad",1.42],[12000,"FBC Melgar","Universitario de Deportes","mitad",1.48],"21,016"],
-	[[11910,"Mitre Santiago Del Estero","Temperley","over",3],[12010,"Independiente Medellin","Boyaca Chico FC","over",2.2],[11900,"Cuniburo FC","Gualaceo SC","over",2.45],[11930,"Atletico GO","Vasco Da Gama RJ","over",2.5],[11930,"Corinthians SP","Gremio RS","over",2.9],[11930,"Carlos Mannucci","Asociacion Deportiva Tarma","over",2.2],[12000,"FBC Melgar","Universitario de Deportes","over",2.375],"6,125,398"],
+	[[11000,"NK Bravo","HSK Zrinjski Mostar","over",2],[11000,"Ordabasy Shymkent","Differdange FC 03","over",2.1],[11100,"Polissya Zhytomyr","NK Olimpija Ljubljana","over",2.05],"86,100"],
+	[[11000,"NK Bravo","HSK Zrinjski Mostar","mitad",1.42],[11000,"Ordabasy Shymkent","Differdange FC 03","mitad",1.444],[11100,"Polissya Zhytomyr","NK Olimpija Ljubljana","mitad",1.42],"29,126"],
+	[[11000,"NK Bravo","HSK Zrinjski Mostar","local",2.4],[11100,"Sabah","Maccabi Haifa","local",2.25],[11000,"FC Urartu","Banik Ostrava","visitante",1.833],[11000,"Ordabasy Shymkent","Differdange FC 03","visitante",3],[11100,"Polissya Zhytomyr","NK Olimpija Ljubljana","visitante",2.25],[11100,"Pafos FC","VMFD Zalgiris","visitante",2.75],[11100,"FC Levadia Tallinn","NK Osijek","visitante",1.769],[11100,"Tobol Kostanay","FC St. Gallen","visitante",1.85],"6,014,893"],
 ]
