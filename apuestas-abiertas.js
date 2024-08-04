38b16dd98dcac7449da90064f5d7cb757d4d8a83
Alexander Diaz <35619574+aliensanderdiaz@users.noreply.github.com>
2024-08-04 10:44:16 -0500

    Add files via upload

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,5 +1,2 @@
 let APUESTAS_ABIERTAS = [
-	[[11900,"Always Ready","The Strongest","over",2],[12200,"Atletico Morelia","CD Tapatio","over",2.1],[11730,"Cerro Porteño","Sportivo Ameliano","over",2.05],[11900,"Guayaquil City FC","Vargas Torres","over",2.1],[11930,"Llaneros FC","Union Magdalena","over",2.1],"379,701"],
-	[[12200,"Atletico Morelia","CD Tapatio","local",1.95],[12000,"Toluca","Chicago Fire","visitante",2],"103,350"],
-	[[12200,"Atletico Morelia","CD Tapatio","mitad",1.45],[11730,"Cerro Porteño","Sportivo Ameliano","mitad",1.42],[11900,"Guayaquil City FC","Vargas Torres","mitad",1.48],[11900,"Always Ready","The Strongest","mitad",1.444],[11930,"Llaneros FC","Union Magdalena","mitad",1.42],"215,203"],
 ]
