b3c08cbce8d4ade0051624df9ba95e9a88f7addc
Alexander Diaz <35619574+aliensanderdiaz@users.noreply.github.com>
2024-08-04 18:56:37 -0500

    Add files via upload

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,2 +1,5 @@
 let APUESTAS_ABIERTAS = [
+	[[11900,"Pachuca","Toronto FC","local",2.1],[11950,"Deportivo Cali","Independiente Medellin","local",2.55],[12000,"Saint Louis FC","FC Juarez","local",1.769],[12130,"Chivas Guadalajara","L.A. Galaxy","local",1.95],[11900,"Philadelphia Union","Cruz Azul","visitante",1.75],"323,308"],
+	[[11745,"CD Junior","Alianza FC","over",2.2],[11800,"Atletico Tucuman","Independiente Rivadavia","over",3],[11800,"Atletico Lanus","CA Tigre","over",2.3],[11800,"Club Sport Emelec","Deportivo Cuenca","over",2.1],[11950,"Deportivo Cali","Independiente Medellin","over",2.4],[11830,"Jorge Wilstermann","Real Tomayapo","over",2.2],"10,427,166"],
+	[[11950,"Deportivo Cali","Independiente Medellin","mitad",1.533],"278,324"],
 ]
