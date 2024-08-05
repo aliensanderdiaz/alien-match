5727edf8850424a13c34ecb23f543143eae5da27
Alexander Diaz <35619574+aliensanderdiaz@users.noreply.github.com>
2024-08-05 15:01:29 -0500

    Add files via upload

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,5 +1,7 @@
 let APUESTAS_ABIERTAS = [
-	[[11400,"St Johnstone","Aberdeen","local",2.45],[11630,"Chapecoense SC","Mirassol SP","local",2.45],[11500,"Gimnasia LP","Rosario Central","visitante",2.55],"367,353"],
+	[[11500,"Gimnasia LP","Rosario Central","visitante",2.62],"26.250"],
+	[[11500,"Gimnasia LP","Rosario Central","over",2.54],"25.500"],
+	[[11500,"Gimnasia LP","Rosario Central","mitad",1.57],"15.714"],
 	[[11500,"Gimnasia LP","Rosario Central","over",2.55],[11630,"Chapecoense SC","Mirassol SP","over",3],[11700,"Club Atletico Atlanta","Gimnasia Y Esgrima Mendoza","over",2.9],[11700,"Ferro Carril Oeste","Agropecuario Argentino","over",2.625],[11710,"Estudiantes Rio Cuarto","CA Aldosivi","over",3.1],"1,805,304"],
 	[[11500,"Gimnasia LP","Rosario Central","mitad",1.571],[11630,"Chapecoense SC","Mirassol SP","mitad",1.666],[11700,"Club Atletico Atlanta","Gimnasia Y Esgrima Mendoza","mitad",1.65],[11700,"Ferro Carril Oeste","Agropecuario Argentino","mitad",1.6],[11710,"Estudiantes Rio Cuarto","CA Aldosivi","mitad",1.727],"119,429"],
 ]
