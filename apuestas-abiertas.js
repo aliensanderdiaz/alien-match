e1246876b514659eac1fd62d0548b4e91e3bc493
Alexander Diaz <35619574+aliensanderdiaz@users.noreply.github.com>
2024-08-06 13:20:30 -0500

    Add files via upload

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,7 +1,5 @@
 let APUESTAS_ABIERTAS = [
-	[[11200,"Malmo FF","PAOK Thessaloniki","local",1.8],[11215,"Midtjylland","Ferencvarosi TC","local",1.769],[11400,"Brasil (F)","España (F)","local",3.25],[11200,"Podbeskidzie Bielsko-Biala","Stomil Olsztyn","visitante",2.75],[11300,"Dynamo Kiev","Glasgow Rangers","visitante",2.05],[11330,"Lille","Fenerbahce","visitante",2.25],[11400,"Recreativo Huelva","CF Cordoba","visitante",2.2],"2,888,232"],
+	[[11400,"Brasil (F)","España (F)","local",3.2],[11300,"Dynamo Kiev","Glasgow Rangers","visitante",2.05],[11330,"Lille","Fenerbahce","visitante",2.25],[11400,"Recreativo Huelva","CF Cordoba","visitante",2.2],"324,720"],
 	[[11200,"AD Ceuta","Real Betis B","over",2.05],[11330,"Lille","Fenerbahce","over",2],[11400,"Brasil (F)","España (F)","over",2.2],[11400,"Recreativo Huelva","CF Cordoba","over",2.2],"198,440"],
 	[[11330,"Lille","Fenerbahce","mitad",1.42],[11400,"Brasil (F)","España (F)","mitad",1.45],"20,590"],
-	[[11100,"EE.UU (F)","Alemania (F)","over",2.15],"21.500"],
-	[[11100,"EE.UU (F)","Alemania (F)","over",2.2],[11100,"Qarabag FK","Ludogorets","over",2],[11215,"Midtjylland","Ferencvarosi TC","over",2.15],[11330,"Lille","Fenerbahce","over",2],[11400,"Brasil (F)","España (F)","over",2.1],[11400,"Recreativo Huelva","CF Cordoba","over",2.15],"854,238"],
 ]
