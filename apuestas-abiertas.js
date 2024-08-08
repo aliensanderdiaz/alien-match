f34092e29c4f2732e3bb07666b2f49a707b93b6c
Alexander Diaz <35619574+aliensanderdiaz@users.noreply.github.com>
2024-08-08 17:26:04 -0500

    Add files via upload

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,6 +1,3 @@
 let APUESTAS_ABIERTAS = [
-	[[11530,"Guayaquil City FC","Cuniburo FC","over",2.5],[11600,"Deportivo La Guaira","FC Angostura","over",2],[11845,"Carabobo FC","Monagas SC","over",2.3],[11900,"San Antonio","Independiente Juniors","over",2.75],[11630,"Sportivo Luqueno","Club Tacuary","over",2.6],[11800,"Goias GO","Sao Paulo SP","over",2.55],[11900,"Club General Caballero JLM","Cerro Porteño","over",2.2],"4,612,823"],
-	[[11630,"Sportivo Luqueno","Club Tacuary","visitante",3.2],[11800,"Goias GO","Sao Paulo SP","visitante",2.1],[11900,"Club General Caballero JLM","Cerro Porteño","visitante",1.769],"118,892"],
-	[[11530,"Guayaquil City FC","Cuniburo FC","mitad",1.5],[11600,"Deportivo La Guaira","FC Angostura","mitad",1.42],[11630,"Sportivo Luqueno","Club Tacuary","mitad",1.571],[11845,"Carabobo FC","Monagas SC","mitad",1.5],[11900,"Club General Caballero JLM","Cerro Porteño","mitad",1.45],[11900,"San Antonio","Independiente Juniors","mitad",1.571],[11800,"Goias GO","Sao Paulo SP","mitad",1.55],"177,321"],
-	[[11300,"Argentinos Juniors","Sarmiento","over",2.7],[11300,"Botev Plovdiv","HSK Zrinjski Mostar","over",2],[11300,"FC Hunedoara","FC Astana","over",2.15],[11330,"Almeria","Al Nassr Riyadh","over",2.2],"523,611"],
+	[[11845,"Carabobo FC","Monagas SC","over",2.3],[11900,"San Antonio","Independiente Juniors","over",2.75],[11630,"Sportivo Luqueno","Club Tacuary","over",2.6],[11800,"Goias GO","Sao Paulo SP","over",2.55],[11900,"Club General Caballero JLM","Cerro Porteño","over",2.2],"4,612,823"],
 ]
