fe320ea9ae19ed9b375b9ace67c5753fb3ff37f9
Alexander Diaz <35619574+aliensanderdiaz@users.noreply.github.com>
2024-08-08 21:19:00 -0500

    Add files via upload

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,5 +1,2 @@
 let APUESTAS_ABIERTAS = [
-	[[12000,"Tigres UANL","Pachuca","local",1.8],[11900,"Club General Caballero JLM","Cerro Porteño","visitante",1.8],[12000,"Correcaminos","Club Atletico La Paz","visitante",2.05],[12130,"Seattle Sounders","L.A. Galaxy","visitante",2.2],[12200,"San Jose Earthquakes","Necaxa","visitante",1.95],"284,942"],
-	[[11900,"San Antonio","Independiente Juniors","over",2.8],[11900,"Club General Caballero JLM","Cerro Porteño","over",2.25],"63,000"],
-	[[11900,"Club General Caballero JLM","Cerro Porteño","mitad",1.48],[11900,"San Antonio","Independiente Juniors","mitad",1.571],"23,257"],
 ]
