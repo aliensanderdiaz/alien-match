c600b48326235721bdd318a7ab46e600673fd534
Alexander Diaz <35619574+aliensanderdiaz@users.noreply.github.com>
2024-08-09 08:59:01 -0500

    Add files via upload

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,2 +1,5 @@
 let APUESTAS_ABIERTAS = [
+	[[11100,"Francia Olímpico","España Olímpico","mitad",1.533],[11130,"Jahn Regensburg","SSV Ulm 1846","mitad",1.42],[11030,"FC Nizhny Novgorod","FK Khimki","mitad",1.444],[11030,"Slavia Mozyr","FC Vitebsk","mitad",1.571],"49,422"],
+	[[11100,"Francia Olímpico","España Olímpico","local",2.25],[11100,"Radomiak Radom","Gornik Zabrze","local",1.95],[11130,"Jahn Regensburg","SSV Ulm 1846","local",2],[11130,"1. FC Kaiserslautern","Greuther Furth","visitante",1.95],"171,113"],
+	[[11100,"Francia Olímpico","España Olímpico","over",2.4],[11130,"Jahn Regensburg","SSV Ulm 1846","over",2],[11030,"FC Nizhny Novgorod","FK Khimki","over",2.05],[11030,"Slavia Mozyr","FC Vitebsk","over",2.5],"246,000"],
 ]
