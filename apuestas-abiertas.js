6da618d0bd7af39089c9d004e2c53f5cab258744
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-08-15 14:50:33 -0500

    ballkani! !

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,6 +1,7 @@
 let APUESTAS_ABIERTAS = [
-	[[11400,"FK Decic Tuzi","HJK Helsinki","local",2.2],[11430,"Real Betis","Girona","local",2.05],[11400,"Shamrock Rovers","NK Celje","visitante",1.95],[11400,"Larne","KF Ballkani","visitante",2.25],"197,876"],
-	[[11400,"Larne","KF Ballkani","mitad",1.53],"15,333"],
+	[[11500,"Talleres de Remedios de Escalada","Banfield","visitante",2],"20,000"],
+	[[11500,"Talleres de Remedios de Escalada","Banfield","mitad",1.6],[11515,"Comerciantes Unidos","UTC Cajamarca","mitad",1.48],[11530,"Club Nueve de Octubre","Manta FC","mitad",1.444],[11600,"FC Angostura","Carabobo FC","mitad",1.533],"52,447"],
+	[[11500,"Talleres de Remedios de Escalada","Banfield","over",2.65],[11515,"Comerciantes Unidos","UTC Cajamarca","over",2.15],[11530,"Club Nueve de Octubre","Manta FC","over",2.2],[11600,"FC Angostura","Carabobo FC","over",2.4],"300,828"],
+	[[11430,"Real Betis","Girona","over",1.9],"20,299"],
 	[[11400,"Larne","KF Ballkani","over",2.4],"24,000"],
-	[[11300,"KF Drita","FK Auda","over",2.2],[11300,"Alianza Atletico","Cusco FC","over",2.35],[11330,"Granada","Albacete Balompie","over",2.15],"111,155"],
 ]
