922e4eb383f957b09f33b0bff171a8e42687a7bf
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-08-16 15:08:11 -0500

    brescia

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,12 +1,6 @@
 let APUESTAS_ABIERTAS = [
-	[[11200,"Celta de Vigo","CD Alaves","local",2],[11200,"Mirandes","CF Cordoba","local",2.5],"50,000"],
-	[[11200,"Celta de Vigo","CD Alaves","over",2.25],[11200,"Mirandes","CF Cordoba","over",2.6],"58,500"],
-	[[11200,"Celta de Vigo","CD Alaves","mitad",1.48],[11200,"Mirandes","CF Cordoba","mitad",1.6],"23,680"],
-	[[11300,"EA Guingamp","Troyes","local",1.8],[11300,"FC Den Bosch","Jong AZ Alkmaar","local",1.85],[11330,"GKS Katowice","Motor Lublin","local",1.75],[11300,"Ajaccio","Rodez Aveyron","visitante",2.375],[11300,"SC Telstar","FC Eindhoven","visitante",2.05],[11300,"SBV Excelsior","De Graafschap","visitante",1.85],[11300,"VVV Venlo","Vitesse","visitante",1.85],[11330,"Brescia","Palermo","visitante",2.05],"1,990,660"],
-	[[11200,"Celta de Vigo","CD Alaves","local",2],[11200,"Mirandes","CF Cordoba","local",2.5],[11300,"SC Amiens","Red Star Paris","local",2.15],[11300,"Clermont Foot","Pau FC","local",1.85],[11300,"Grenoble","Laval","local",2.05],[11300,"Dunkerque","Annecy FC","local",2.15],"876,542"],
-	[[11300,"Grenoble","Laval","over",2.375],[11300,"Dunkerque","Annecy FC","over",2.2],[11300,"EA Guingamp","Troyes","over",2.2],[11330,"Brescia","Palermo","over",2.1],[11300,"Asociacion Deportiva Tarma","FBC Melgar","over",2.05],"494,860"],
-	[[11200,"Celta de Vigo","CD Alaves","over",2.25],[11200,"Mirandes","CF Cordoba","over",2.6],[11300,"Ajaccio","Rodez Aveyron","over",2.4],[11300,"SC Amiens","Red Star Paris","over",2.4],[11300,"Clermont Foot","Pau FC","over",2.15],"724,464"],
-	[[11200,"Celta de Vigo","CD Alaves","mitad",1.48],[11200,"Mirandes","CF Cordoba","mitad",1.6],[11300,"Ajaccio","Rodez Aveyron","mitad",1.533],[11300,"SC Amiens","Red Star Paris","mitad",1.533],[11300,"Clermont Foot","Pau FC","mitad",1.45],[11300,"Grenoble","Laval","mitad",1.533],[11300,"Dunkerque","Annecy FC","mitad",1.48],[11300,"EA Guingamp","Troyes","mitad",1.48],[11330,"Brescia","Palermo","mitad",1.45],[11300,"Asociacion Deportiva Tarma","FBC Melgar","mitad",1.45],"570,058"],
-	[[11100,"Unirea Slobozia","Petrolul Ploiesti","over",2.8],"28,000"],
-	[[11000,"Vorskla Poltava","Zmina Obolon Kyiv","over",2.45],"24,500"],
+	[[11630,"Sportivo Luqueno","Guarani Asuncion","local",2.4],[11700,"Platense","CA Barracas Central","local",2.05],[11700,"Ponte Preta SP","Goias GO","local",2.1],[11700,"Montevideo Wanderers","CA Cerro","local",2.15],"222,138"],
+	[[11515,"Universidad Cesar Vallejo","Sport Boys Association","over",2.25],[11630,"Sportivo Luqueno","Guarani Asuncion","over",2.5],[11700,"Platense","CA Barracas Central","over",2.9],[11700,"Ponte Preta SP","Goias GO","over",2.45],[11700,"Montevideo Wanderers","CA Cerro","over",2.35],"939,192"],
+	[[11700,"Platense","CA Barracas Central","mitad",1.615],[11700,"Ponte Preta SP","Goias GO","mitad",1.55],[11515,"Universidad Cesar Vallejo","Sport Boys Association","mitad",1.48],[11630,"Sportivo Luqueno","Guarani Asuncion","mitad",1.571],[11700,"Montevideo Wanderers","CA Cerro","mitad",1.533],"89,290"],
+	[[11330,"Brescia","Palermo","over",2.1],[11430,"Las Palmas","Sevilla","over",2.2],[11430,"CF Cadiz","Real Zaragoza","over",3.1],"300,762"],
 ]
