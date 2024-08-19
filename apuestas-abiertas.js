a2cc577b2db0ae7a3cc3022d0444fa1332c591b2
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-08-18 19:40:40 -0500

    envigado

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,5 +1,7 @@
 let APUESTAS_ABIERTAS = [
+	[[12000,"Boyaca Chico FC","Envigado FC","visitante",3],"30,000"],
+	[[12000,"Boyaca Chico FC","Envigado FC","over",2.54],"25,500"],
+	[[12000,"Boyaca Chico FC","Envigado FC","mitad",1.55],"15,500"],
+	[[11830,"Huracan","CA Belgrano","over",2.65],[11830,"Colon de Santa Fe","Gimnasia Y Esgrima Mendoza","over",2.875],[11830,"Royal Pari","Club Aurora","over",2],"152,375"],
 	[[11745,"Atletico Bucaramanga","América de Cali","over",2.8],[11800,"Club Sport Emelec","El Nacional","over",2.1],[11830,"Huracan","CA Belgrano","over",2.8],[11830,"Colon de Santa Fe","Gimnasia Y Esgrima Mendoza","over",2.8],[11830,"Royal Pari","Club Aurora","over",2],"921,984"],
-	[[11800,"Club Sport Emelec","El Nacional","local",1.8],[11830,"Huracan","CA Belgrano","local",1.85],[11745,"Atletico Bucaramanga","América de Cali","visitante",2.45],[11905,"Tijuana","Santos Laguna","visitante",2.8],"228,438"],
-	[[11745,"Atletico Bucaramanga","América de Cali","mitad",1.615],[11800,"Club Sport Emelec","El Nacional","mitad",1.45],[11830,"Huracan","CA Belgrano","mitad",1.6],[11830,"Colon de Santa Fe","Gimnasia Y Esgrima Mendoza","mitad",1.65],[11830,"Royal Pari","Club Aurora","mitad",1.42],"87,808"],
 ]
