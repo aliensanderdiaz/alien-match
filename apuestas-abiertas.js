29ce9a9ba77be8aa1f9b85a1c047a9e9a13e888f
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-08-20 14:03:34 -0500

    rayo

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,8 +1,11 @@
 let APUESTAS_ABIERTAS = [
+	[[11400,"Lille","Slavia Prague","local",1.85],[11400,"Bodo Glimt","Red Star Belgrade","visitante",2.25],"41,625"],
+	[[11400,"Lille","Slavia Prague","over",2.2],[11400,"Rayo Zuliano","Academia Puerto Cabello","over",2.2],"48,400"],
+	[[11400,"Lille","Slavia Prague","mitad",1.48],[11400,"Rayo Zuliano","Academia Puerto Cabello","mitad",1.48],"21,904"],
 	[[11400,"Lille","Slavia Prague","local",1.9],[11400,"Bodo Glimt","Red Star Belgrade","visitante",2.25],"42,750"],
 	[[11345,"Woking FC","Dagenham & Redbridge","over",2],[11345,"Yeovil Town","Ebbsfleet United","over",2.15],[11345,"Tamworth FC","Hartlepool","over",2],[11400,"Lille","Slavia Prague","over",2.2],[11400,"Rayo Zuliano","Academia Puerto Cabello","over",2.15],"406,780"],
-	[[11400,"Lille","Slavia Prague","mitad",1.48],[11345,"York City","Rochdale","mitad",1.42],[11345,"Woking FC","Dagenham & Redbridge","mitad",1.444],[11345,"Yeovil Town","Ebbsfleet United","mitad",1.45],[11345,"Tamworth FC","Hartlepool","mitad",1.42],[11400,"Rayo Zuliano","Academia Puerto Cabello","mitad",1.45],"90,631"],
+	[[11400,"Lille","Slavia Prague","mitad",1.48],[11345,"Woking FC","Dagenham & Redbridge","mitad",1.444],[11345,"Yeovil Town","Ebbsfleet United","mitad",1.45],[11345,"Tamworth FC","Hartlepool","mitad",1.42],[11400,"Rayo Zuliano","Academia Puerto Cabello","mitad",1.45],"90,631"],
 	[[11400,"NK Dinamo Zagreb","Qarabag FK","local",1.727],[11400,"Lille","Slavia Prague","local",1.769],[11400,"Bodo Glimt","Red Star Belgrade","visitante",2.4],"140,018"],
 	[[11300,"UTC Cajamarca","Asociacion Deportiva Tarma","over",2],[11345,"Southend United","Boston United","over",2.15],[11345,"York City","Rochdale","over",2],[11345,"Woking FC","Dagenham & Redbridge","over",2.05],[11345,"Yeovil Town","Ebbsfleet United","over",2.1],[11400,"Lille","Slavia Prague","over",2],"1,666,035"],
-	[[11345,"Southend United","Boston United","mitad",1.45],[11345,"York City","Rochdale","mitad",1.42],[11345,"Woking FC","Dagenham & Redbridge","mitad",1.444],[11345,"Yeovil Town","Ebbsfleet United","mitad",1.42],"90,283"],
+	[[11345,"Woking FC","Dagenham & Redbridge","mitad",1.444],[11345,"Yeovil Town","Ebbsfleet United","mitad",1.42],"90,283"],
 ]
