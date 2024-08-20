b4f8c74fcf97afa3c6272450255fbe0644bb50c4
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-08-19 21:37:14 -0500

    uefa

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,9 +1,5 @@
 let APUESTAS_ABIERTAS = [
-	[[11800,"EC Vitoria","Cruzeiro MG","over",2.8],[11815,"Jaguares De Córdoba","Tolima","over",2.55],"71,400"],
-	[[11800,"EC Vitoria","Cruzeiro MG","visitante",2.55],[11815,"Jaguares De Córdoba","Tolima","visitante",2.2],"56,100"],
-	[[11800,"EC Vitoria","Cruzeiro MG","mitad",1.615],[11815,"Jaguares De Córdoba","Tolima","mitad",1.571],"25,385"],
-	[[11900,"Defensa Y Justicia","Banfield","local",2.2],[11730,"Sportivo Ameliano","Libertad Asuncion","visitante",1.95],[11800,"EC Vitoria","Cruzeiro MG","visitante",2.6],[11815,"Jaguares De Córdoba","Tolima","visitante",2.2],[11900,"Tecnico Universitario","Delfin","visitante",2.625],"644,144"],
-	[[11730,"Sportivo Ameliano","Libertad Asuncion","over",2.15],[11800,"EC Vitoria","Cruzeiro MG","over",2.75],[11815,"Jaguares De Córdoba","Tolima","over",2.55],[11900,"Defensa Y Justicia","Banfield","over",2.4],[11900,"Tecnico Universitario","Delfin","over",2.15],[11905,"Quilmes AC","Agropecuario Argentino","over",2.85],[11910,"Club Atletico Atlanta","Deportivo Moron","over",3.1],"6,873,336"],
-	[[11800,"EC Vitoria","Cruzeiro MG","mitad",1.615],[11815,"Jaguares De Córdoba","Tolima","mitad",1.571],[11900,"Defensa Y Justicia","Banfield","mitad",1.533],[11900,"Tecnico Universitario","Delfin","mitad",1.45],[11730,"Sportivo Ameliano","Libertad Asuncion","mitad",1.48],[11905,"Quilmes AC","Agropecuario Argentino","mitad",1.65],[11910,"Club Atletico Atlanta","Deportivo Moron","mitad",1.7],"234,299"],
-	[[11645,"Union Santa Fe","Argentinos Juniors","over",3.2],[11700,"Montevideo City Torque","La Luz FC","over",2.3],"195,040"],
+	[[11100,"FC Noah","MFK Ruzomberok","local",1.909],[11400,"NK Dinamo Zagreb","Qarabag FK","local",1.727],[11400,"Lille","Slavia Prague","local",1.769],[11400,"Bodo Glimt","Red Star Belgrade","visitante",2.4],"140,018"],
+	[[11100,"FC Noah","MFK Ruzomberok","over",2.25],[11300,"UTC Cajamarca","Asociacion Deportiva Tarma","over",2],[11345,"Southend United","Boston United","over",2.15],[11345,"York City","Rochdale","over",2],[11345,"Woking FC","Dagenham & Redbridge","over",2.05],[11345,"Yeovil Town","Ebbsfleet United","over",2.1],[11400,"Lille","Slavia Prague","over",2],"1,666,035"],
+	[[11100,"FC Noah","MFK Ruzomberok","mitad",1.48],[11300,"UTC Cajamarca","Asociacion Deportiva Tarma","mitad",1.444],[11345,"Southend United","Boston United","mitad",1.45],[11345,"York City","Rochdale","mitad",1.42],[11345,"Woking FC","Dagenham & Redbridge","mitad",1.444],[11345,"Yeovil Town","Ebbsfleet United","mitad",1.42],"90,283"],
 ]
