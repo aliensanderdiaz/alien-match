bc730356792dec7f56423b0c0adb7fcd450209bf
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-08-23 15:42:28 -0500

    laura

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,7 +1,7 @@
 let APUESTAS_ABIERTAS = [
-	[[11430,"Sevilla","Villarreal","local",1.833],[11430,"SD Huesca","Deportivo La Coruna","local",2.5],"44,280"],
-	[[11330,"Modena FC","Bari","over",2.45],[11345,"Dagenham & Redbridge","Halifax Town","over",2.05],[11430,"SD Huesca","Deportivo La Coruna","over",3.1],"155,698"],
-	[[11430,"SD Huesca","Deportivo La Coruna","mitad",1.7],"38,208"],
-	[[11300,"SC Bastia","SC Amiens","over",2.55],[11330,"Modena FC","Bari","over",2.55],[11300,"Kocaelispor","Erzurum BB","over",2.05],[11345,"Dagenham & Redbridge","Halifax Town","over",2.05],[11430,"SD Huesca","Deportivo La Coruna","over",3.1],"847,129"],
-	[[11300,"Alanyaspor","Goztepe","over",2.1],[11300,"Troyes","Clermont Foot","over",2.1],[11300,"Laval","EA Guingamp","over",2.5],"474,075"],
+	[[11600,"Independiente Rivadavia","Platense","over",3.75],[11630,"Sportivo Trinidense","Sportivo Luqueno","over",2.35],"88,125"],
+	[[11600,"Independiente Rivadavia","Platense","mitad",1.8],[11630,"Sportivo Trinidense","Sportivo Luqueno","mitad",1.533],"27,600"],
+	[[11600,"Independiente Rivadavia","Platense","local",2.65],[11630,"Sportivo Trinidense","Sportivo Luqueno","visitante",2.35],"62,275"],
+	[[11530,"Vargas Torres","Independiente Juniors","over",3.4],"34,000"],
+	[[11530,"Vargas Torres","Independiente Juniors","mitad",1.8],"18,000"],
 ]
