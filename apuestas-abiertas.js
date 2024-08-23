f72e1a2ee02e98e06839de7637a4fe7c5b0960e2
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-08-23 17:26:30 -0500

    apuestas

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,7 +1,7 @@
 let APUESTAS_ABIERTAS = [
+	[[11800,"Union La Calera","Deportes Copiapo","local",1.8],[11900,"Macara","Deportivo Cuenca","local",1.95],[11900,"Nacional Asuncion","Sol de America","local",1.95],[11930,"Deportivo Pasto","Atletico Bucaramanga","local",2.15],[11930,"Ponte Preta SP","Operario PR","local",2.2],"323,745"],
+	[[11900,"Macara","Deportivo Cuenca","over",2.2],[11930,"Deportivo Pasto","Atletico Bucaramanga","over",3],[11930,"Ponte Preta SP","Operario PR","over",2.7],[11800,"Guabira","Blooming","over",2.15],[11900,"Nacional Asuncion","Sol de America","over",2.2],"842,886"],
+	[[11900,"Macara","Deportivo Cuenca","mitad",1.48],[11930,"Deportivo Pasto","Atletico Bucaramanga","mitad",1.65],[11930,"Ponte Preta SP","Operario PR","mitad",1.615],[11800,"Guabira","Blooming","mitad",1.45],[11900,"Nacional Asuncion","Sol de America","mitad",1.48],"84,655"],
 	[[11600,"Independiente Rivadavia","Platense","over",3.75],[11630,"Sportivo Trinidense","Sportivo Luqueno","over",2.35],"88,125"],
-	[[11600,"Independiente Rivadavia","Platense","mitad",1.8],[11630,"Sportivo Trinidense","Sportivo Luqueno","mitad",1.533],"27,600"],
-	[[11600,"Independiente Rivadavia","Platense","local",2.65],[11630,"Sportivo Trinidense","Sportivo Luqueno","visitante",2.35],"62,275"],
 	[[11530,"Vargas Torres","Independiente Juniors","over",3.4],"34,000"],
-	[[11530,"Vargas Torres","Independiente Juniors","mitad",1.8],"18,000"],
 ]
