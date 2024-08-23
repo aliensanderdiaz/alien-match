e615b1a8c5b8e03a096aedb9f20c87fdcf0fd53c
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-08-23 14:36:29 -0500

    ups

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,7 +1,7 @@
 let APUESTAS_ABIERTAS = [
-	[[11100,"Widzew Lodz","Radomiak Radom","local",1.85],[11100,"Petrolul Ploiesti","FC Otelul Galati","local",2.4],[11130,"Hannover 96","Hamburger SV","visitante",1.8],"79,920"],
-	[[11100,"Petrolul Ploiesti","FC Otelul Galati","over",2.62],"26,250"],
-	[[11100,"Petrolul Ploiesti","FC Otelul Galati","mitad",1.6],"16,000"],
-	[[11100,"Widzew Lodz","Radomiak Radom","local",1.95],[11100,"Petrolul Ploiesti","FC Otelul Galati","local",2.45],[11200,"Uniao Leiria","Alverca","local",1.8],[11130,"Hannover 96","Hamburger SV","visitante",1.8],[11200,"FC Erzgebirge Aue","Dynamo Dresden","visitante",1.8],[11200,"Racing Santander","SD Eibar","visitante",2.25],[11200,"Celta de Vigo","Valencia","visitante",2.55],"1,598,604"],
-	[[11000,"Slutsksakhar Slutsk","FC Vitebsk","over",2.05],[11200,"Celta de Vigo","Valencia","over",2.15],[11200,"Racing Santander","SD Eibar","over",2.25],[11200,"FK Arsenal Dzerzhinsk","Isloch","over",2.05],[11100,"Petrolul Ploiesti","FC Otelul Galati","over",2.55],[11200,"Uniao Leiria","Alverca","over",2],[11200,"Rubin Kazan","FC Dynamo Makhachkala","over",2.15],"2,229,140"],
+	[[11430,"Sevilla","Villarreal","local",1.833],[11430,"SD Huesca","Deportivo La Coruna","local",2.5],"44,280"],
+	[[11330,"Modena FC","Bari","over",2.45],[11345,"Dagenham & Redbridge","Halifax Town","over",2.05],[11430,"SD Huesca","Deportivo La Coruna","over",3.1],"155,698"],
+	[[11430,"SD Huesca","Deportivo La Coruna","mitad",1.7],"38,208"],
+	[[11300,"SC Bastia","SC Amiens","over",2.55],[11330,"Modena FC","Bari","over",2.55],[11300,"Kocaelispor","Erzurum BB","over",2.05],[11345,"Dagenham & Redbridge","Halifax Town","over",2.05],[11430,"SD Huesca","Deportivo La Coruna","over",3.1],"847,129"],
+	[[11300,"Alanyaspor","Goztepe","over",2.1],[11300,"Troyes","Clermont Foot","over",2.1],[11300,"Laval","EA Guingamp","over",2.5],"474,075"],
 ]
