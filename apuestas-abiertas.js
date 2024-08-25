313a3622379f40911c88b6f7dd65d46d40bb8904
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-08-24 19:17:40 -0500

    tik

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,8 +1,4 @@
 let APUESTAS_ABIERTAS = [
-	[[11200,"Levante","CF Cadiz","local",2.05],[11200,"Barcelona","Athletic Bilbao","visitante",2.55],[11200,"Panseraikos","Aris Thessaloniki","visitante",1.9],"99,323"],
-	[[11200,"Levante","CF Cadiz","over",2.375],[11200,"Panseraikos","Aris Thessaloniki","over",2.05],[11200,"Slavia Mozyr","Shakhter Soligorsk","over",2.1],"102,244"],
-	[[11200,"Levante","CF Cadiz","mitad",1.533],[11200,"Panseraikos","Aris Thessaloniki","mitad",1.444],[11200,"Slavia Mozyr","Shakhter Soligorsk","mitad",1.45],"32,115"],
-	[[11115,"Sivasspor","Eyupspor","local",2.05],[11130,"OFI Crete","Atromitos","local",2.05],[11130,"Udinese","Lazio","visitante",2.1],"88,253"],
-	[[11105,"Arsenal de Sarandi","San Martin de Tucuman","over",3.7],[11130,"Udinese","Lazio","over",2.2],[11130,"OFI Crete","Atromitos","over",2],"162,800"],
-	[[11130,"Udinese","Lazio","mitad",1.48],[11105,"Arsenal de Sarandi","San Martin de Tucuman","mitad",1.833],[11130,"OFI Crete","Atromitos","mitad",1.42],"38,529"],
+	[[12200,"Phoenix Rising FC","North Carolina","mitad",1.444],[11930,"Tolima","CD Junior","local",2],[12000,"Tigres UANL","Chivas Guadalajara","local",1.95],[11930,"Nashville SC","Austin FC","visitante",2.2],[11930,"Sporting Kansas City","Orlando City SC","visitante",1.93],[12000,"Atlas de Guadalajara","Pumas Unam","visitante",2.25],"453,917"],
+	[[11800,"Cancun FC","Jaiba Brava","over",2.05],[11800,"Detroit City","Tulsa Roughnecks FC","over",2.05],[11805,"Defensores de Belgrano","Atletico Rafaela","over",2.75],"906,059"],
 ]
