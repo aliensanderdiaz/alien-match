f86842a0dbd2b6ec77dbf65fe59b3e3935b47257
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-08-26 13:10:51 -0500

    ideam

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,6 +1,7 @@
 let APUESTAS_ABIERTAS = [
-	[[11100,"LNZ Cherkasy","Karpaty LVIV","over",2.5],[11130,"Cagliari","Como","over",2],[11200,"Korona Kielce","Stal Mielec","over",2.1],[11200,"Cartagena","Real Zaragoza","over",2.65],[11100,"Gloria Buzau","Universitatea Cluj","over",2.1],[11300,"Union Comercio","Cusco FC","over",2],"1,168,650"],
-	[[11130,"Cagliari","Como","mitad",1.42],[11200,"Korona Kielce","Stal Mielec","mitad",1.45],[11200,"Cartagena","Real Zaragoza","mitad",1.6],[11300,"Union Comercio","Cusco FC","mitad",1.444],"73,758"],
-	[[11100,"LNZ Cherkasy","Karpaty LVIV","over",2.37],"23,750"],
-	[[11130,"Cagliari","Como","local",1.93],[11200,"Korona Kielce","Stal Mielec","local",2],[11200,"Cartagena","Real Zaragoza","visitante",2.25],"165,805"],
+	[[11345,"Verona","Juventus","over",2.15],[11345,"Red Star Paris","Ajaccio","over",2.55],[11400,"CA Barracas Central","Velez Sarsfield","over",2.15],[11400,"Central Cordoba","Banfield","over",2.9],[11430,"CF Cordoba","Burgos CF","over",2.375],[11410,"Nueva Chicago","Colon de Santa Fe","over",3],"2,435,566"],
+	[[11345,"Red Star Paris","Ajaccio","mitad",1.571],[11430,"CF Cordoba","Burgos CF","mitad",1.533],[11345,"Verona","Juventus","mitad",1.45],[11400,"CA Barracas Central","Velez Sarsfield","mitad",1.45],[11400,"Central Cordoba","Banfield","mitad",1.65],[11410,"Nueva Chicago","Colon de Santa Fe","mitad",1.7],"142,102"],
+	[[11345,"Verona","Juventus","visitante",1.85],[11345,"Red Star Paris","Ajaccio","visitante",2.55],[11400,"CA Barracas Central","Velez Sarsfield","visitante",1.9],[11400,"Central Cordoba","Banfield","visitante",2.8],[11400,"FC Uta Arad","Farul Constanta","visitante",2],[11430,"CF Cordoba","Burgos CF","visitante",2.5],"1,254,855"],
+	[[11300,"Union Comercio","Cusco FC","mitad",1.45],"14,500"],
+	[[11300,"Union Comercio","Cusco FC","over",2.04],"20,500"],
 ]
