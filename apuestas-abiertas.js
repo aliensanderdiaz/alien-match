1daeb2940c5e9db39a79e755cadfbd9576ef7bd0
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-08-30 19:49:35 -0500

    p2

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,6 +1,4 @@
 let APUESTAS_ABIERTAS = [
-	[[11900,"Union Santa Fe","Deportivo Riestra","local",1.95],[12000,"Atletico San Luis","Atlas de Guadalajara","visitante",2.1],[12000,"Jaiba Brava","Mineros de Zacatecas","visitante",1.95],[12205,"Tijuana","Leon","visitante",2.15],"171,683"],
+	[[12000,"Atletico San Luis","Atlas de Guadalajara","visitante",2.1],[12000,"Jaiba Brava","Mineros de Zacatecas","visitante",1.95],[12205,"Tijuana","Leon","visitante",2.15],"88,043"],
 	[[11900,"Union Santa Fe","Deportivo Riestra","over",2.7],[11930,"Santos SP","Ponte Preta SP","over",2.1],[11910,"Atletico Rafaela","Chaco For Ever","over",3.4],"192,780"],
-	[[11930,"Santos SP","Ponte Preta SP","mitad",1.42],[11900,"Union Santa Fe","Deportivo Riestra","mitad",1.6],[11910,"Atletico Rafaela","Chaco For Ever","mitad",1.8],"40,896"],
-	[[11700,"Sarmiento","Atletico Lanus","over",2.87],"28,750"],
 ]
