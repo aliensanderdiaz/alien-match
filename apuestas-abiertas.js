763f9bdb3d8563a4e378e413809c179ac2589ce6
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-09-01 10:58:26 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,6 +1,7 @@
 let APUESTAS_ABIERTAS = [
-	[[11000,"AC Le Havre","AJ Auxerre","local",2.25],[11000,"Angers SCO","Nice","local",2.85],[11000,"Stade Reims","Rennes","local",2.15],[11000,"Manchester United","Liverpool","local",2],[10945,"PSV Eindhoven","GO Ahead Eagles","visitante",3.2],[11000,"Slavia Prague","FK Pardubice","visitante",4.5],[11000,"FK Teplice","FK Mlada Boleslav","visitante",2],[11000,"Deportivo La Coruna","Ferrol","visitante",2.75],[11000,"Burgos CF","CD Castellon","visitante",2.05],[11000,"Osasuna","Celta de Vigo","visitante",2.25],[11000,"CD Alaves","Las Palmas","visitante",3],"302,188,998"],
-	[[11000,"CD Alaves","Las Palmas","over",2.3],[11000,"Osasuna","Celta de Vigo","over",2],[11000,"Deportivo La Coruna","Ferrol","over",2.35],[11000,"Angers SCO","Nice","over",2.3],[11000,"AC Le Havre","AJ Auxerre","over",2.1],"522,123"],
-	[[11000,"CD Alaves","Las Palmas","mitad",1.48],[11000,"Osasuna","Celta de Vigo","mitad",1.42],[11000,"Deportivo La Coruna","Ferrol","mitad",1.5],[11000,"Angers SCO","Nice","mitad",1.5],[11000,"AC Le Havre","AJ Auxerre","mitad",1.45],"68,565"],
-	[[10830,"FC Botosani","Petrolul Ploiesti","over",2.75],"57,750"],
+	[[11115,"Istanbul Basaksehir FK","Antalyaspor","local",1.769],[11115,"Eyupspor","Trabzonspor","local",1.95],[11130,"Génova","Verona","local",2],[11130,"Fiorentina","Monza","local",1.769],[11130,"Palestino","Nublense","local",1.95],[11130,"Volos NPS","OFI Crete","local",2.15],[11130,"St. Gilloise","Anderlecht","visitante",2.2],"1,125,976"],
+	[[11130,"Génova","Verona","over",2.3],[11130,"Fiorentina","Monza","over",2],[11130,"Volos NPS","OFI Crete","over",2.2],[11130,"Club Nueve de Octubre","Cuniburo FC","over",2.2],"222,640"],
+	[[11130,"Génova","Verona","mitad",1.5],[11130,"Volos NPS","OFI Crete","mitad",1.48],[11130,"Club Nueve de Octubre","Cuniburo FC","mitad",1.45],"32,190"],
+	[[11030,"Bayern Munich","Freiburg","visitante",2.8],[11030,"Pogon Szczecin","Slask Wroclaw","visitante",2.25],"63,000"],
+	[[11000,"CD Alaves","Las Palmas","over",2.3],[11000,"Deportivo La Coruna","Ferrol","over",2.35],[11000,"Angers SCO","Nice","over",2.3],[11000,"AC Le Havre","AJ Auxerre","over",2.1],"522,123"],
 ]
