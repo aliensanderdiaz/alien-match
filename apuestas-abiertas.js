aafa62ddb35cd07f23c70e59fd8a58f80eefb5ca
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-09-04 17:50:45 -0500

    vila

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,4 +1,6 @@
 let APUESTAS_ABIERTAS = [
-	[[11330,"Club Atletico Tembetary","CS 2 de Mayo","over",2.1],"21,000"],
-	[[11330,"Club Atletico Tembetary","CS 2 de Mayo","mitad",1.45],"14,500"],
+	[[11800,"Vila Nova FC","CRB Maceio AL","local",1.93],"19,300"],
+	[[11800,"Vila Nova FC","CRB Maceio AL","over",2.55],[11900,"Guayaquil City FC","Club Nueve de Octubre","over",2.375],[11930,"Internacional FC de Palmira","Orsomarso","over",2.6],[11930,"Leones FC","Union Magdalena","over",2.1],[11930,"Deportes Iquique","Coquimbo","over",2.05],"677,876"],
+	[[11800,"Vila Nova FC","CRB Maceio AL","mitad",1.571],[11900,"Guayaquil City FC","Club Nueve de Octubre","mitad",1.48],[11930,"Deportes Iquique","Coquimbo","mitad",1.444],[11930,"Internacional FC de Palmira","Orsomarso","mitad",1.6],[11930,"Leones FC","Union Magdalena","mitad",1.42],"76,325"],
+	[[11645,"Caracas FC","Academia Puerto Cabello","over",2.75],"27,500"],
 ]
