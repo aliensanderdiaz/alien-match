f95a75d3dc10f4552c30197d7cd61e77d07dcf3a
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-09-04 20:58:49 -0500

    sri

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,6 +1,5 @@
 let APUESTAS_ABIERTAS = [
-	[[11800,"Vila Nova FC","CRB Maceio AL","local",1.93],"19,300"],
-	[[11800,"Vila Nova FC","CRB Maceio AL","over",2.55],[11900,"Guayaquil City FC","Club Nueve de Octubre","over",2.375],[11930,"Internacional FC de Palmira","Orsomarso","over",2.6],[11930,"Leones FC","Union Magdalena","over",2.1],[11930,"Deportes Iquique","Coquimbo","over",2.05],"677,876"],
-	[[11800,"Vila Nova FC","CRB Maceio AL","mitad",1.571],[11900,"Guayaquil City FC","Club Nueve de Octubre","mitad",1.48],[11930,"Deportes Iquique","Coquimbo","mitad",1.444],[11930,"Internacional FC de Palmira","Orsomarso","mitad",1.6],[11930,"Leones FC","Union Magdalena","mitad",1.42],"76,325"],
-	[[11645,"Caracas FC","Academia Puerto Cabello","over",2.75],"27,500"],
+	[[10515,"Sri Lanka","Camboya","local",2.75],"22,462"],
+	[[10515,"Sri Lanka","Camboya","over",2.62],"21,436"],
+	[[10515,"Sri Lanka","Camboya","mitad",1.6],"13,066"],
 ]
