d4d24542dd38266a46d5852402e84b970a55f026
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-09-07 16:11:11 -0500

    lunes

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,4 +1,7 @@
 let APUESTAS_ABIERTAS = [
-	[[10910,"Charlton Athletic","Rotherham","over",2.1],[10915,"Granada","Deportivo La Coruna","over",2.15],"45,150"],
-	[[10900,"Barrow FC","Swindon Town","over",2],[10900,"Crewe Alexandra","Morecambe","over",2.05],[10900,"Doncaster Rovers","Gillingham","over",2],[10900,"Hartlepool","Halifax Town","over",2],[10900,"Braintree Town","Woking FC","over",2.2],"360,800"],
+	[[11700,"Avai SC","Sport Recife PE","local",2.2],"22,000"],
+	[[11700,"Avai SC","Sport Recife PE","over",2.875],[11700,"Tristan Suarez","Talleres de Remedios de Escalada","over",2.65],"76,188"],
+	[[11700,"Avai SC","Sport Recife PE","mitad",1.65],[11700,"Tristan Suarez","Talleres de Remedios de Escalada","mitad",1.615],"26,654"],
+	[[11610,"América de Cali","Jaguares De Córdoba","over",2.04],"20,500"],
+	[[11610,"América de Cali","Jaguares De Córdoba","mitad",1.42],"14,200"],
 ]
