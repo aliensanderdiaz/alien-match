9b8e11f4f7f511a9f6fdc808cf49405ca74067fe
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-09-07 12:45:13 -0500

    lu

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,7 +1,4 @@
 let APUESTAS_ABIERTAS = [
-	[[10900,"Colchester","Bromley FC","visitante",2.1],[10900,"MK Dons","Walsall","visitante",2.05],[10915,"Granada","Deportivo La Coruna","visitante",2.25],"96,863"],
-	[[10900,"Cheltenham Town","Harrogate Town","local",1.75],[10900,"Barrow FC","Swindon Town","local",1.769],[10910,"Charlton Athletic","Rotherham","local",2],[10900,"Crewe Alexandra","Morecambe","local",1.8],[10900,"Doncaster Rovers","Gillingham","local",1.75],[10900,"Newport","Port Vale","local",2.1],"409,621"],
-	[[10900,"Hartlepool","Halifax Town","mitad",1.42],[10900,"Braintree Town","Woking FC","mitad",1.48],[10900,"Crewe Alexandra","Morecambe","mitad",1.42],[10910,"Charlton Athletic","Rotherham","mitad",1.45],[10915,"Granada","Deportivo La Coruna","mitad",1.45],"62,744"],
 	[[10910,"Charlton Athletic","Rotherham","over",2.1],[10915,"Granada","Deportivo La Coruna","over",2.15],"45,150"],
 	[[10900,"Barrow FC","Swindon Town","over",2],[10900,"Crewe Alexandra","Morecambe","over",2.05],[10900,"Doncaster Rovers","Gillingham","over",2],[10900,"Hartlepool","Halifax Town","over",2],[10900,"Braintree Town","Woking FC","over",2.2],"360,800"],
 ]
