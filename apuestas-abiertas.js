0681ae5e34ad6866def36ccbcaaab408d969e633
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-09-08 10:45:39 -0500

    chi

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,7 +1,2 @@
 let APUESTAS_ABIERTAS = [
-	[[11700,"Avai SC","Sport Recife PE","local",2.2],"22,000"],
-	[[11700,"Avai SC","Sport Recife PE","over",2.875],[11700,"Tristan Suarez","Talleres de Remedios de Escalada","over",2.65],"76,188"],
-	[[11700,"Avai SC","Sport Recife PE","mitad",1.65],[11700,"Tristan Suarez","Talleres de Remedios de Escalada","mitad",1.615],"26,654"],
-	[[11610,"América de Cali","Jaguares De Córdoba","over",2.04],"20,500"],
-	[[11610,"América de Cali","Jaguares De Córdoba","mitad",1.42],"14,200"],
 ]
