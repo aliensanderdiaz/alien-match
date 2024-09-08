8914908f4f64bbb399c618ca460c78ec771763cc
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-09-08 18:01:31 -0500

    dom

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,2 +1,6 @@
 let APUESTAS_ABIERTAS = [
+	[[11920,"Deportivo Pasto","Independiente Medellin","visitante",2.75],"27,500"],
+	[[11830,"Cooper","Plaza Colonia","over",2.55],[11920,"Deportivo Pasto","Independiente Medellin","over",2.7],[11900,"Club Sport Emelec","Tecnico Universitario","over",2.25],[11910,"Patronato Parana","Quilmes AC","over",2.75],"426,009"],
+	[[11920,"Deportivo Pasto","Independiente Medellin","mitad",1.6],[11830,"Cooper","Plaza Colonia","mitad",1.5],[11900,"Club Sport Emelec","Tecnico Universitario","mitad",1.48],[11910,"Patronato Parana","Quilmes AC","mitad",1.615],"57,378"],
+	[[11630,"America Mineiro MG","Guarani SP","over",2.9],[11710,"Alianza FC","Fortaleza FC","over",2.6],[11710,"Deportivo Moron","Estudiantes Rio Cuarto","over",3.7],"864,838"],
 ]
