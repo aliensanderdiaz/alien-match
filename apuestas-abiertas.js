30576379d9c2efaa504150f0616263d331b3b1f6
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-09-09 10:37:52 -0500

    l

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,5 +1,8 @@
 let APUESTAS_ABIERTAS = [
+	[[11345,"Noruega","Austria","local",1.95],[11345,"Montenegro","Gales","visitante",2.15],[11400,"Albacete Balompie","SD Eibar","visitante",2.15],[11400,"CD Castellon","CF Cadiz","visitante",2.375],[11345,"Francia","Bélgica","local",1.727],"369,774"],
+	[[11345,"Montenegro","Gales","mitad",1.55],[11400,"Albacete Balompie","SD Eibar","mitad",1.42],[11400,"CD Castellon","CF Cadiz","mitad",1.42],[11400,"Etiopia","Congo DR","mitad",1.6],[11400,"Angola","Sudán","mitad",1.6],"80,011"],
+	[[11345,"Montenegro","Gales","over",2.55],[11400,"Albacete Balompie","SD Eibar","over",2],[11400,"CD Castellon","CF Cadiz","over",2],[11400,"Etiopia","Congo DR","over",2.8],[11400,"Angola","Sudán","over",2.7],"771,120"],
 	[[11100,"Chipre","Kosovo","visitante",1.95],"19,500"],
-	[[11000,"Niger","Ghana","over",2.35],[11000,"Guinea Ecuatorial","Togo","over",2.75],[11100,"Chipre","Kosovo","over",2.25],[11100,"Uganda","República del Congo","over",2.8],[11100,"Madagascar","Comores","over",2.55],"1,038,201"],
-	[[11100,"Chipre","Kosovo","mitad",1.48],[11000,"Niger","Ghana","mitad",1.5],[11000,"Guinea Ecuatorial","Togo","mitad",1.615],[11100,"Uganda","República del Congo","mitad",1.6],[11100,"Madagascar","Comores","mitad",1.6],"91,806"],
+	[[11100,"Niger","Ghana","over",2.35],[11000,"Guinea Ecuatorial","Togo","over",2.75],[11100,"Chipre","Kosovo","over",2.25],[11100,"Uganda","República del Congo","over",2.8],[11100,"Madagascar","Comores","over",2.55],"1,038,201"],
+	[[11100,"Chipre","Kosovo","mitad",1.48],[11100,"Niger","Ghana","mitad",1.5],[11100,"Uganda","República del Congo","mitad",1.6],[11100,"Madagascar","Comores","mitad",1.6],"91,806"],
 ]
