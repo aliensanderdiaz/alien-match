a1e8e1890eb47f55d76a2244e5e7aa82a2105c5d
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-09-11 11:01:44 -0500

    l

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,6 +1,2 @@
 let APUESTAS_ABIERTAS = [
-	[[12000,"Canadá","Mexico","local",2.45],[11930,"Paraguay","Brasil","visitante",1.727],"69,943"],
-	[[11930,"Paraguay","Brasil","over",2.25],[12000,"Canadá","Mexico","over",2.2],"49,500"],
-	[[11930,"Paraguay","Brasil","mitad",1.45],[12000,"Canadá","Mexico","mitad",1.48],"21,460"],
-	[[11700,"Venezuela","Uruguay","over",2.65],"26,500"],
 ]
