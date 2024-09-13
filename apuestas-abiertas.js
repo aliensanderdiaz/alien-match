d965dcc46d4a128dec20a624dad63d91c4c29587
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-09-13 10:57:02 -0500

    lunes

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,2 +1,6 @@
 let APUESTAS_ABIERTAS = [
+	[[11200,"Torreense","Portimonense SC","local",2.15],[11100,"Motor Lublin","Gornik Zabrze","visitante",2.2],[11130,"Karlsruher SC","Schalke 04","visitante",1.95],"92,235"],
+	[[11100,"Motor Lublin","Gornik Zabrze","over",2],[11200,"Torreense","Portimonense SC","over",2],"40,000"],
+	[[11100,"Motor Lublin","Gornik Zabrze","mitad",1.42],[11200,"Torreense","Portimonense SC","mitad",1.42],"20,164"],
+	[[11000,"Zmina Obolon Kyiv","FC Oleksandriya","over",2.04],"20,500"],
 ]
