db449704d7199c5d486ab64d900f80ab73dc1c1f
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-09-16 13:58:21 -0500

    l

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,5 +1,7 @@
 let APUESTAS_ABIERTAS = [
-	[[10830,"Augsburg","FC St. Pauli","over",2.05],[10830,"Panseraikos","Levadiakos","over",2.5],[10830,"Egaleo Athen","Iraklis 1908","over",2],[10830,"AO Neas Ionias","Anagennisi Artas","over",2.25],"461,250"],
-	[[10830,"Egaleo Athen","Iraklis 1908","over",2],[10830,"AO Neas Ionias","Anagennisi Artas","over",2.2],"73,901"],
-	[[10800,"Torino","Lecce","over",2.3],[10800,"Salernitana","SC Pisa","over",2],[10800,"Panachaiki","Kalamata","over",2.4],"655,776"],
+	[[11345,"Grenoble","Caen","local",2.15],[11400,"Rayo Vallecano","Osasuna","local",2.1],[11400,"Birmingham City","Wrexham FC","visitante",2.25],[11415,"CF Estrela","Boavista","visitante",2.75],"279,366"],
+	[[11345,"Grenoble","Caen","over",2.05],[11400,"Rayo Vallecano","Osasuna","over",2.5],[11415,"CF Estrela","Boavista","over",2.25],[11430,"Montevideo City Torque","Colon FC de Uruguay","over",2.45],"282,516"],
+	[[11345,"Grenoble","Caen","mitad",1.45],[11400,"Rayo Vallecano","Osasuna","mitad",1.55],[11415,"CF Estrela","Boavista","mitad",1.48],[11430,"Montevideo City Torque","Colon FC de Uruguay","mitad",1.48],"49,229"],
+	[[11300,"CA Barracas Central","Banfield","over",2.45],[11300,"Rapid Bucharest","Universitatea Cluj","over",2.35],"57,575"],
+	[[11200,"Feirense","Pacos Ferreira","over",2],[11200,"Adanaspor","Keciorengucu","over",2],"40,000"],
 ]
