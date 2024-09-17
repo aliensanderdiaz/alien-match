c9d9e0f51131ce09550ea41b0c823ff60cdf03fd
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-09-17 16:40:46 -0500

    pith

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,4 +1,5 @@
 let APUESTAS_ABIERTAS = [
-	[[11910,"San Martin de Tucuman","Agropecuario Argentino","over",2.84],"16,715"],
-	[[11800,"Internacional RS","Cuiaba MT","over",2.35],[11830,"Alianza FC","Atlético Nacional","over",2.55],[11900,"Independiente Rivadavia","Defensa Y Justicia","over",2.55],[11900,"Tecnico Universitario","Libertad (Ecu)","over",2.05],[11900,"Guarani Asuncion","Nacional Asuncion","over",2.15],[11910,"San Martin de Tucuman","Agropecuario Argentino","over",2.85],"1,919,488"],
+	[[11600,"Envigado FC","Jaguares De Córdoba","local",2.55],[11630,"Sportivo Ameliano","Sol de America","local",2.2],[11700,"Novorizontino SP","Brusque FC","local",1.769],"99,254"],
+	[[11530,"Atletico de Cali","Union Magdalena","over",2.15],[11600,"Envigado FC","Jaguares De Córdoba","over",2.55],[11630,"Sportivo Ameliano","Sol de America","over",2.5],[11700,"Novorizontino SP","Brusque FC","over",2.55],[11730,"Real Soacha Cundinamarca","Real Cartagena","over",2.3],"803,872"],
+	[[11600,"Envigado FC","Jaguares De Córdoba","mitad",1.571],[11700,"Novorizontino SP","Brusque FC","mitad",1.55],[11630,"Sportivo Ameliano","Sol de America","mitad",1.571],[11730,"Real Soacha Cundinamarca","Real Cartagena","mitad",1.5],"57,413"],
 ]
