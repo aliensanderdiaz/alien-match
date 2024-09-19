930aff03b5a5a0eb6394a6a88f6be6995dc3859a
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-09-19 13:21:02 -0500

    champ

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,5 +1,6 @@
 let APUESTAS_ABIERTAS = [
-	[[11400,"Brest","Sturm Graz","local",1.85],[11400,"AS Monaco","Barcelona","local",2.15],[11145,"Red Star Belgrade","Benfica","visitante",1.769],[11200,"CD Leganes","Athletic Bilbao","visitante",2.1],[11400,"Atalanta","Arsenal","visitante",1.769],[11400,"Atlético Madrid","RB Leipzig","visitante",2.4],"627,494"],
+	[[11400,"Brest","Sturm Graz","local",1.85],[11400,"AS Monaco","Barcelona","local",2.15],[11400,"Atalanta","Arsenal","visitante",1.8],[11400,"Atlético Madrid","RB Leipzig","visitante",2.45],"175,408"],
+	[[11300,"Union Comercio","Alianza Atletico","over",2.2],"22,000"],
+	[[11200,"CD Leganes","Athletic Bilbao","over",2.7],"27,000"],
 	[[11200,"CD Leganes","Athletic Bilbao","over",2.45],[11300,"Union Comercio","Alianza Atletico","over",2.15],[11400,"Brest","Sturm Graz","over",2],"105,350"],
-	[[11200,"CD Leganes","Athletic Bilbao","mitad",1.55],[11300,"Union Comercio","Alianza Atletico","mitad",1.48],"22,940"],
 ]
