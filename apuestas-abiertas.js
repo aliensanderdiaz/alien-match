3581d6aea88fb0badbf4787027b8b86a9f08d49e
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-09-18 23:01:34 -0500

    600

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,12 +1,5 @@
 let APUESTAS_ABIERTAS = [
-	[[11630,"USA (F) U20","Corea del Norte (F) Sub-20","local",2.55],[11700,"Sport Recife PE","Goias GO","local",1.8],[11700,"Fluminense RJ","Atletico MG","visitante",2.875],[11730,"Vila Nova FC","Chapecoense SC","visitante",2.9],"382,691"],
-	[[11530,"Comerciantes Unidos","Universidad Cesar Vallejo","over",2.25],[11600,"Gualaceo SC","Cuniburo FC","over",2.35],[11600,"Club Martin Ledesma","Guairena FC","over",2.7],[11700,"Fluminense RJ","Atletico MG","over",3.1],[11700,"Sport Recife PE","Goias GO","over",2.45],[11730,"Vila Nova FC","Chapecoense SC","over",2.2],"2,385,419"],
-	[[11700,"Fluminense RJ","Atletico MG","mitad",1.7],[11700,"Sport Recife PE","Goias GO","mitad",1.533],[11730,"Vila Nova FC","Chapecoense SC","mitad",1.48],[11530,"Comerciantes Unidos","Universidad Cesar Vallejo","mitad",1.48],[11600,"Gualaceo SC","Cuniburo FC","mitad",1.48],[11600,"Club Martin Ledesma","Guairena FC","mitad",1.615],"136,504"],
-	[[11400,"Manchester City","Inter","visitante",2.8],"28,000"],
-	[[11300,"Atlético Grau","Alianza Lima","over",2.55],[11400,"San Antonio Bulo Bulo","Always Ready","over",2.25],"57,375"],
-	[[11300,"Atlético Grau","Alianza Lima","mitad",1.6],[11400,"San Antonio Bulo Bulo","Always Ready","mitad",1.5],"24,000"],
-	[[11230,"Lausanne Sports","FC Lugano","visitante",1.95],"19,500"],
-	[[11145,"Bologna","Shakhtar Donetsk","over",2],[11200,"Real Betis","Getafe","over",2.8],[11200,"Samsunspor","Istanbul Basaksehir FK","over",2.2],[11100,"CSKA Moscow","Krasnodar","over",2],"246,400"],
-	[[11145,"Bologna","Shakhtar Donetsk","mitad",1.42],[11200,"Real Betis","Getafe","mitad",1.615],[11200,"Samsunspor","Istanbul Basaksehir FK","mitad",1.48],[11100,"CSKA Moscow","Krasnodar","mitad",1.42],"69,901"],
-	[[11200,"Real Betis","Getafe","local",2.1],[11145,"Bologna","Shakhtar Donetsk","local",1.85],[11200,"Samsunspor","Istanbul Basaksehir FK","visitante",2.3],[11230,"Lausanne Sports","FC Lugano","visitante",1.833],"621,278"],
+	[[11400,"Brest","Sturm Graz","local",1.85],[11400,"AS Monaco","Barcelona","local",2.15],[11145,"Red Star Belgrade","Benfica","visitante",1.769],[11200,"CD Leganes","Athletic Bilbao","visitante",2.1],[11400,"Atalanta","Arsenal","visitante",1.769],[11400,"Atlético Madrid","RB Leipzig","visitante",2.4],"627,494"],
+	[[11200,"CD Leganes","Athletic Bilbao","over",2.45],[11300,"Union Comercio","Alianza Atletico","over",2.15],[11400,"Brest","Sturm Graz","over",2],"105,350"],
+	[[11200,"CD Leganes","Athletic Bilbao","mitad",1.55],[11300,"Union Comercio","Alianza Atletico","mitad",1.48],"22,940"],
 ]
