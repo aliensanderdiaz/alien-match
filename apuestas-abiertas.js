9c748bbe64db9e46cf7a7e767b1b87906fc508ee
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-09-19 14:56:07 -0500

    ecu

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,6 +1,7 @@
 let APUESTAS_ABIERTAS = [
-	[[11400,"Brest","Sturm Graz","local",1.85],[11400,"AS Monaco","Barcelona","local",2.15],[11400,"Atalanta","Arsenal","visitante",1.8],[11400,"Atlético Madrid","RB Leipzig","visitante",2.45],"175,408"],
-	[[11300,"Union Comercio","Alianza Atletico","over",2.2],"22,000"],
-	[[11200,"CD Leganes","Athletic Bilbao","over",2.7],"27,000"],
-	[[11200,"CD Leganes","Athletic Bilbao","over",2.45],[11300,"Union Comercio","Alianza Atletico","over",2.15],[11400,"Brest","Sturm Graz","over",2],"105,350"],
+	[[11530,"Club Nueve de Octubre","Independiente Juniors","over",2.5],"25,000"],
+	[[11530,"Club Nueve de Octubre","Independiente Juniors","mitad",1.53],"15,333"],
+	[[11700,"Flamengo RJ","Penarol Montevideo","local",1.85],[11700,"Talleres de Remedios de Escalada","Huracan","visitante",2],[11730,"CRB Maceio AL","Ponte Preta SP","visitante",3],"111,000"],
+	[[11700,"Flamengo RJ","Penarol Montevideo","over",2.55],[11700,"Talleres de Remedios de Escalada","Huracan","over",2.9],[11730,"CRB Maceio AL","Ponte Preta SP","over",2.6],[11530,"Club Nueve de Octubre","Independiente Juniors","over",2.5],[11830,"Encarnacion","Club Tacuary","over",2.25],[11900,"Vargas Torres","Guayaquil City FC","over",3.1],"3,352,708"],
+	[[11700,"Flamengo RJ","Penarol Montevideo","mitad",1.533],[11700,"Talleres de Remedios de Escalada","Huracan","mitad",1.615],[11730,"CRB Maceio AL","Ponte Preta SP","mitad",1.571],[11530,"Club Nueve de Octubre","Independiente Juniors","mitad",1.533],[11830,"Encarnacion","Club Tacuary","mitad",1.5],[11900,"Vargas Torres","Guayaquil City FC","mitad",1.65],"147,713"],
 ]
