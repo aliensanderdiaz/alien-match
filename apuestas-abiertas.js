ea3c93564f72813786c2129a444a7266ff4f5aa2
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-09-23 16:58:23 -0500

    ba

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,8 +1,5 @@
 let APUESTAS_ABIERTAS = [
-	[[11000,"FC Uta Arad","CS Municipal Studentesc Iasi","visitante",2.65],[11200,"Gaziantep BB","Trabzonspor","visitante",1.95],[11200,"Goztepe","Kayserispor","visitante",2.4],"124,020"],
-	[[10730,"Kolos Kovalyovka","Zorya Lugansk","over",2.5],[11000,"FC Uta Arad","CS Municipal Studentesc Iasi","over",2.2],"55,000"],
-	[[10730,"Kolos Kovalyovka","Zorya Lugansk","mitad",1.571],[11000,"FC Uta Arad","CS Municipal Studentesc Iasi","mitad",1.48],"23,257"],
-	[[12030,"CD Junior","Rionegro Aguilas","local",2.15],"21,500"],
-	[[12030,"CD Junior","Rionegro Aguilas","mitad",1.6],[12000,"Universidad Cesar Vallejo","Asociacion Deportiva Tarma","mitad",1.42],"22,720"],
-	[[12000,"Universidad Cesar Vallejo","Asociacion Deportiva Tarma","over",2],[12030,"CD Junior","Rionegro Aguilas","over",2.7],"54,000"],
+	[[11645,"Sarmiento","Central Cordoba","over",3.1],[11730,"Boca Juniors de Cali","CD Real Santander","over",2.15],"66,650"],
+	[[11645,"Sarmiento","Central Cordoba","visitante",2.87],"28,750"],
+	[[11645,"Sarmiento","Central Cordoba","mitad",1.727],[11730,"Boca Juniors de Cali","CD Real Santander","mitad",1.42],"24,527"],
 ]
