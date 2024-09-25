8942ab164057dfb1c7e4d3aaf4f2071d71da46ea
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-09-25 12:41:29 -0500

    lunes

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,6 +1,8 @@
 let APUESTAS_ABIERTAS = [
-	[[10500,"Kashima Antlers","Vissel Kobe","over",2.15],[10800,"Mashuk-Kmv Pyatigorsk","Rodina Moscow","over",2.1],[10800,"Pas Giannina","Panachaiki","over",2.2],[10800,"Egaleo Athen","Kampaniakos","over",2.15],"213,560"],
-	[[10500,"Kashima Antlers","Vissel Kobe","visitante",2],[10635,"Shanghai Port","Shanghai Shenhua","visitante",2.25],"45,000"],
-	[[10500,"Kashima Antlers","Vissel Kobe","mitad",1.45],[10800,"Pas Giannina","Panachaiki","mitad",1.48],[10800,"Egaleo Athen","Kampaniakos","mitad",1.48],"31,761"],
-	[[11930,"River Plate","Colo Colo","over",2.1],[11930,"Corinthians SP","Fortaleza CE","over",2.2],[11930,"Botafogo SP","CRB Maceio AL","over",2.95],[11930,"Ponte Preta SP","America Mineiro MG","over",2.4],[11930,"Barranquilla","Atletico Huila","over",2.2],"3,382,173"],
+	[[11330,"Gornik Zabrze","Radomiak Radom","local",1.85],[11400,"Anderlecht","Ferencvarosi TC","local",2],[11400,"Nice","Real Sociedad","local",2.25],[11300,"Cobresal","Audax Italiano","visitante",2.25],[11400,"Midtjylland","Hoffenheim","visitante",1.769],[11400,"Dynamo Kiev","Lazio","visitante",2.05],[11400,"Ludogorets","Slavia Prague","visitante",1.93],[11400,"Génova","Sampdoria","visitante",2.65],"3,474,628"],
+	[[11300,"Juventud de Las Piedras","Albion FC","over",2.625],[11400,"Dynamo Kiev","Lazio","over",2.1],[11400,"Nice","Real Sociedad","over",2.5],[11400,"Ludogorets","Slavia Prague","over",2.1],[11400,"Anderlecht","Ferencvarosi TC","over",2],[11400,"Génova","Sampdoria","over",2.3],"1,331,269"],
+	[[11400,"Dynamo Kiev","Lazio","mitad",1.45],[11400,"Nice","Real Sociedad","mitad",1.55],[11400,"Ludogorets","Slavia Prague","mitad",1.42],[11400,"Anderlecht","Ferencvarosi TC","mitad",1.42],[11300,"Juventud de Las Piedras","Albion FC","mitad",1.55],[11400,"Génova","Sampdoria","mitad",1.5],"105,366"],
+	[[11230,"Chojniczanka","Znicz Pruszkow","local",2.15],"21,500"],
+	[[11200,"TSV Alemannia Aachen","SV Waldhof Mannheim","visitante",2],[11200,"Girona","Rayo Vallecano","visitante",2.75],[11200,"1.FC Saarbrücken","FC Viktoria Köln","visitante",2.45],"802,987"],
+	[[11100,"GKS Olimpia Grudziadz","GKS Tychy 71","visitante",1.8],"182,160"],
 ]
