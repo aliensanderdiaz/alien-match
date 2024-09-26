229c943e86d23bb460b1d3ef242892a1e161a4b8
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-09-26 06:45:44 -0500

    1m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,8 +1,4 @@
 let APUESTAS_ABIERTAS = [
-	[[11330,"Gornik Zabrze","Radomiak Radom","local",1.85],[11400,"Anderlecht","Ferencvarosi TC","local",2],[11400,"Nice","Real Sociedad","local",2.25],[11300,"Cobresal","Audax Italiano","visitante",2.25],[11400,"Midtjylland","Hoffenheim","visitante",1.769],[11400,"Dynamo Kiev","Lazio","visitante",2.05],[11400,"Ludogorets","Slavia Prague","visitante",1.93],[11400,"Génova","Sampdoria","visitante",2.65],"3,474,628"],
-	[[11300,"Juventud de Las Piedras","Albion FC","over",2.625],[11400,"Dynamo Kiev","Lazio","over",2.1],[11400,"Nice","Real Sociedad","over",2.5],[11400,"Ludogorets","Slavia Prague","over",2.1],[11400,"Anderlecht","Ferencvarosi TC","over",2],[11400,"Génova","Sampdoria","over",2.3],"1,331,269"],
-	[[11400,"Dynamo Kiev","Lazio","mitad",1.45],[11400,"Nice","Real Sociedad","mitad",1.55],[11400,"Ludogorets","Slavia Prague","mitad",1.42],[11400,"Anderlecht","Ferencvarosi TC","mitad",1.42],[11300,"Juventud de Las Piedras","Albion FC","mitad",1.55],[11400,"Génova","Sampdoria","mitad",1.5],"105,366"],
-	[[11230,"Chojniczanka","Znicz Pruszkow","local",2.15],"21,500"],
-	[[11200,"TSV Alemannia Aachen","SV Waldhof Mannheim","visitante",2],[11200,"Girona","Rayo Vallecano","visitante",2.75],[11200,"1.FC Saarbrücken","FC Viktoria Köln","visitante",2.45],"802,987"],
-	[[11100,"GKS Olimpia Grudziadz","GKS Tychy 71","visitante",1.8],"182,160"],
+	[[10700,"Kifisias","Lamia","over",2.15],[10800,"Cerrito CS","Colon FC de Uruguay","over",2.6],[10900,"Spartak Kostroma","FK Yenisey","over",2.1],[11100,"Larissa","Panseraikos","over",2.15],[11130,"Monza","Brescia","over",2.1],[11200,"Las Palmas","Real Betis","over",2],"1,060,032"],
+	[[11200,"Las Palmas","Real Betis","mitad",1.42],[10700,"Kifisias","Lamia","mitad",1.48],[10800,"Cerrito CS","Colon FC de Uruguay","mitad",1.55],[10900,"Spartak Kostroma","FK Yenisey","mitad",1.45],[11100,"Larissa","Panseraikos","mitad",1.48],[11130,"Monza","Brescia","mitad",1.45],"101,363"],
 ]
