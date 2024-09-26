1b8ff366e0208acf18f352dbe86782b9dc5064e6
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-09-26 16:36:09 -0500

    l

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,7 +1,9 @@
 let APUESTAS_ABIERTAS = [
-	[[11500,"Mushuc Runa","Libertad (Ecu)","over",2],[11530,"Club Nueve de Octubre","Vargas Torres","over",2.35],"47,000"],
-	[[11530,"Club Nueve de Octubre","Vargas Torres","mitad",1.45],[11600,"Club Leones Del Norte","Manta FC","mitad",1.65],"23,925"],
-	[[11400,"Roma","Athletic Bilbao","visitante",2.45],[11400,"Lyon","Olympiacos","local",1.75],[11400,"Ajax Amsterdam","Besiktas","visitante",2.1],[11400,"Celta de Vigo","Atlético Madrid","visitante",1.8],"162,068"],
-	[[11400,"Roma","Athletic Bilbao","over",2.15],"21,500"],
-	[[11400,"Roma","Athletic Bilbao","mitad",1.48],"14,800"],
+	[[11700,"Chapecoense SC","Amazonas FC","local",2.15],[11700,"Penarol Montevideo","Flamengo RJ","visitante",2.2],"47,300"],
+	[[11700,"Penarol Montevideo","Flamengo RJ","over",2.55],[11700,"Chapecoense SC","Amazonas FC","over",2.7],"68,850"],
+	[[11700,"Penarol Montevideo","Flamengo RJ","mitad",1.55],[11700,"Chapecoense SC","Amazonas FC","mitad",1.615],"25,038"],
+	[[11700,"Chapecoense SC","Amazonas FC","local",2.15],[11930,"Cruzeiro MG","Libertad Asuncion","local",1.769],[11700,"Penarol Montevideo","Flamengo RJ","visitante",2.15],[11930,"Racing Club","Athletico PR","visitante",3.3],[12030,"Atlético Nacional","CD Junior","visitante",2.95],"796,155"],
+	[[11700,"Penarol Montevideo","Flamengo RJ","over",2.5],[11700,"Chapecoense SC","Amazonas FC","over",2.65],[11830,"Independiente Santa Fe","Alianza FC","over",2.15],[11930,"Racing Club","Athletico PR","over",2.1],[11930,"Cruzeiro MG","Libertad Asuncion","over",2.1],[12030,"Atlético Nacional","CD Junior","over",2.15],[11900,"Blooming","Always Ready","over",2.35],"3,173,725"],
+	[[11700,"Penarol Montevideo","Flamengo RJ","mitad",1.55],[11700,"Chapecoense SC","Amazonas FC","mitad",1.6],[11830,"Independiente Santa Fe","Alianza FC","mitad",1.45],[11930,"Racing Club","Athletico PR","mitad",1.42],[11930,"Cruzeiro MG","Libertad Asuncion","mitad",1.45],[12030,"Atlético Nacional","CD Junior","mitad",1.45],[11900,"Blooming","Always Ready","mitad",1.533],"164,619"],
+	[[11530,"Club Nueve de Octubre","Vargas Torres","over",2.35],"47,000"],
 ]
