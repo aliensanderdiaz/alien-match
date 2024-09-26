77ae0402ee72d649d75506fdd9d3764709ea9826
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-09-26 12:54:04 -0500

    lñ

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,4 +1,7 @@
 let APUESTAS_ABIERTAS = [
-	[[10700,"Kifisias","Lamia","over",2.15],[10800,"Cerrito CS","Colon FC de Uruguay","over",2.6],[10900,"Spartak Kostroma","FK Yenisey","over",2.1],[11100,"Larissa","Panseraikos","over",2.15],[11130,"Monza","Brescia","over",2.1],[11200,"Las Palmas","Real Betis","over",2],"1,060,032"],
-	[[11200,"Las Palmas","Real Betis","mitad",1.42],[10700,"Kifisias","Lamia","mitad",1.48],[10800,"Cerrito CS","Colon FC de Uruguay","mitad",1.55],[10900,"Spartak Kostroma","FK Yenisey","mitad",1.45],[11100,"Larissa","Panseraikos","mitad",1.48],[11130,"Monza","Brescia","mitad",1.45],"101,363"],
+	[[11500,"Mushuc Runa","Libertad (Ecu)","over",2],[11530,"Club Nueve de Octubre","Vargas Torres","over",2.35],"47,000"],
+	[[11530,"Club Nueve de Octubre","Vargas Torres","mitad",1.45],[11600,"Club Leones Del Norte","Manta FC","mitad",1.65],"23,925"],
+	[[11400,"Roma","Athletic Bilbao","visitante",2.45],[11400,"Lyon","Olympiacos","local",1.75],[11400,"Ajax Amsterdam","Besiktas","visitante",2.1],[11400,"Celta de Vigo","Atlético Madrid","visitante",1.8],"162,068"],
+	[[11400,"Roma","Athletic Bilbao","over",2.15],"21,500"],
+	[[11400,"Roma","Athletic Bilbao","mitad",1.48],"14,800"],
 ]
