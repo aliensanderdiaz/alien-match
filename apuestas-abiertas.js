33d029db97aa8e93e7d12d5f0cc14dd624d923e1
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-09-29 10:57:00 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,8 +1,11 @@
 let APUESTAS_ABIERTAS = [
+	[[11100,"Bodrum Belediyesi Bodrumspor","Adana Demirspor","local",1.769],[11130,"KAA Gent","OH Leuven","local",1.75],[11100,"Empoli","Fiorentina","visitante",2],[11130,"Real Oviedo","SD Eibar","visitante",2.625],"113,784"],
+	[[11100,"Empoli","Fiorentina","over",2.1],[11130,"CF Cordoba","Ferrol","over",2.1],[11130,"Real Oviedo","SD Eibar","over",2.5],"77,175"],
+	[[11100,"Empoli","Fiorentina","mitad",1.45],[11130,"CF Cordoba","Ferrol","mitad",1.444],[11130,"Real Oviedo","SD Eibar","mitad",1.55],"22,725"],
+	[[11100,"Empoli","Fiorentina","mitad",1.44],"11,269"],
 	[[11030,"GKS Piast Gliwice","Jagiellonia Bialystok","visitante",2.55],[11030,"Nublense","Union La Calera","visitante",2.75],"70,125"],
 	[[11030,"Uruguay Montevideo FC","Cooper","over",2.35],"23,500"],
 	[[11030,"Uruguay Montevideo FC","Cooper","mitad",1.48],"14,800"],
 	[[10930,"FC Famalicao","Nacional Madeira","over",2.05],[10930,"Uniao Leiria","Maritimo","over",2.05],"42,025"],
-	[[10915,"Burgos CF","Granada","over",2.1],"21,000"],
 	[[10915,"Athletic Bilbao","Sevilla","over",2.04],"20,500"],
 ]
