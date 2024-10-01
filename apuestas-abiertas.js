d8fc6f734c9caa5bd6b70ddb816a56506f1c5d30
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-09-30 22:48:46 -0500

    t

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,7 +1,5 @@
 let APUESTAS_ABIERTAS = [
-	[[11345,"EA Guingamp","Caen","local",1.9],[11345,"Parma","Cagliari","local",1.769],[11330,"Brescia","Cremonese","local",2.25],[11415,"Avs Futebol Sad","Sporting Farense","local",1.909],"144,393"],
-	[[11330,"Brescia","Cremonese","mitad",1.48],[11345,"EA Guingamp","Caen","mitad",1.45],[11415,"Avs Futebol Sad","Sporting Farense","mitad",1.42],"30,473"],
-	[[11230,"FC Sudtirol","Palermo","visitante",2.25],"22,500"],
-	[[11230,"FC Sudtirol","Palermo","mitad",1.5],[11230,"Institucion Atletica Sud America","Plaza Colonia","mitad",1.571],"23,571"],
-	[[11230,"FC Sudtirol","Palermo","mitad",1.5],[11230,"Institucion Atletica Sud America","Plaza Colonia","mitad",1.533],"76,590"],
+	[[11145,"FC Salzburg","Brest","visitante",2.45],[11145,"Stuttgart","AC Sparta Prague","visitante",2.45],"60,025"],
+	[[10800,"Albion FC","Cerrito CS","over",2.55],[10815,"Rubin Kazan","Fakel Voronezh","over",2],"51,000"],
+	[[10800,"Albion FC","Cerrito CS","mitad",1.53],"15,333"],
 ]
