a5dede9e56de8ff4c6029f5f33842124dc1d01ce
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-10-06 10:49:54 -0500

    600

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,8 +1,5 @@
 let APUESTAS_ABIERTAS = [
-	[[11430,"Atletico MG","EC Vitoria","local",1.95],[11500,"Ituano SP","Guarani SP","local",2.1],[11500,"Ponte Preta SP","Botafogo SP","local",2.15],[11430,"Bragantino SP","Palmeiras SP","visitante",1.85],[11430,"Athletico PR","Botafogo RJ","visitante",2.05],"333,901"],
-	[[11430,"Bragantino SP","Palmeiras SP","over",2],[11430,"Athletico PR","Botafogo RJ","over",2],[11430,"Atletico MG","EC Vitoria","over",2.45],[11500,"Ituano SP","Guarani SP","over",2.55],[11500,"Ponte Preta SP","Botafogo SP","over",2.6],[11500,"Rayo Zuliano","Carabobo FC","over",2.35],"1,526,889"],
-	[[11430,"Athletico PR","Botafogo RJ","mitad",1.42],[11430,"Atletico MG","EC Vitoria","mitad",1.533],[11500,"Ituano SP","Guarani SP","mitad",1.571],[11500,"Ponte Preta SP","Botafogo SP","mitad",1.571],[11500,"Rayo Zuliano","Carabobo FC","mitad",1.533],[11500,"Agropecuario Argentino","Guillermo Brown","mitad",1.55],"127,786"],
-	[[11400,"Envigado FC","Atletico Bucaramanga","visitante",2.37],"23,750"],
-	[[11400,"Envigado FC","Atletico Bucaramanga","over",2.9],[11400,"CD Tenerife","Cartagena","over",2.55],"73,950"],
-	[[11400,"Envigado FC","Atletico Bucaramanga","mitad",1.727],[11400,"CD Tenerife","Cartagena","mitad",1.55],"26,773"],
+	[[11130,"Levante","Real Oviedo","local",1.909],[11130,"Slavia Prague","AC Sparta Prague","local",1.8],[11130,"Sevilla","Real Betis","visitante",2.25],[11100,"Monza","Roma","visitante",1.95],[11215,"St Truiden","KAA Gent","visitante",1.9],[11230,"Panathinaikos","Olympiacos","visitante",2.1],"601,574"],
+	[[11100,"Monza","Roma","mitad",1.48],[11130,"Sevilla","Real Betis","mitad",1.45],[11130,"Levante","Real Oviedo","mitad",1.444],[11100,"Istanbulspor AS","Kocaelispor","mitad",1.45],[11230,"Levadiakos","PAOK Thessaloniki","mitad",1.42],"63,824"],
+	[[11030,"Pogon Szczecin","GKS Piast Gliwice","mitad",1.42],[11015,"FC Botosani","Rapid Bucharest","mitad",1.42],[11030,"Palestino","Coquimbo","mitad",1.48],"43,106"],
 ]
