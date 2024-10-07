618330db3c788312b381ca4dcf29460aa00c6c21
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-10-07 07:05:48 -0500

    lunw

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,5 +1,4 @@
 let APUESTAS_ABIERTAS = [
-	[[11130,"Levante","Real Oviedo","local",1.909],[11130,"Slavia Prague","AC Sparta Prague","local",1.8],[11130,"Sevilla","Real Betis","visitante",2.25],[11100,"Monza","Roma","visitante",1.95],[11215,"St Truiden","KAA Gent","visitante",1.9],[11230,"Panathinaikos","Olympiacos","visitante",2.1],"601,574"],
-	[[11100,"Monza","Roma","mitad",1.48],[11130,"Sevilla","Real Betis","mitad",1.45],[11130,"Levante","Real Oviedo","mitad",1.444],[11100,"Istanbulspor AS","Kocaelispor","mitad",1.45],[11230,"Levadiakos","PAOK Thessaloniki","mitad",1.42],"63,824"],
-	[[11030,"Pogon Szczecin","GKS Piast Gliwice","mitad",1.42],[11015,"FC Botosani","Rapid Bucharest","mitad",1.42],[11030,"Palestino","Coquimbo","mitad",1.48],"43,106"],
+	[[11145,"Juventud de Las Piedras","CA Atenas De San Carlos","over",2.65],"26,500"],
+	[[11145,"Juventud de Las Piedras","CA Atenas De San Carlos","mitad",1.55],"15,500"],
 ]
