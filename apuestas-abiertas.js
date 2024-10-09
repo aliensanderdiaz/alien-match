11099295cdbc8e34993d3e286ae8bbcbb3243911
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-10-09 12:54:24 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,9 +1,6 @@
 let APUESTAS_ABIERTAS = [
-	[[11900,"Sport Recife PE","Ceara CE","local",1.85],[11900,"Atletico Tucuman","CA Tigre","visitante",2.75],[11900,"Goias GO","Santos SP","visitante",2.3],[12000,"Deportivo Pereira","Tolima","visitante",2.4],"280,830"],
-	[[11900,"Atletico Tucuman","CA Tigre","over",2.35],[11900,"Sport Recife PE","Ceara CE","over",2.25],[11900,"Goias GO","Santos SP","over",2.65],[12000,"Deportivo Pereira","Tolima","over",2.8],"392,333"],
-	[[11900,"Atletico Tucuman","CA Tigre","mitad",1.5],[11900,"Sport Recife PE","Ceara CE","mitad",1.48],[11900,"Goias GO","Santos SP","mitad",1.6],[12000,"Deportivo Pereira","Tolima","mitad",1.65],[11905,"Deportivo Moron","Gimnasia Y Esgrima Mendoza","mitad",1.615],"94,674"],
-	[[11800,"Fortaleza FC","Jaguares De Córdoba","local",1.85],"18,500"],
-	[[11800,"Fortaleza FC","Jaguares De Córdoba","over",2.3],[11810,"Ferro Carril Oeste","Nueva Chicago","over",2.3],"52,900"],
-	[[11810,"Ferro Carril Oeste","Nueva Chicago","mitad",1.5],"15,000"],
-	[[11645,"Defensa Y Justicia","Rosario Central","over",2.7],"75,600"],
+	[[11600,"Envigado FC","Deportivo Pasto","local",3.2],"32,000"],
+	[[11600,"Envigado FC","Deportivo Pasto","visitante",2.2],"22,000"],
+	[[11400,"Puerto Montt","Huachipato","mitad",1.533],[11500,"CD Real Santander","Cucuta Deportivo","mitad",1.42],[11530,"Club Nueve de Octubre","San Antonio","mitad",1.48],[11530,"Chacaritas SC","Guayaquil City FC","mitad",1.42],[11600,"Envigado FC","Deportivo Pasto","mitad",1.615],[11600,"Coquimbo","Universidad de Chile","mitad",1.615],"119,406"],
+	[[11400,"Puerto Montt","Huachipato","over",2.4],[11500,"CD Real Santander","Cucuta Deportivo","over",2.1],[11530,"Club Nueve de Octubre","San Antonio","over",2.35],[11530,"Chacaritas SC","Guayaquil City FC","over",2.2],[11600,"Envigado FC","Deportivo Pasto","over",2.85],[11600,"Coquimbo","Universidad de Chile","over",2.7],"2,005,071"],
 ]
