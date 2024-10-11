fe4ab6ba581d7666e682e20e39ee4394a7d6526a
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-10-11 06:41:47 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,5 +1,5 @@
 let APUESTAS_ABIERTAS = [
-	[[10430,"Syria","Tayikistan","local",1.95],[10800,"Mozambique","Eswatini","local",1.769],[10800,"Guinea Ecuatorial","Liberia","visitante",3.65],[10900,"Madagascar","Gambia","visitante",2.4],"302,220"],
-	[[10430,"Syria","Tayikistan","over",2.15],[10800,"Guinea Ecuatorial","Liberia","over",2.45],[10800,"Mozambique","Eswatini","over",2.45],[10800,"Zambia","Chad","over",2.25],[10900,"Madagascar","Gambia","over",2.55],"740,446"],
-	[[10430,"Syria","Tayikistan","mitad",1.444],[10800,"Guinea Ecuatorial","Liberia","mitad",1.533],[10800,"Mozambique","Eswatini","mitad",1.533],[10800,"Zambia","Chad","mitad",1.48],[10900,"Madagascar","Gambia","mitad",1.6],"80,418"],
+	[[10800,"Guinea Ecuatorial","Liberia","over",2.45],[10800,"Mozambique","Eswatini","over",2.55],[10800,"Zambia","Chad","over",2.1],[10900,"Madagascar","Gambia","over",2.55],"334,554"],
+	[[10800,"Mozambique","Eswatini","local",1.769],[10800,"Guinea Ecuatorial","Liberia","visitante",3.65],[10900,"Madagascar","Gambia","visitante",2.4],"302,220"],
+	[[10800,"Guinea Ecuatorial","Liberia","mitad",1.533],[10800,"Mozambique","Eswatini","mitad",1.533],[10800,"Zambia","Chad","mitad",1.48],[10900,"Madagascar","Gambia","mitad",1.6],"80,418"],
 ]
