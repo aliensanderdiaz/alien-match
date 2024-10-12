af50f11fb4ce9fe6ea386f369cd7751b744f5a13
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-10-12 16:16:57 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,5 +1,6 @@
 let APUESTAS_ABIERTAS = [
-	[[10630,"Chesterfield","Notts County","visitante",1.95],"19,500"],
-	[[10800,"Lituania","Kosovo","mitad",1.55],[10800,"Alverca","FC Penafiel","mitad",1.45],"32,589"],
-	[[10800,"Lituania","Kosovo","mitad",1.55],[10900,"Tranmere","Bradford City","mitad",1.42],[10915,"Cartagena","Ferrol","mitad",1.571],[10800,"Alverca","FC Penafiel","mitad",1.45],"72,719"],
+	[[11630,"Santos SP","Mirassol SP","local",1.75],"17,500"],
+	[[11630,"Santos SP","Mirassol SP","mitad",1.48],"14,800"],
+	[[11630,"Santos SP","Mirassol SP","local",1.75],[11730,"Dominica","Bermuda","local",2.5],[11800,"Olimpia Asuncion","Sol de America","local",1.909],[11745,"La Equidad","CD Junior","visitante",2.7],[11800,"CD Everton Vina Del Mar","Union La Calera","visitante",2.875],[11800,"Montevideo Wanderers","Atletico River Plate (Uru)","visitante",2.45],"1,588,446"],
+	[[11630,"Santos SP","Mirassol SP","mitad",1.48],[11745,"La Equidad","CD Junior","mitad",1.65],[11800,"Montevideo Wanderers","Atletico River Plate (Uru)","mitad",1.42],[11800,"CD Everton Vina Del Mar","Union La Calera","mitad",1.42],[11800,"Talleres de Remedios de Escalada","Gimnasia Jujuy","mitad",1.85],[11800,"Pittsburgh Riverhounds","Charleston Battery","mitad",1.42],[11800,"Olimpia Asuncion","Sol de America","mitad",1.5],"194,032"],
 ]
