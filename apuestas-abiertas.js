7bfd32cb57256fd279bd1f7a96ff4b113c1ec535
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-10-17 21:20:19 -0500

    s

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,8 +1,2 @@
 let APUESTAS_ABIERTAS = [
-	[[11800,"Flamengo RJ","Fluminense RJ","local",1.93],[11800,"America Mineiro MG","Goias GO","local",1.769],"34,146"],
-	[[11800,"Flamengo RJ","Fluminense RJ","over",2.625],[11800,"America Mineiro MG","Goias GO","over",2.375],[11800,"Sport Huancayo","Atlético Grau","over",2.25],"140,273"],
-	[[11800,"Flamengo RJ","Fluminense RJ","mitad",1.571],[11800,"America Mineiro MG","Goias GO","mitad",1.5],[11800,"Sport Huancayo","Atlético Grau","mitad",1.5],"35,357"],
-	[[11800,"Flamengo RJ","Fluminense RJ","local",1.93],[11800,"America Mineiro MG","Goias GO","local",1.769],[11840,"América de Cali","Deportivo Cali","local",1.93],[12000,"Atletico Morelia","Mineros de Zacatecas","visitante",2.05],[12040,"Deportivo Pasto","Atletico Bucaramanga","visitante",2.95],"398,543"],
-	[[11800,"Flamengo RJ","Fluminense RJ","over",2.625],[11800,"America Mineiro MG","Goias GO","over",2.4],[11800,"Sport Huancayo","Atlético Grau","over",2.25],[11840,"América de Cali","Deportivo Cali","over",2.9],"411,075"],
-	[[11800,"Flamengo RJ","Fluminense RJ","mitad",1.571],[11800,"America Mineiro MG","Goias GO","mitad",1.5],[11840,"América de Cali","Deportivo Cali","mitad",1.615],[12000,"Atletico Morelia","Mineros de Zacatecas","mitad",1.45],[12040,"Deportivo Pasto","Atletico Bucaramanga","mitad",1.7],[11800,"Sport Huancayo","Atlético Grau","mitad",1.5],"140,789"],
 ]
