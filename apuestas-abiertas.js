8cfe8a96cc3fbb5611dd4cfee24ad3a993299851
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-10-20 11:01:30 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,8 +1,3 @@
 let APUESTAS_ABIERTAS = [
-	[[11910,"Tolima","Alianza FC","local",1.727],[11915,"Union Santa Fe","Huracan","visitante",3.25],"56,136"],
-	[[11910,"Tolima","Alianza FC","over",2.2],[11915,"Union Santa Fe","Huracan","over",3.9],"85,800"],
-	[[11910,"Tolima","Alianza FC","mitad",1.45],[11915,"Union Santa Fe","Huracan","mitad",1.85],"26,825"],
-	[[11910,"Tolima","Alianza FC","local",1.727],[11915,"Union Santa Fe","Huracan","visitante",3.25],[12000,"Houston Dynamo","L.A. Galaxy","visitante",1.85],[12000,"Austin FC","Colorado Rapids","visitante",1.93],[12006,"FC Juarez","Leon","visitante",2.05],"410,892"],
-	[[11910,"Tolima","Alianza FC","over",2.2],[11915,"Union Santa Fe","Huracan","over",3.85],[12000,"El Paso Locomotive FC","Sacramento Republic FC","over",2],"169,400"],
-	[[11910,"Tolima","Alianza FC","mitad",1.45],[11915,"Union Santa Fe","Huracan","mitad",1.833],[12000,"El Paso Locomotive FC","Sacramento Republic FC","mitad",1.42],"37,748"],
+	[[10800,"Lecce","Fiorentina","mitad",1.45],[10800,"Juve Stabia","Cremonese","mitad",1.45],[10800,"Reggiana","Frosinone","mitad",1.5],[10800,"Cooper","Colon FC de Uruguay","mitad",1.533],"46,525"],
 ]
