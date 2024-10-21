27f6cda80df2b56c00b68a53e3a2eb10090c91a9
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-10-20 21:47:38 -0500

    lunes

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,4 +1,5 @@
 let APUESTAS_ABIERTAS = [
-	[[11600,"Patronato Parana","San Martin de San Juan","over",2.7],[11630,"Ponte Preta SP","Guarani SP","over",2.85],[11700,"Barcelona SC","Club Sport Emelec","over",2.55],[11730,"San Lorenzo","CA Barracas Central","over",2.55],[11730,"Godoy Cruz","Newells Old Boys","over",2.8],[11730,"Sport Recife PE","Botafogo SP","over",2.25],[11730,"Temperley","Nueva Chicago","over",2.8],"8,826,480"],
-	[[11630,"Ponte Preta SP","Guarani SP","mitad",1.65],[11700,"Barcelona SC","Club Sport Emelec","mitad",1.55],[11730,"San Lorenzo","CA Barracas Central","mitad",1.55],[11730,"Godoy Cruz","Newells Old Boys","mitad",1.615],[11730,"Sport Recife PE","Botafogo SP","mitad",1.48],[11600,"Patronato Parana","San Martin de San Juan","mitad",1.6],[11730,"Temperley","Nueva Chicago","mitad",1.615],"244,952"],
+	[[11030,"CS Municipal Studentesc Iasi","Unirea Slobozia","local",2],[11200,"Bodrum Belediyesi Bodrumspor","Caykur Rizespor","local",2.1],[11200,"Radomiak Radom","Puszcza Niepolomice","local",1.8],"75,600"],
+	[[11000,"Rukh Vynnyky","Karpaty LVIV","over",2.05],[11030,"CS Municipal Studentesc Iasi","Unirea Slobozia","over",2.35],[11200,"Bodrum Belediyesi Bodrumspor","Caykur Rizespor","over",2.2],"105,985"],
+	[[11200,"Bodrum Belediyesi Bodrumspor","Caykur Rizespor","mitad",1.48],[11000,"Rukh Vynnyky","Karpaty LVIV","mitad",1.42],[11030,"CS Municipal Studentesc Iasi","Unirea Slobozia","mitad",1.5],"31,524"],
 ]
