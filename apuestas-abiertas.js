c7b478fd6547b5ec40e6e538deaf8f98e3c18a3f
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-10-22 22:08:23 -0500

    w

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,7 +1,2 @@
 let APUESTAS_ABIERTAS = [
-	[[11930,"Novorizontino SP","Avai SC","local",2],[11930,"Vila Nova FC","Amazonas FC","local",2.05],[12000,"Jaiba Brava","Dorados","local",1.8],[11930,"Atletico MG","River Plate","visitante",2.9],"214,020"],
-	[[11930,"Atletico MG","River Plate","over",3],[11930,"Novorizontino SP","Avai SC","over",2.7],[11930,"Vila Nova FC","Amazonas FC","over",2.4],"194,400"],
-	[[11930,"Atletico MG","River Plate","mitad",1.666],[11930,"Novorizontino SP","Avai SC","mitad",1.571],[11930,"Vila Nova FC","Amazonas FC","mitad",1.5],"39,286"],
-	[[11830,"CF Montreal","Atlanta United","local",2.04],"20,500"],
-	[[11800,"Llaneros FC","Atletico Huila","over",2.5],[11800,"Real Soacha Cundinamarca","Cucuta Deportivo","over",2.35],"135,125"],
 ]
