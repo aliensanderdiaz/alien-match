77e9accc9d4b7c4e7c6cc1e6b3fc494fbd717213
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-10-28 12:39:00 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,5 +1,9 @@
 let APUESTAS_ABIERTAS = [
-	[[10900,"Istanbul Basaksehir FK","Eyupspor","local",1.909],[11100,"FC Botosani","Universitatea Cluj","visitante",2.2],"42,000"],
+	[[11300,"CD Mafra","Uniao Leiria","over",2.5],"25,000"],
+	[[11300,"CD Mafra","Uniao Leiria","mitad",1.55],"15,500"],
+	[[11300,"CD Mafra","Uniao Leiria","local",2.54],"25,500"],
+	[[11300,"CD Mafra","Uniao Leiria","local",2.5],[11400,"Farul Constanta","CS U Craiova","visitante",1.95],[11430,"Ferrol","CD Tenerife","visitante",2.8],[11500,"Imbabura SC","Tecnico Universitario","visitante",2.25],[11500,"RCD Mallorca","Athletic Bilbao","visitante",2.55],[11500,"Blackpool","Wigan","visitante",2.375],"1,860,026"],
+	[[11430,"Ferrol","CD Tenerife","over",3],[11500,"RCD Mallorca","Athletic Bilbao","over",2.6],[11500,"Imbabura SC","Tecnico Universitario","over",2.2],[11300,"CD Mafra","Uniao Leiria","over",2.4],"411,840"],
+	[[11430,"Ferrol","CD Tenerife","mitad",1.7],[11500,"RCD Mallorca","Athletic Bilbao","mitad",1.6],[11500,"Imbabura SC","Tecnico Universitario","mitad",1.48],[11300,"CD Mafra","Uniao Leiria","mitad",1.533],"61,726"],
 	[[11100,"FC Botosani","Universitatea Cluj","over",2.5],"25,000"],
-	[[11100,"FC Botosani","Universitatea Cluj","mitad",1.55],"15,500"],
 ]
