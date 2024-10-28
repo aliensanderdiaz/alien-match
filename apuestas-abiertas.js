c4ac45157c3cbec0a1c20260e600597a95b07221
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-10-27 22:38:39 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,6 +1,5 @@
 let APUESTAS_ABIERTAS = [
-	[[11820,"CD Junior","América de Cali","visitante",2.45],"24,500"],
-	[[11820,"CD Junior","América de Cali","over",2.5],[11830,"Jorge Wilstermann","CA Nacional Potosi","over",2.05],[11830,"Deportivo La Guaira","Monagas SC","over",2],"102,500"],
-	[[11820,"CD Junior","América de Cali","mitad",1.571],[11830,"Jorge Wilstermann","CA Nacional Potosi","mitad",1.42],[11830,"Deportivo La Guaira","Monagas SC","mitad",1.42],"31,686"],
-	[[11700,"Oaxaca","CD Tepatitlan de Morelos","visitante",1.95],"42,900"],
+	[[10900,"Istanbul Basaksehir FK","Eyupspor","local",1.909],[11100,"FC Botosani","Universitatea Cluj","visitante",2.2],"42,000"],
+	[[11100,"FC Botosani","Universitatea Cluj","over",2.5],"25,000"],
+	[[11100,"FC Botosani","Universitatea Cluj","mitad",1.55],"15,500"],
 ]
