df11b1c13ad75a78ea17d82f44ab55ab3eed10c2
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-11-01 16:51:01 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,11 +1,5 @@
 let APUESTAS_ABIERTAS = [
-	[[11500,"Lille","Lyon","visitante",1.95],[11400,"Clermont Foot","Lorient","visitante",1.8],[11400,"FC Martigues","Red Star Paris","visitante",2.05],[11445,"Raith Rovers","Ayr United","visitante",2.25],[11500,"Luton Town","West Bromwich","visitante",2.05],[11300,"Alverca","Feirense","visitante",2.4],"796,542"],
-	[[11300,"FC Viktoria Köln","VfB Stuttgart II","local",1.8],[11400,"Troyes","Laval","local",2],[11400,"Dunkerque","SC Amiens","local",2.1],[11400,"Annecy FC","Pau FC","local",2.05],[11400,"FC Eindhoven","ADO Den Haag","local",2.25],[11500,"CD Alaves","RCD Mallorca","visitante",2.75],"958,939"],
-	[[11530,"Orense SC","Delfin","over",2.3],[11300,"Alverca","Feirense","over",2.15],[11330,"FC Schaffhausen","AC Bellinzona","over",2],[11430,"Montevideo City Torque","Tacuarembo","over",2.1],[11445,"Raith Rovers","Ayr United","over",2.25],"467,303"],
-	[[11400,"Annecy FC","Pau FC","over",2],[11400,"Dunkerque","SC Amiens","over",2.1],[11400,"FC Martigues","Red Star Paris","over",2.1],[11400,"Troyes","Laval","over",2.15],[11500,"CD Alaves","RCD Mallorca","over",2.7],"512,001"],
-	[[11330,"FC Schaffhausen","AC Bellinzona","mitad",1.42],[11445,"Raith Rovers","Ayr United","mitad",1.5],"21,300"],
-	[[11400,"Annecy FC","Pau FC","mitad",1.42],[11400,"Dunkerque","SC Amiens","mitad",1.45],[11400,"FC Martigues","Red Star Paris","mitad",1.45],[11400,"Troyes","Laval","mitad",1.48],[11500,"CD Alaves","RCD Mallorca","mitad",1.615],[11530,"Orense SC","Delfin","mitad",1.48],[11300,"Alverca","Feirense","mitad",1.45],"153,176"],
-	[[11230,"SSV Ulm 1846","Schalke 04","visitante",2.15],"21,500"],
-	[[11200,"Gaziantep BB","Goztepe","visitante",2.15],[11230,"SC Preussen Münster","Fortuna Dusseldorf","visitante",1.769],[11230,"SSV Ulm 1846","Schalke 04","visitante",2.1],"79,881"],
-	[[11200,"Boluspor","Ümraniyespor","over",2.15],"21,500"],
+	[[11700,"Central Cordoba","San Lorenzo","visitante",3.25],[11700,"Jaguares De Córdoba","Deportivo Pereira","visitante",2.55],[11700,"Deportivo Maldonado","Rampla Juniors","visitante",2.45],"203,044"],
+	[[11700,"Central Cordoba","San Lorenzo","over",3.25],[11700,"Jaguares De Córdoba","Deportivo Pereira","over",2.7],[11700,"Deportivo Maldonado","Rampla Juniors","over",2.3],"201,825"],
+	[[11700,"Central Cordoba","San Lorenzo","mitad",1.727],[11700,"Jaguares De Córdoba","Deportivo Pereira","mitad",1.6],[11700,"Deportivo Maldonado","Rampla Juniors","mitad",1.5],"41,455"],
 ]
