41bdca005344341445ed6f418fad90ec6e71ebc0
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-11-23 08:00:51 -0500

    s

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,9 +1,6 @@
 let APUESTAS_ABIERTAS = [
+	[[10800,"Dynamo Dresden","1.FC Saarbrücken","local",1.75],[10800,"Hansa Rostock","Arminia Bielefeld","visitante",2.2],[10800,"TSV Alemannia Aachen","TSV 1860 München","visitante",2.1],[10800,"Rot Weiss Essen","SV Sandhausen","visitante",1.95],[10800,"Valencia","Real Betis","visitante",2.45],[10800,"Gaziantep BB","Istanbul Basaksehir FK","visitante",2.15],"830,461"],
+	[[10800,"Valencia","Real Betis","over",2.45],[10800,"Hansa Rostock","Arminia Bielefeld","over",2],[10800,"Boluspor","Sanliurfaspor","over",2.1],"102,900"],
+	[[10800,"Valencia","Real Betis","mitad",1.55],[10800,"Hansa Rostock","Arminia Bielefeld","mitad",1.444],[10800,"TSV Alemannia Aachen","TSV 1860 München","mitad",1.42],[10800,"Boluspor","Sanliurfaspor","mitad",1.45],"46,099"],
 	[[10730,"Stevenage","Leyton Orient","visitante",2.6],[10730,"Grimsby Town","Colchester","visitante",2.15],[10730,"FK Baumit Jablonec","Banik Ostrava","visitante",2],"111,800"],
-	[[10700,"Hannover 96","SV Darmstadt 98","local",1.75],[10700,"Fortuna Dusseldorf","SV Elversberg","local",1.8],[10730,"Sheffield Wednesday","Cardiff City","local",1.909],[10700,"Greuther Furth","Karlsruher SC","visitante",1.769],[10730,"Bristol City","Burnley","visitante",2.2],[10730,"Coventry","Sheffield United","visitante",2.2],"514,952"],
-	[[10730,"Bristol City","Burnley","over",2.15],[10730,"Sheffield Wednesday","Cardiff City","over",2],[10730,"Coventry","Sheffield United","over",2],[10730,"Stevenage","Leyton Orient","over",2.8],[10700,"SC Lusitania Angra","Sao Joao Ver","over",2.2],[10730,"Rochdale","Oldham","over",2],"1,059,520"],
-	[[10730,"Bristol City","Burnley","mitad",1.45],[10730,"Sheffield Wednesday","Cardiff City","mitad",1.42],[10730,"Coventry","Sheffield United","mitad",1.42],[10730,"Stevenage","Leyton Orient","mitad",1.615],[10700,"SC Lusitania Angra","Sao Joao Ver","mitad",1.48],[10730,"Rochdale","Oldham","mitad",1.42],"99,259"],
-	[[10600,"Portimonense SC","Benfica B","visitante",2.15],"21.500"],
-	[[10600,"Vorskla Poltava","LNZ Cherkasy","over",2.35],"23.500"],
-	[[10600,"Vorskla Poltava","LNZ Cherkasy","mitad",1.53],"15.333"],
 ]
