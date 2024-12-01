6d97ba8f84b7cb504401ae2b65aada35d1a278a9
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-12-01 16:07:48 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,9 +1,4 @@
 let APUESTAS_ABIERTAS = [
-	[[10800,"Caykur Rizespor","Kayserispor","local",1.75],[10745,"Racing Club Montevideo","Atletico River Plate (Uru)","local",2],[10730,"SSV Ulm 1846","Greuther Furth","visitante",2.1],[10730,"Karlsruher SC","Hamburger SV","visitante",1.833],[10730,"Nurnberg","Fortuna Dusseldorf","visitante",1.95],[10730,"VfL Osnabruck","TSV Alemannia Aachen","visitante",2.1],[10800,"CD Castellon","Malaga","visitante",2.5],"1,379,503"],
-	[[10745,"Racing Club Montevideo","Atletico River Plate (Uru)","over",2.2],[10800,"Isloch","FC Vitebsk","over",2.15],"47,300"],
-	[[10745,"Racing Club Montevideo","Atletico River Plate (Uru)","mitad",1.48],[10800,"Isloch","FC Vitebsk","mitad",1.48],"21,904"],
-	[[10600,"FC Penafiel","CD Mafra","local",2],[10630,"Udinese","Génova","local",1.95],[10000,"Daegu FC","Chungnam Asan FC","visitante",2.4],[10200,"Seoul E-Land","Jeonbuk Hyundai Motors","visitante",1.9],[10615,"Korona Kielce","Gornik Zabrze","visitante",2.25],"400,140"],
-	[[10615,"Korona Kielce","Gornik Zabrze","over",2.1],[10530,"Ümraniyespor","Genclerbirligi","over",2],[10600,"FC Oleksandriya","Chernomorets Odessa","over",2.1],[10630,"Udinese","Génova","over",2.2],[10600,"FC Penafiel","CD Mafra","over",2.2],"426,888"],
-	[[10615,"Korona Kielce","Gornik Zabrze","mitad",1.45],[10630,"Udinese","Génova","mitad",1.48],[10530,"Ümraniyespor","Genclerbirligi","mitad",1.444],[10600,"FC Penafiel","CD Mafra","mitad",1.48],[10600,"FC Oleksandriya","Chernomorets Odessa","mitad",1.45],"66,521"],
-	[[12200,"L.A. Galaxy","Seattle Sounders","local",1.76],"17.692"],
+	[[11630,"Athletico PR","Fluminense RJ","local",2.15],[11600,"América de Cali","Tolima","visitante",2.8],[11630,"Bragantino SP","Cruzeiro MG","visitante",2.65],[11630,"EC Vitoria","Fortaleza CE","visitante",2.625],"418,766"],
+	[[11600,"América de Cali","Tolima","mitad",1.7],[11630,"Bragantino SP","Cruzeiro MG","mitad",1.48],[11630,"EC Vitoria","Fortaleza CE","mitad",1.533],[11630,"Athletico PR","Fluminense RJ","mitad",1.55],[11630,"Real Tomayapo","Jorge Wilstermann","mitad",1.45],"86,706"],
 ]
