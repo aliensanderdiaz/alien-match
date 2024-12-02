ec4431250abcbd7778b29ccd0196845a19da6d82
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-12-01 19:34:28 -0500

    m2

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,4 +1,2 @@
 let APUESTAS_ABIERTAS = [
-	[[11630,"Athletico PR","Fluminense RJ","local",2.15],[11600,"América de Cali","Tolima","visitante",2.8],[11630,"Bragantino SP","Cruzeiro MG","visitante",2.65],[11630,"EC Vitoria","Fortaleza CE","visitante",2.625],"418,766"],
-	[[11600,"América de Cali","Tolima","mitad",1.7],[11630,"Bragantino SP","Cruzeiro MG","mitad",1.48],[11630,"EC Vitoria","Fortaleza CE","mitad",1.533],[11630,"Athletico PR","Fluminense RJ","mitad",1.55],[11630,"Real Tomayapo","Jorge Wilstermann","mitad",1.45],"86,706"],
 ]
