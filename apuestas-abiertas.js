14e02602f089286c7ce4e75e00fb544600f0028f
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-12-05 17:39:22 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,6 +1,8 @@
 let APUESTAS_ABIERTAS = [
-	[[11300,"Orihuela CF","Getafe","mitad",1.45],"14.500"],
-	[[11430,"Fulham","Brighton","visitante",1.95],[11500,"Lazio","Napoles","visitante",2.3],[11515,"Bournemouth","Tottenham","visitante",1.8],"80,730"],
-	[[11300,"Orihuela CF","Getafe","over",2.15],[11400,"SD Ponferradina","CD Castellon","over",2.15],[11400,"Andorra CF","Cartagena","over",2.3],[11400,"Barakaldo CF","Ferrol","over",2.5],[11400,"UD Marbella","Burgos CF","over",2.35],[11500,"Lazio","Napoles","over",2.1],"1,311,692"],
-	[[11300,"Orihuela CF","Getafe","mitad",1.45],[11400,"SD Ponferradina","CD Castellon","mitad",1.48],[11400,"Andorra CF","Cartagena","mitad",1.5],[11400,"Barakaldo CF","Ferrol","mitad",1.571],[11400,"UD Marbella","Burgos CF","mitad",1.5],[11500,"Lazio","Napoles","mitad",1.45],"110,021"],
+	[[11800,"Athletico PR","Bragantino SP","local",1.95],[11830,"Atlético Nacional","Deportivo Pasto","visitante",3.2],"62,400"],
+	[[11800,"Athletico PR","Bragantino SP","over",2.05],[11800,"Fluminense RJ","Cuiaba MT","over",2.25],[11830,"Atlético Nacional","Deportivo Pasto","over",2.2],"101,475"],
+	[[11800,"Athletico PR","Bragantino SP","mitad",1.42],[11800,"Fluminense RJ","Cuiaba MT","mitad",1.45],[11830,"Atlético Nacional","Deportivo Pasto","mitad",1.45],"29,856"],
+	[[11800,"Athletico PR","Bragantino SP","local",1.95],[12030,"Millonarios","Independiente Santa Fe","local",2.15],[12100,"America","Cruz Azul","local",2.45],[11830,"Atlético Nacional","Deportivo Pasto","visitante",3.2],"328,692"],
+	[[11800,"Athletico PR","Bragantino SP","over",2.05],[11800,"Fluminense RJ","Cuiaba MT","over",2.25],[11830,"Atlético Nacional","Deportivo Pasto","over",2.2],[12030,"Millonarios","Independiente Santa Fe","over",2.875],[12100,"America","Cruz Azul","over",2.55],"743,939"],
+	[[11800,"Athletico PR","Bragantino SP","mitad",1.42],[11800,"Fluminense RJ","Cuiaba MT","mitad",1.48],[11830,"Atlético Nacional","Deportivo Pasto","mitad",1.48],[12030,"Millonarios","Independiente Santa Fe","mitad",1.65],[12100,"America","Cruz Azul","mitad",1.571],"80,647"],
 ]
