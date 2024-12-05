e8bca3e0aa7c21e0ba884cc37f50b786b30a4258
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-12-05 12:46:54 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,6 +1,6 @@
 let APUESTAS_ABIERTAS = [
-	[[11130,"FK Mlada Boleslav","Slovan Liberec","local",1.95],"19.500"],
-	[[10530,"Karaman Belediyespor","Esenler Erokspor","over",2.05],[10530,"Ankara Demirspor","Kastamonuspor","over",2.05],[11000,"Panionios","Panseraikos","over",2.3],"77,548"],
-	[[10530,"Karaman Belediyespor","Esenler Erokspor","mitad",1.45],[10530,"Ankara Demirspor","Kastamonuspor","mitad",1.444],[11000,"Panionios","Panseraikos","mitad",1.5],"31,417"],
-	[[12200,"Atletico San Luis","Monterrey","over",3.4],"34.000"],
+	[[11300,"Orihuela CF","Getafe","mitad",1.45],"14.500"],
+	[[11430,"Fulham","Brighton","visitante",1.95],[11500,"Lazio","Napoles","visitante",2.3],[11515,"Bournemouth","Tottenham","visitante",1.8],"80,730"],
+	[[11300,"Orihuela CF","Getafe","over",2.15],[11400,"SD Ponferradina","CD Castellon","over",2.15],[11400,"Andorra CF","Cartagena","over",2.3],[11400,"Barakaldo CF","Ferrol","over",2.5],[11400,"UD Marbella","Burgos CF","over",2.35],[11500,"Lazio","Napoles","over",2.1],"1,311,692"],
+	[[11300,"Orihuela CF","Getafe","mitad",1.45],[11400,"SD Ponferradina","CD Castellon","mitad",1.48],[11400,"Andorra CF","Cartagena","mitad",1.5],[11400,"Barakaldo CF","Ferrol","mitad",1.571],[11400,"UD Marbella","Burgos CF","mitad",1.5],[11500,"Lazio","Napoles","mitad",1.45],"110,021"],
 ]
