798bcec34c26a0cc5b11c017d255e426edba3470
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-12-04 23:15:59 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,5 +1,6 @@
 let APUESTAS_ABIERTAS = [
-	[[11200,"Korona Kielce","Widzew Lodz","local",2.1],[11130,"Dukla Praha","FC Slovacko","visitante",2.15],[11130,"FK Pardubice","Sigma Olomouc","visitante",2.05],"92,558"],
-	[[10830,"FC Oleksandriya","Dynamo Kiev","over",2.25],[11100,"Rukh Vynnyky","Polissya Zhytomyr","over",2.8],[11130,"FK Pardubice","Sigma Olomouc","over",2.1],[11130,"Dukla Praha","FC Slovacko","over",2.35],"310,905"],
-	[[10830,"FC Oleksandriya","Dynamo Kiev","mitad",1.5],[11130,"FK Pardubice","Sigma Olomouc","mitad",1.45],[11130,"Dukla Praha","FC Slovacko","mitad",1.533],[11200,"Korona Kielce","Widzew Lodz","mitad",1.42],"47,357"],
+	[[11130,"FK Mlada Boleslav","Slovan Liberec","local",1.95],"19.500"],
+	[[10530,"Karaman Belediyespor","Esenler Erokspor","over",2.05],[10530,"Ankara Demirspor","Kastamonuspor","over",2.05],[11000,"Panionios","Panseraikos","over",2.3],"77,548"],
+	[[10530,"Karaman Belediyespor","Esenler Erokspor","mitad",1.45],[10530,"Ankara Demirspor","Kastamonuspor","mitad",1.444],[11000,"Panionios","Panseraikos","mitad",1.5],"31,417"],
+	[[12200,"Atletico San Luis","Monterrey","over",3.4],"34.000"],
 ]
