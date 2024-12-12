7b8643b9ff67c9297c8c28db468be60941654fa0
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-12-12 06:51:07 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,10 +1,5 @@
 let APUESTAS_ABIERTAS = [
-	[[11910,"Velez Sarsfield","Central Cordoba","over",3],"30.000"],
-	[[11910,"Velez Sarsfield","Central Cordoba","local",2.15],"21.500"],
-	[[11910,"Velez Sarsfield","Central Cordoba","mitad",1.65],"16.500"],
-	[[11445,"Millwall","Sheffield United","visitante",2.25],[11445,"Cardiff City","Preston","visitante",2.5],[11445,"Hull City","Watford","visitante",2.15],"120,938"],
-	[[11445,"QPR","Oxford United","local",2],[11500,"Juventus","Manchester City","local",2.4],[11500,"Borussia Dortmund","Barcelona","local",1.85],[11500,"West Bromwich","Coventry","local",2.1],"186,480"],
-	[[11445,"Millwall","Sheffield United","visitante",2.35],[11445,"Cardiff City","Preston","visitante",2.4],[11445,"Hull City","Watford","visitante",2.2],[11445,"QPR","Oxford United","local",1.95],[11500,"West Bromwich","Coventry","local",2.1],[11500,"Juventus","Manchester City","local",2.6],[11500,"Borussia Dortmund","Barcelona","local",1.85],"2,443,998"],
-	[[11445,"Millwall","Sheffield United","over",2.5],[11445,"Cardiff City","Preston","over",2.15],[11445,"QPR","Oxford United","over",2.1],[11500,"Juventus","Manchester City","over",2],[11500,"West Bromwich","Coventry","over",2.1],"474,075"],
-	[[11445,"Millwall","Sheffield United","mitad",1.55],[11445,"Cardiff City","Preston","mitad",1.48],[11445,"QPR","Oxford United","mitad",1.444],[11500,"West Bromwich","Coventry","mitad",1.45],"48,047"],
+	[[10800,"Vikingur Reykjavik","Djurgardens IF","visitante",1.95],"19.500"],
+	[[11245,"NK Olimpija Ljubljana","Cercle Brugge","over",2.1],[11245,"FC Noah","Apoel Nicosia","over",2.2],[11245,"Dinamo Minsk","Larne","over",2.1],[11245,"St. Gilloise","Nice","over",2],"194,040"],
+	[[11245,"St. Gilloise","Nice","mitad",1.42],[11245,"Dinamo Minsk","Larne","mitad",1.42],[11245,"FC Noah","Apoel Nicosia","mitad",1.48],[11245,"NK Olimpija Ljubljana","Cercle Brugge","mitad",1.45],"43,272"],
 ]
--- ligas.js
+++ ligas.js
@@ -1,13 +1,12 @@
 let LIGAS = [
-	{"liga":"ZA-ARGC","mitad":true,"mitadFavorito":true,"codigoWplay":19572},
 	{"liga":"ZA-BOL1","mitad":false,"mitadFavorito":false,"codigoWplay":19786},
-	{"liga":"U-CHAM","mitad":true,"mitadFavorito":true,"codigoWplay":19161},
+	{"liga":"ZA-COLC","mitad":true,"mitadFavorito":true,"codigoWplay":29698},
 	{"liga":"U-EURO","mitad":true,"mitadFavorito":true,"codigoWplay":19162},
-	{"liga":"R-ING2","mitad":true,"mitadFavorito":true,"codigoWplay":19156},
+	{"liga":"U-CONF","mitad":true,"mitadFavorito":true,"codigoWplay":413776},
+	{"liga":"ZA-MEX1","mitad":true,"mitadFavorito":true,"codigoWplay":19329},
 	{"liga":"I-AMI","mitad":true,"mitadFavorito":true,"codigoWplay":19287},
 	{"liga":"I-AMI23","mitad":false,"mitadFavorito":false,"codigoWplay":20182},
 	{"liga":"I-AMI21","mitad":false,"mitadFavorito":false,"codigoWplay":19345},
 	{"liga":"I-OLIH","codigoWplay":45631,"mitad":true,"mitadFavorito":true},
-	{"liga":"AMIE","codigoWplay":617783,"mitad":true,"mitadFavorito":true},
-	{"liga":"AMIC","codigoWplay":20183,"mitad":true,"mitadFavorito":true},
+	{"liga":"ZA-URU2","mitad":false,"mitadFavorito":false,"codigoWplay":26834},
 ]
