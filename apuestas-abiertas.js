c07b2e8df2a0e2e8f7b096792aea4b3bb925b10d
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-12-12 12:55:50 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,5 +1,12 @@
 let APUESTAS_ABIERTAS = [
-	[[10800,"Vikingur Reykjavik","Djurgardens IF","visitante",1.95],"19.500"],
+	[[11500,"Bodo Glimt","Besiktas","visitante",2.35],[11500,"Slavia Prague","Anderlecht","visitante",3],[11500,"Ajax Amsterdam","Lazio","visitante",2.05],"144,525"],
+	[[11500,"Shamrock Rovers","Borac Banja Luka","local",2.3],[11500,"FK Mlada Boleslav","Jagiellonia Bialystok","visitante",1.909],[11500,"AC Omonia Nicosia","Rapid Vienna","visitante",1.95],[11500,"FC St. Gallen","Vitoria Guimaraes","visitante",1.85],[11500,"IF Elfsborg","Qarabag FK","visitante",1.85],"293,044"],
+	[[11500,"Shamrock Rovers","Borac Banja Luka","local",2.3],[11500,"FK Mlada Boleslav","Jagiellonia Bialystok","visitante",1.909],[11500,"AC Omonia Nicosia","Rapid Vienna","visitante",1.95],[11500,"FC St. Gallen","Vitoria Guimaraes","visitante",1.85],[11500,"IF Elfsborg","Qarabag FK","visitante",1.85],[11500,"Ajax Amsterdam","Lazio","visitante",2.05],[11500,"Bodo Glimt","Besiktas","visitante",2.35],[11500,"Slavia Prague","Anderlecht","visitante",3],"4,235,215"],
+	[[11445,"Uruguay Montevideo FC","Juventud de Las Piedras","over",2.3],[11500,"Shamrock Rovers","Borac Banja Luka","over",2.45],"56,350"],
+	[[11500,"Shamrock Rovers","Borac Banja Luka","mitad",1.533],[11445,"Uruguay Montevideo FC","Juventud de Las Piedras","mitad",1.45],"22,233"],
+	[[11245,"Roma","Sporting Braga","visitante",2.5],[11245,"Hoffenheim","FCSB","visitante",2.75],[11245,"Ludogorets","AZ Alkmaar","visitante",2.15],[11245,"St. Gilloise","Nice","visitante",2.6],"384,313"],
+	[[11245,"Dinamo Minsk","Larne","local",1.75],[11245,"PAOK Thessaloniki","Ferencvarosi TC","local",1.769],[11245,"NK Olimpija Ljubljana","Cercle Brugge","local",1.95],[11245,"Istanbul Basaksehir FK","1. FC Heidenheim","local",1.75],[11245,"HJK Helsinki","Molde","visitante",1.75],[11245,"FC Noah","Apoel Nicosia","visitante",2.25],"416,021"],
+	[[11245,"Dinamo Minsk","Larne","local",1.75],[11245,"PAOK Thessaloniki","Ferencvarosi TC","local",1.769],[11245,"NK Olimpija Ljubljana","Cercle Brugge","local",1.95],[11245,"Istanbul Basaksehir FK","1. FC Heidenheim","local",1.75],[11245,"HJK Helsinki","Molde","visitante",1.75],[11245,"FC Noah","Apoel Nicosia","visitante",2.25],[11245,"Roma","Sporting Braga","visitante",2.5],[11245,"Hoffenheim","FCSB","visitante",2.75],[11245,"Ludogorets","AZ Alkmaar","visitante",2.15],[11245,"St. Gilloise","Nice","visitante",2.6],"15,988,226"],
 	[[11245,"NK Olimpija Ljubljana","Cercle Brugge","over",2.1],[11245,"FC Noah","Apoel Nicosia","over",2.2],[11245,"Dinamo Minsk","Larne","over",2.1],[11245,"St. Gilloise","Nice","over",2],"194,040"],
-	[[11245,"St. Gilloise","Nice","mitad",1.42],[11245,"Dinamo Minsk","Larne","mitad",1.42],[11245,"FC Noah","Apoel Nicosia","mitad",1.48],[11245,"NK Olimpija Ljubljana","Cercle Brugge","mitad",1.45],"43,272"],
+	[[11245,"St. Gilloise","Nice","mitad",1.42],[11245,"Dinamo Minsk","Larne","mitad",1.42],[11245,"FC Noah","Apoel Nicosia","mitad",1.48],"43,272"],
 ]
