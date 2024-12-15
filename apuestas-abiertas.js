9af0dfec80617e565d1ec4e7c0f42bc9e35843e6
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-12-14 19:23:33 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,7 +1,7 @@
 let APUESTAS_ABIERTAS = [
+	[[10615,"Utrecht","GO Ahead Eagles","local",1.769],[10630,"Lecce","Monza","local",2.2],[10530,"Hatayspor","Goztepe","visitante",2.05],[10600,"FC Felgueiras 1932","FC Vizela","visitante",2.3],"185,523"],
+	[[10630,"Lecce","Monza","mitad",1.533],[10530,"Bandirmaspor","Genclerbirligi","mitad",1.42],[10600,"FC Felgueiras 1932","FC Vizela","mitad",1.42],[10600,"FC Ingulets","Kolos Kovalyovka","mitad",1.6],"49,469"],
 	[[11915,"Racing Club","River Plate","visitante",1.85],"18.500"],
 	[[11930,"Llaneros FC","Union Magdalena","over",2.6],"26.000"],
 	[[11930,"Llaneros FC","Union Magdalena","mitad",1.57],"15.714"],
-	[[11715,"Boca Juniors","CA Independiente","over",2.7],"27.000"],
-	[[11715,"Boca Juniors","CA Independiente","over",2.7],[11930,"Llaneros FC","Union Magdalena","over",2.625],"70,875"],
 ]
