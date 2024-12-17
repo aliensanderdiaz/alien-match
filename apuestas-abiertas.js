076753d44d7e50950edfe8c41c44fabdc1ec0941
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-12-17 07:07:04 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,9 +1,6 @@
 let APUESTAS_ABIERTAS = [
-	[[11445,"Dunkerque","Caen","local",1.95],[11500,"Fleetwood Town","Accrington","local",1.769],[11445,"Lazio","Inter","visitante",1.95],[11500,"Bournemouth","West Ham United","visitante",2.3],"117,426"],
-	[[11445,"Lazio","Inter","mitad",1.42],[11445,"Dunkerque","Caen","mitad",1.42],"20,164"],
-	[[11445,"Dunkerque","Caen","local",1.95],[11500,"Fleetwood Town","Accrington","local",1.769],[11445,"Lazio","Inter","visitante",1.95],[11500,"Bournemouth","West Ham United","visitante",2.3],[11545,"Rio Ave","Vitoria Guimaraes","visitante",1.909],"295,398"],
-	[[11445,"Lazio","Inter","over",2],[11445,"Dunkerque","Caen","over",2],[11545,"Rio Ave","Vitoria Guimaraes","over",2.1],"84,000"],
-	[[11445,"Lazio","Inter","mitad",1.42],[11445,"Dunkerque","Caen","mitad",1.42],[11545,"Rio Ave","Vitoria Guimaraes","mitad",1.45],"29,238"],
-	[[11000,"Qatar Sub 23","Arabia Saudí Sub 23","over",2.15],"21.500"],
-	[[11000,"Qatar Sub 23","Arabia Saudí Sub 23","mitad",1.42],"14.200"],
+	[[11300,"Albacete Balompie","Levante","visitante",2.15],"21.500"],
+	[[11515,"Real Zaragoza","Real Oviedo","local",2.25],[11300,"Albacete Balompie","Levante","visitante",2.15],[11515,"CF Cordoba","SD Eibar","visitante",2.55],"123,356"],
+	[[11515,"Real Zaragoza","Real Oviedo","over",2.65],[11515,"CF Cordoba","SD Eibar","over",2.2],"58,300"],
+	[[11515,"Real Zaragoza","Real Oviedo","mitad",1.6],[11515,"CF Cordoba","SD Eibar","mitad",1.48],"23,680"],
 ]
