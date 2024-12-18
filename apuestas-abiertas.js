e7a3954bd3bf851322f6599b49a8096395988b61
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-12-18 13:44:24 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,4 +1,12 @@
 let APUESTAS_ABIERTAS = [
-	[[10500,"Kastamonuspor","Bodrum Belediyesi Bodrumspor","over",2.04],"20.500"],
-	[[10500,"Kastamonuspor","Bodrum Belediyesi Bodrumspor","mitad",1.42],"14.200"],
+	[[11515,"Malaga","CD Eldense","over",2.7],[11515,"Mirandes","Sporting Gijon","over",3.1],[11530,"Espanyol","Valencia","over",2.65],"221,805"],
+	[[11500,"AS Monaco","PSG","local",2.15],[11515,"Malaga","CD Eldense","local",2.3],[11530,"Espanyol","Valencia","local",2.5],[11530,"Villarreal","Rayo Vallecano","local",1.75],[11515,"Mirandes","Sporting Gijon","visitante",2.9],"627,397"],
+	[[11515,"Malaga","CD Eldense","mitad",1.615],[11515,"Mirandes","Sporting Gijon","mitad",1.7],[11530,"Espanyol","Valencia","mitad",1.615],"44,361"],
+	[[11400,"AEK Athens","PAOK Thessaloniki","over",2.15],"21.500"],
+	[[11400,"AEK Athens","PAOK Thessaloniki","local",2],"20.000"],
+	[[11400,"AEK Athens","PAOK Thessaloniki","mitad",1.48],"14.800"],
+	[[11515,"Mirandes","Sporting Gijon","visitante",2.9],[11400,"AEK Athens","PAOK Thessaloniki","local",2],[11500,"AS Monaco","PSG","local",2.1],[11515,"Malaga","CD Eldense","local",2.3],[11530,"Espanyol","Valencia","local",2.4],[11530,"Villarreal","Rayo Vallecano","local",1.75],"1,176,588"],
+	[[11515,"Malaga","CD Eldense","over",2.7],[11515,"Mirandes","Sporting Gijon","over",3.1],[11530,"Espanyol","Valencia","over",2.55],[11400,"AEK Athens","PAOK Thessaloniki","over",2.15],"458,885"],
+	[[11515,"Malaga","CD Eldense","mitad",1.615],[11515,"Mirandes","Sporting Gijon","mitad",1.7],[11530,"Espanyol","Valencia","mitad",1.6],[11400,"AEK Athens","PAOK Thessaloniki","mitad",1.48],"65,029"],
+	[[11300,"Ferrol","Almeria","over",2.04],"20.500"],
 ]
