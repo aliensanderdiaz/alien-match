699ce3d7db85f8f2c0223a4fc06996615b6a49d1
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-12-20 07:22:09 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,2 +1,5 @@
 let APUESTAS_ABIERTAS = [
+	[[11200,"Sepsi OSK","Universitatea Cluj","visitante",2.25],[11230,"Fortuna Dusseldorf","1. FC Magdeburg","local",1.769],[11300,"Leixoes SC","CD Mafra","local",2],"79,615"],
+	[[11200,"Sepsi OSK","Universitatea Cluj","over",2.5],[11300,"Leixoes SC","CD Mafra","over",2.25],"56,250"],
+	[[11200,"Sepsi OSK","Universitatea Cluj","mitad",1.55],[11300,"Leixoes SC","CD Mafra","mitad",1.5],"23,250"],
 ]
