08f701542aee193702fd6ba409f4871da6112c94
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-12-21 15:02:45 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,9 +1,7 @@
 let APUESTAS_ABIERTAS = [
+	[[11445,"Beerschot","Kortrijk","over",2.1],"21.000"],
+	[[11445,"Beerschot","Kortrijk","mitad",1.44],"14.444"],
+	[[11445,"Beerschot","Kortrijk","local",2.05],[11500,"Heracles Almelo","Groningen","local",2],[11500,"Barcelona","Atlético Madrid","visitante",2.1],"86,100"],
 	[[11445,"Beerschot","Kortrijk","over",2],[11530,"Moreirense","FC Porto","over",2],[11900,"Velez Sarsfield","Estudiantes de La Plata","over",2.85],"114,000"],
 	[[11445,"Beerschot","Kortrijk","mitad",1.42],[11900,"Velez Sarsfield","Estudiantes de La Plata","mitad",1.615],"22,938"],
-	[[11445,"Beerschot","Kortrijk","local",2.1],[11500,"Heracles Almelo","Groningen","local",1.95],[11400,"AZ Alkmaar","Twente","local",1.769],[11900,"Velez Sarsfield","Estudiantes de La Plata","local",2.3],[11500,"Barcelona","Atlético Madrid","visitante",2.1],"349,933"],
-	[[11300,"Boavista","Avs Futebol Sad","over",2.4],"24.000"],
-	[[11300,"Boavista","Avs Futebol Sad","local",2.2],"22.000"],
-	[[11300,"Boavista","Avs Futebol Sad","mitad",1.53],"15.333"],
-	[[11230,"Osasuna","Athletic Bilbao","over",2.45],[11230,"Real Zaragoza","Ferrol","over",2.2],[11230,"Sporting Gijon","Malaga","over",2.35],[11245,"Almere City FC","Heerenveen","over",2],[11300,"Boavista","Avs Futebol Sad","over",2.4],"1,367,982"],
 ]
