fca98e22ea5608ce46c5745d01679012658d92ca
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-12-22 08:57:23 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,5 +1,13 @@
 let APUESTAS_ABIERTAS = [
-	[[10600,"Pacos Ferreira","Alverca","local",2.1],[10630,"Roma","Parma","visitante",2.55],[10530,"Gaziantep BB","Antalyaspor","visitante",2.2],"253,292"],
-	[[10530,"Petrolul Ploiesti","Unirea Slobozia","over",2.6],[10600,"Pacos Ferreira","Alverca","over",2.1],"54,600"],
-	[[10600,"Pacos Ferreira","Alverca","mitad",1.45],"22,786"],
+	[[10900,"Juve Stabia","Cesena","local",2.25],[10900,"Leicester City","Wolverhampton Wanderers","local",2],[10930,"VfL Bochum","1. FC Heidenheim","local",1.769],[10900,"Venezia","Cagliari","visitante",2.1],[10900,"Oliveirense","Uniao Leiria","visitante",1.93],"322,681"],
+	[[10900,"Juve Stabia","Cesena","over",2.45],[10900,"Oliveirense","Uniao Leiria","over",2.05],"50,225"],
+	[[10900,"Juve Stabia","Cesena","mitad",1.55],[10900,"Oliveirense","Uniao Leiria","mitad",1.42],"22,010"],
+	[[10830,"MVV Maastricht","SC Telstar","local",2],"20.000"],
+	[[10830,"Panseraikos","Panetolikos FC","over",2.2],"22.000"],
+	[[10830,"Panseraikos","Panetolikos FC","visitante",2.4],"24.000"],
+	[[10830,"Panseraikos","Panetolikos FC","mitad",1.5],"15.000"],
+	[[10930,"VfL Bochum","1. FC Heidenheim","local",1.8],[10900,"Juve Stabia","Cesena","local",2.3],[10900,"Leicester City","Wolverhampton Wanderers","local",2],[10830,"Panseraikos","Panetolikos FC","visitante",2.45],[10900,"Venezia","Cagliari","visitante",2.1],[10830,"MVV Maastricht","SC Telstar","local",1.95],[10900,"Oliveirense","Uniao Leiria","visitante",1.93],"1,603,274"],
+	[[10900,"Juve Stabia","Cesena","over",2.5],[10830,"Panseraikos","Panetolikos FC","over",2.25],[10900,"Oliveirense","Uniao Leiria","over",2.05],"115,313"],
+	[[10900,"Juve Stabia","Cesena","mitad",1.571],[10900,"Oliveirense","Uniao Leiria","mitad",1.42],[10830,"Panseraikos","Panetolikos FC","mitad",1.5],"33,471"],
+	[[10800,"Valencia","CD Alaves","over",2.5],[10800,"Burgos CF","CD Tenerife","over",2.55],[10800,"CFR Cluj","FC Otelul Galati","over",2.6],[10800,"Boluspor","Kocaelispor","over",2.1],"348,075"],
 ]
