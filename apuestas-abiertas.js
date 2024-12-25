cb542a4a50071b8f08ffa4eaeb7df82cf7c6a606
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-12-25 09:21:10 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,2 +1,8 @@
 let APUESTAS_ABIERTAS = [
+	[[10730,"Hearts","Hibernian","visitante",2.2],[10730,"Antwerp","Genk","visitante",1.95],"42,900"],
+	[[10630,"SC Pisa","Sassuolo","visitante",2],"20.000"],
+	[[10630,"SC Pisa","Sassuolo","visitante",2],[10730,"Hearts","Hibernian","visitante",2.2],[10730,"Antwerp","Genk","visitante",1.95],"85,800"],
+	[[10900,"Brescia","Modena FC","local",2.35],[10630,"SC Pisa","Sassuolo","visitante",2],[10730,"Hearts","Hibernian","visitante",2.2],[10730,"Antwerp","Genk","visitante",1.95],[10900,"Cesena","Cremonese","visitante",2.2],[10900,"Cosenza","Catanzaro","visitante",2.375],[10900,"Frosinone","Salernitana","visitante",2.45],[10900,"Reggiana","Juve Stabia","visitante",2.45],[10900,"FC Sudtirol","Cittadella","visitante",2.8],"17,706,456"],
+	[[10900,"FC Sudtirol","Cittadella","over",2.625],[10900,"Brescia","Modena FC","over",2.45],[10900,"Cesena","Cremonese","over",2.2],[10900,"Cosenza","Catanzaro","over",2.2],[10900,"Frosinone","Salernitana","over",2.2],[10900,"Reggiana","Juve Stabia","over",2.5],"1,711,999"],
+	[[10900,"FC Sudtirol","Cittadella","mitad",1.6],[10900,"Reggiana","Juve Stabia","mitad",1.55],[10900,"Frosinone","Salernitana","mitad",1.48],[10900,"Cosenza","Catanzaro","mitad",1.48],[10900,"Cesena","Cremonese","mitad",1.48],[10900,"Brescia","Modena FC","mitad",1.533],"123,275"],
 ]
