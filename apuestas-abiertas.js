2ca3de1d365a61ad234562195c8fed8745965825
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-12-26 18:07:49 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,5 +1,6 @@
 let APUESTAS_ABIERTAS = [
-	[[11430,"Sampdoria","Carrarese Calcio","local",2.15],[11230,"Derby County","West Bromwich","visitante",2.4],[11230,"Mechelen","Standard Liege","visitante",2.6],[11445,"KAA Gent","St. Gilloise","visitante",1.85],"248,196"],
-	[[11230,"Derby County","West Bromwich","over",2.5],[11430,"Sampdoria","Carrarese Calcio","over",2.4],"60,000"],
-	[[11430,"Sampdoria","Carrarese Calcio","mitad",1.533],"24,095"],
+	[[11515,"Arsenal","Ipswich Town","visitante",4],[11430,"Brighton","Brentford","visitante",2.25],[11445,"Anderlecht","FCV Dender EH","visitante",2.95],[11230,"OH Leuven","Beerschot","visitante",2.6],"690,300"],
+	[[11445,"Dunfermline","Falkirk","visitante",1.85],[11445,"St Truiden","Cercle Brugge","visitante",2],[11515,"Arouca","Gil Vicente","visitante",2.55],"94,350"],
+	[[11445,"Dunfermline","Falkirk","over",2.05],[11515,"Arouca","Gil Vicente","over",2.375],"48,688"],
+	[[11515,"Arouca","Gil Vicente","mitad",1.533],[11445,"Dunfermline","Falkirk","mitad",1.42],"21,773"],
 ]
--- ligas.js
+++ ligas.js
@@ -1,10 +1,8 @@
 let LIGAS = [
 	{"liga":"YE-BEL1","mitad":true,"mitadFavorito":true,"codigoWplay":19372},
-	{"liga":"R-ESC1","mitad":true,"mitadFavorito":true,"codigoWplay":19208},
+	{"liga":"R-ESC2","mitad":false,"mitadFavorito":true,"codigoWplay":19879},
 	{"liga":"R-ING1","mitad":true,"mitadFavorito":true,"codigoWplay":19157},
-	{"liga":"R-ING2","mitad":true,"mitadFavorito":true,"codigoWplay":19156},
-	{"liga":"R-INGO","mitad":true,"mitadFavorito":true,"codigoWplay":19326},
-	{"liga":"R-INGT","mitad":true,"mitadFavorito":true,"codigoWplay":19300},
-	{"liga":"R-INGN","mitad":false,"mitadFavorito":false,"codigoWplay":19374},
-	{"liga":"ITA2","mitad":true,"mitadFavorito":true,"codigoWplay":19328},
+	{"liga":"AMIE","codigoWplay":617783,"mitad":true,"mitadFavorito":true},
+	{"liga":"AMIC","codigoWplay":20183,"mitad":true,"mitadFavorito":true},
+	{"liga":"YE-POR1","mitad":true,"mitadFavorito":true,"codigoWplay":19211},
 ]
