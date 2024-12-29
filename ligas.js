1ac5a3e3d3017cf91ca53a138b90cf49fca7648c
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-12-29 18:47:46 -0500

    m

--- ligas.js
+++ ligas.js
@@ -1,13 +1,8 @@
 let LIGAS = [
-	{"liga":"R-ESC1","mitad":true,"mitadFavorito":true,"codigoWplay":19208},
 	{"liga":"R-ING1","mitad":true,"mitadFavorito":true,"codigoWplay":19157},
-	{"liga":"R-ING2","mitad":true,"mitadFavorito":true,"codigoWplay":19156},
-	{"liga":"R-INGO","mitad":true,"mitadFavorito":true,"codigoWplay":19326},
 	{"liga":"R-INGT","mitad":true,"mitadFavorito":true,"codigoWplay":19300},
 	{"liga":"ITA1","mitad":true,"mitadFavorito":true,"codigoWplay":19159},
-	{"liga":"ITA2","mitad":true,"mitadFavorito":true,"codigoWplay":19328},
 	{"liga":"AMIE","codigoWplay":617783,"mitad":true,"mitadFavorito":true},
 	{"liga":"AMIC","codigoWplay":20183,"mitad":true,"mitadFavorito":true},
 	{"liga":"YE-POR1","mitad":true,"mitadFavorito":true,"codigoWplay":19211},
-	{"liga":"YE-POR2","mitad":false,"mitadFavorito":true,"codigoWplay":19338},
 ]
