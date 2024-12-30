263a98e87956bf7eed8477be9c38762a9be97d5f
aliensanderdiaz <aliensanderdiaz@gmail.com>
2024-12-30 13:23:16 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,9 +1,5 @@
 let APUESTAS_ABIERTAS = [
-	[[11230,"Como","Lecce","over",2.04],"20.500"],
-	[[11230,"Como","Lecce","visitante",2.85],"28.500"],
-	[[11230,"Como","Lecce","mitad",1.42],"14.200"],
-	[[10900,"Botafogo PB","Serra Branca PB","over",2],"20.000"],
-	[[11500,"Manchester United","Newcastle","local",1.9],[11230,"Como","Lecce","visitante",2.85],[11445,"Aston Villa","Brighton","visitante",2.15],[11500,"AFC Wimbledon","Gillingham","visitante",3],[11515,"FC Famalicao","CD Santa Clara","visitante",2.55],"890,632"],
-	[[10900,"Botafogo PB","Serra Branca PB","over",2],[11230,"Como","Lecce","over",2.05],[11500,"AFC Wimbledon","Gillingham","over",2.25],[11515,"FC Famalicao","CD Santa Clara","over",2.7],"249,075"],
-	[[11230,"Como","Lecce","mitad",1.42],[11500,"AFC Wimbledon","Gillingham","mitad",1.48],[11515,"FC Famalicao","CD Santa Clara","mitad",1.615],"33,949"],
+	[[11500,"Manchester United","Newcastle","local",1.95],[11445,"Aston Villa","Brighton","visitante",2.1],[11500,"AFC Wimbledon","Gillingham","visitante",3.1],[11515,"FC Famalicao","CD Santa Clara","visitante",2.45],"311,015"],
+	[[11445,"Bologna","Verona","over",2],[11500,"AFC Wimbledon","Gillingham","over",2.45],[11515,"FC Famalicao","CD Santa Clara","over",2.8],"137,200"],
+	[[11500,"AFC Wimbledon","Gillingham","mitad",1.533],[11515,"FC Famalicao","CD Santa Clara","mitad",1.65],"25,300"],
 ]
