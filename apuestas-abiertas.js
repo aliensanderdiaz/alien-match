a0fcf0cb65aca146f8f1b77a5fc11014b0a6b82e
aliensanderdiaz <aliensanderdiaz@gmail.com>
2025-01-02 11:38:40 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,5 +1,7 @@
 let APUESTAS_ABIERTAS = [
-	[[11500,"Sunderland","Sheffield United","over",2.2],"22.000"],
-	[[11500,"Sunderland","Sheffield United","local",2.15],"21.500"],
-	[[11500,"Sunderland","Sheffield United","mitad",1.48],"14.800"],
+	[[11445,"Newport","AFC Wimbledon","over",2.05],[11445,"Gillingham","Bromley FC","over",2.15],"44,075"],
+	[[11400,"Inter","Atalanta","local",1.833],[11445,"Newport","AFC Wimbledon","visitante",1.9],[11445,"Gillingham","Bromley FC","visitante",2.375],"82,729"],
+	[[11445,"Newport","AFC Wimbledon","mitad",1.42],[11445,"Gillingham","Bromley FC","mitad",1.48],"21,016"],
+	[[11230,"Dundee","Dundee United","visitante",1.93],"19.300"],
+	[[11000,"Kilmarnock","St Mirren","over",2.1],"21.000"],
 ]
