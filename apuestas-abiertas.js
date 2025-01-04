4f40712829bad9f5d62c06f297eeeb03e6ede7d4
aliensanderdiaz <aliensanderdiaz@gmail.com>
2025-01-04 07:44:05 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,8 +1,8 @@
 let APUESTAS_ABIERTAS = [
-	[[10600,"Portimonense SC","Alverca","visitante",2.15],"21.500"],
-	[[10530,"Sanliurfaspor","Corum Belediyespor","over",2.05],[10600,"Portimonense SC","Alverca","over",2.05],"42,025"],
-	[[10530,"Sanliurfaspor","Corum Belediyespor","mitad",1.42],[10600,"Portimonense SC","Alverca","mitad",1.45],"20,590"],
-	[[10730,"Tottenham","Newcastle","local",1.85],[10730,"Burton Albion","Northampton","local",2],[10600,"Portimonense SC","Alverca","visitante",2.15],[10730,"Swansea","West Bromwich","visitante",2.15],[10730,"Blackburn Rovers","Burnley","visitante",2.2],[10730,"Cambridge United","Bristol Rovers","visitante",2.45],"921,865"],
-	[[10730,"Swansea","West Bromwich","over",2],[10730,"Blackburn Rovers","Burnley","over",2.45],[10730,"Burton Albion","Northampton","over",2.1],[10530,"Sanliurfaspor","Corum Belediyespor","over",2.05],[10600,"Portimonense SC","Alverca","over",2.05],"432,437"],
-	[[10730,"Swansea","West Bromwich","mitad",1.42],[10730,"Blackburn Rovers","Burnley","mitad",1.55],[10730,"Burton Albion","Northampton","mitad",1.45],[10530,"Sanliurfaspor","Corum Belediyespor","mitad",1.42],[10600,"Portimonense SC","Alverca","mitad",1.45],"65,712"],
+	[[10800,"Antalyaspor","Eyupspor","local",2.1],[10800,"EA Guingamp","Dunkerque","local",2],[10800,"FC Metz","Pau FC","local",1.769],[10800,"Bradford City","Grimsby Town","local",1.8],"133,754"],
+	[[10800,"EA Guingamp","Dunkerque","over",2],"20.000"],
+	[[10800,"EA Guingamp","Dunkerque","mitad",1.42],"14.200"],
+	[[10730,"Burton Albion","Northampton","local",2.1],[10730,"Swansea","West Bromwich","visitante",2.375],[10730,"Blackburn Rovers","Burnley","visitante",2.3],[10730,"Cambridge United","Bristol Rovers","visitante",2.55],"536,281"],
+	[[10730,"Swansea","West Bromwich","over",2.1],[10730,"Blackburn Rovers","Burnley","over",2.7],[10730,"Burton Albion","Northampton","over",2.25],[10730,"Cambridge United","Bristol Rovers","over",2.1],"267,908"],
+	[[10730,"Swansea","West Bromwich","mitad",1.45],[10730,"Blackburn Rovers","Burnley","mitad",1.6],[10730,"Burton Albion","Northampton","mitad",1.48],[10730,"Cambridge United","Bristol Rovers","mitad",1.444],"49,596"],
 ]
