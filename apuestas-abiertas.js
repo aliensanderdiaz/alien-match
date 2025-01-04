19ca5c5e5338c2ab7a5a94c868f79a0e14dad475
aliensanderdiaz <aliensanderdiaz@gmail.com>
2025-01-03 19:44:06 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,10 +1,8 @@
 let APUESTAS_ABIERTAS = [
-	[[11500,"Nice","Rennes","local",2.05],[11515,"Vitoria Guimaraes","Sporting De Lisboa","visitante",1.8],"36,900"],
-	[[11500,"Nice","Rennes","over",2.04],"20.500"],
-	[[11500,"Nice","Rennes","mitad",1.44],"14.444"],
-	[[11400,"Caen","Clermont Foot","local",1.95],[11400,"Grenoble","SC Bastia","visitante",2.15],"508,445"],
-	[[11400,"Juventus","AC Milán","over",2.2],[11400,"Ajaccio","Annecy FC","over",2.65],[11400,"Grenoble","SC Bastia","over",2.45],[11400,"Caen","Clermont Foot","over",2],[11400,"SC Amiens","Troyes","over",2.25],"642,758"],
-	[[11400,"Caen","Clermont Foot","mitad",1.42],"76,308"],
-	[[11300,"Granada","Getafe","over",2.625],[11300,"Pontevedra CF","RCD Mallorca","over",2.35],"144,966"],
-	[[11300,"Granada","Getafe","over",2.7],[11300,"Pontevedra CF","RCD Mallorca","over",2.2],"600,935"],
+	[[10600,"Portimonense SC","Alverca","visitante",2.15],"21.500"],
+	[[10530,"Sanliurfaspor","Corum Belediyespor","over",2.05],[10600,"Portimonense SC","Alverca","over",2.05],"42,025"],
+	[[10530,"Sanliurfaspor","Corum Belediyespor","mitad",1.42],[10600,"Portimonense SC","Alverca","mitad",1.45],"20,590"],
+	[[10730,"Tottenham","Newcastle","local",1.85],[10730,"Burton Albion","Northampton","local",2],[10600,"Portimonense SC","Alverca","visitante",2.15],[10730,"Swansea","West Bromwich","visitante",2.15],[10730,"Blackburn Rovers","Burnley","visitante",2.2],[10730,"Cambridge United","Bristol Rovers","visitante",2.45],"921,865"],
+	[[10730,"Swansea","West Bromwich","over",2],[10730,"Blackburn Rovers","Burnley","over",2.45],[10730,"Burton Albion","Northampton","over",2.1],[10530,"Sanliurfaspor","Corum Belediyespor","over",2.05],[10600,"Portimonense SC","Alverca","over",2.05],"432,437"],
+	[[10730,"Swansea","West Bromwich","mitad",1.42],[10730,"Blackburn Rovers","Burnley","mitad",1.55],[10730,"Burton Albion","Northampton","mitad",1.45],[10530,"Sanliurfaspor","Corum Belediyespor","mitad",1.42],[10600,"Portimonense SC","Alverca","mitad",1.45],"65,712"],
 ]
