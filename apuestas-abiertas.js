5392eb6a54ed39892b990f96716018f66aa1ccfa
aliensanderdiaz <aliensanderdiaz@gmail.com>
2025-01-04 12:24:10 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,8 +1,10 @@
 let APUESTAS_ABIERTAS = [
-	[[10800,"Antalyaspor","Eyupspor","local",2.1],[10800,"EA Guingamp","Dunkerque","local",2],[10800,"FC Metz","Pau FC","local",1.769],[10800,"Bradford City","Grimsby Town","local",1.8],"133,754"],
-	[[10800,"EA Guingamp","Dunkerque","over",2],"20.000"],
-	[[10800,"EA Guingamp","Dunkerque","mitad",1.42],"14.200"],
-	[[10730,"Burton Albion","Northampton","local",2.1],[10730,"Swansea","West Bromwich","visitante",2.375],[10730,"Blackburn Rovers","Burnley","visitante",2.3],[10730,"Cambridge United","Bristol Rovers","visitante",2.55],"536,281"],
-	[[10730,"Swansea","West Bromwich","over",2.1],[10730,"Blackburn Rovers","Burnley","over",2.7],[10730,"Burton Albion","Northampton","over",2.25],[10730,"Cambridge United","Bristol Rovers","over",2.1],"267,908"],
-	[[10730,"Swansea","West Bromwich","mitad",1.45],[10730,"Blackburn Rovers","Burnley","mitad",1.6],[10730,"Burton Albion","Northampton","mitad",1.48],[10730,"Cambridge United","Bristol Rovers","mitad",1.444],"49,596"],
+	[[11230,"Brighton","Arsenal","mitad",1.4],"14.000"],
+	[[11230,"Brighton","Arsenal","over",1.95],"19.500"],
+	[[11230,"Brighton","Arsenal","visitante",1.8],"18.000"],
+	[[11200,"Fiorentina","Napoles","over",2.35],"23.500"],
+	[[11200,"Fiorentina","Napoles","mitad",1.5],"15.000"],
+	[[11200,"Fiorentina","Napoles","visitante",2.1],[11230,"Brighton","Arsenal","visitante",1.8],[11200,"Samsunspor","Trabzonspor","local",1.85],"69,930"],
+	[[11200,"Fiorentina","Napoles","over",2.15],[11300,"Lille","Nantes","over",2],[11300,"CD Tondela","Uniao Leiria","over",2.2],"94,600"],
+	[[11200,"Fiorentina","Napoles","mitad",1.45],[11300,"CD Tondela","Uniao Leiria","mitad",1.48],"21,460"],
 ]
