4fe945b1660b5ccea2355f20f8cd55cdc26c51b5
aliensanderdiaz <aliensanderdiaz@gmail.com>
2025-01-05 13:31:04 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,7 +1,10 @@
 let APUESTAS_ABIERTAS = [
-	[[11030,"CD Santa Clara","Sporting Farense","local",1.909],[11030,"FC Vizela","Maritimo","local",2],[11300,"Casa Pia Atletico","FC Famalicao","local",2.1],[11330,"Panathinaikos","PAOK Thessaloniki","local",1.95],[11130,"Liverpool","Manchester United","visitante",2.6],[11130,"PSG","AS Monaco","visitante",2.25],[11200,"Torino","Parma","visitante",2.55],[11230,"Atromitos","Asteras Tripolis","visitante",2.9],[11300,"Pacos Ferreira","Academica Viseu","visitante",2.15],"14,542,632"],
-	[[11030,"CD Santa Clara","Sporting Farense","mitad",1.55],[11300,"Casa Pia Atletico","FC Famalicao","mitad",1.533],[11030,"FC Vizela","Maritimo","mitad",1.45],[11100,"Adanaspor","Amedspor","mitad",1.48],[11230,"Atromitos","Asteras Tripolis","mitad",1.571],[11300,"Pacos Ferreira","Academica Viseu","mitad",1.48],"118,619"],
-	[[11000,"Crewe Alexandra","Bromley FC","local",2.05],[11000,"Dundee United","Hearts","local",2.1],"90,405"],
-	[[11000,"Dundee United","Hearts","mitad",1.42],[11000,"Crewe Alexandra","Bromley FC","mitad",1.42],"20,164"],
-	[[10930,"Motherwell","Aberdeen","visitante",2.1],[10930,"Aris Thessaloniki","OFI Crete","visitante",2.75],"121,275"],
+	[[11330,"Panathinaikos","PAOK Thessaloniki","over",2.04],"20.500"],
+	[[11330,"Panathinaikos","PAOK Thessaloniki","local",2],"20.000"],
+	[[11330,"Panathinaikos","PAOK Thessaloniki","mitad",1.44],"14.444"],
+	[[11330,"Panathinaikos","PAOK Thessaloniki","local",1.93],"87,140"],
+	[[11300,"Casa Pia Atletico","FC Famalicao","over",2.45],[11300,"Pacos Ferreira","Academica Viseu","over",2.2],"53,900"],
+	[[11330,"Panathinaikos","PAOK Thessaloniki","mitad",1.42],"32,225"],
+	[[11230,"Atromitos","Asteras Tripolis","over",2.54],"25.500"],
+	[[11230,"Atromitos","Asteras Tripolis","over",2.5],[11300,"Casa Pia Atletico","FC Famalicao","over",2.45],[11300,"Pacos Ferreira","Academica Viseu","over",2.2],"134,750"],
 ]
