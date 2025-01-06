e72c67cfa5e46397ded2db4b17769e30a50db48c
aliensanderdiaz <aliensanderdiaz@gmail.com>
2025-01-06 09:33:55 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,7 +1,13 @@
 let APUESTAS_ABIERTAS = [
-	[[11530,"CF Estrela","Estoril","over",2.35],"23.500"],
-	[[11530,"CF Estrela","Estoril","local",2.15],"21.500"],
-	[[11445,"Roma","Lazio","over",2.1],"21.000"],
-	[[11445,"Roma","Lazio","over",2.1],[11530,"CF Estrela","Estoril","over",2.4],"50,400"],
-	[[11530,"CF Estrela","Estoril","mitad",1.533],"22,233"],
+	[[11500,"QPR","Luton Town","local",2.2],[11515,"Gil Vicente","Rio Ave","local",1.95],[11500,"Wolverhampton Wanderers","Nottingham Forest","visitante",2],"85,800"],
+	[[11500,"Wolverhampton Wanderers","Nottingham Forest","over",2],[11500,"QPR","Luton Town","over",2.15],[11515,"Gil Vicente","Rio Ave","over",2.1],"90,300"],
+	[[11500,"Wolverhampton Wanderers","Nottingham Forest","mitad",1.42],[11500,"QPR","Luton Town","mitad",1.48],[11515,"Gil Vicente","Rio Ave","mitad",1.444],"30,356"],
+	[[11400,"Inter","AC Milán","local",1.76],"17.692"],
+	[[11200,"Sakaryaspor","Genclerbirligi","over",2.2],"22.000"],
+	[[11200,"Sakaryaspor","Genclerbirligi","mitad",1.48],"14.800"],
+	[[11100,"Panetolikos FC","Olympiacos","over",2.04],"20.500"],
+	[[11100,"Panetolikos FC","Olympiacos","mitad",1.42],"14.200"],
+	[[11400,"Inter","AC Milán","local",1.769],[11500,"QPR","Luton Town","local",2.2],[11515,"Gil Vicente","Rio Ave","local",1.95],[11500,"Wolverhampton Wanderers","Nottingham Forest","visitante",2],"151,800"],
+	[[11500,"Wolverhampton Wanderers","Nottingham Forest","over",2],[11500,"QPR","Luton Town","over",2.15],[11515,"Gil Vicente","Rio Ave","over",2.1],[11100,"Panetolikos FC","Olympiacos","over",2.05],[11200,"Sakaryaspor","Genclerbirligi","over",2.2],"407,253"],
+	[[11500,"Wolverhampton Wanderers","Nottingham Forest","mitad",1.42],[11500,"QPR","Luton Town","mitad",1.48],[11515,"Gil Vicente","Rio Ave","mitad",1.444],[11100,"Panetolikos FC","Olympiacos","mitad",1.42],[11200,"Sakaryaspor","Genclerbirligi","mitad",1.48],"63,797"],
 ]
