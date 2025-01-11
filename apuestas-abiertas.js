14d0d9970d6ff6b990dcbdc3c7bbe866677a1aea
aliensanderdiaz <aliensanderdiaz@gmail.com>
2025-01-11 17:28:05 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,6 +1,9 @@
 let APUESTAS_ABIERTAS = [
-	[[11630,"Rio Branco PR","Azuriz FC PR","mitad",1.44],"14.020"],
+	[[11800,"Atletico Morelia","Jaiba Brava","local",2],[11800,"Atletico San Luis","Tigres UANL","visitante",1.833],"36,667"],
+	[[11800,"Atletico Morelia","Jaiba Brava","over",2.15],[11815,"Itabaiana SE","AD Carmopolis SE","over",2.2],"47,300"],
+	[[11800,"Atletico Morelia","Jaiba Brava","mitad",1.48],[11815,"Itabaiana SE","AD Carmopolis SE","mitad",1.48],"21,904"],
+	[[11800,"Atletico Morelia","Jaiba Brava","local",2],[12000,"Atlante","Club Celaya","local",1.769],[12105,"Dorados","Mineros de Zacatecas","local",2.05],[11800,"Atletico San Luis","Tigres UANL","visitante",1.8],[12205,"Cruz Azul","Atlas de Guadalajara","visitante",3.5],"456,992"],
+	[[11800,"Atletico Morelia","Jaiba Brava","over",2.15],[11815,"Itabaiana SE","AD Carmopolis SE","over",2.2],[11830,"Central SC","ADJG Jaguar","over",2],[11900,"Porto Sport Club","Jacobina BA","over",2.15],[12000,"Atlante","Club Celaya","over",2],"406,780"],
+	[[11800,"Atletico Morelia","Jaiba Brava","mitad",1.48],[11815,"Itabaiana SE","AD Carmopolis SE","mitad",1.48],[11900,"Porto Sport Club","Jacobina BA","mitad",1.45],"31,761"],
 	[[11700,"Bangu RJ","Portuguesa RJ","visitante",1.95],"19.500"],
-	[[11630,"Rio Branco PR","Azuriz FC PR","over",2.05],[11700,"Bangu RJ","Portuguesa RJ","over",2.15],[11700,"Uniao Rondonopolis MT","Mixto MT","over",2.2],[11700,"Luverdense MT","EC Primavera MT","over",2.2],"213,323"],
-	[[11630,"Rio Branco PR","Azuriz FC PR","mitad",1.444],[11700,"Bangu RJ","Portuguesa RJ","mitad",1.45],[11700,"Uniao Rondonopolis MT","Mixto MT","mitad",1.48],[11700,"Luverdense MT","EC Primavera MT","mitad",1.45],"44,947"],
 ]
