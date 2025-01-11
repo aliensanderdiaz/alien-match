48cba1b60d7831b9ed38661b23e50428e7f42173
aliensanderdiaz <aliensanderdiaz@gmail.com>
2025-01-11 18:57:22 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,9 +1,10 @@
 let APUESTAS_ABIERTAS = [
-	[[11800,"Atletico Morelia","Jaiba Brava","local",2],[11800,"Atletico San Luis","Tigres UANL","visitante",1.833],"36,667"],
-	[[11800,"Atletico Morelia","Jaiba Brava","over",2.15],[11815,"Itabaiana SE","AD Carmopolis SE","over",2.2],"47,300"],
-	[[11800,"Atletico Morelia","Jaiba Brava","mitad",1.48],[11815,"Itabaiana SE","AD Carmopolis SE","mitad",1.48],"21,904"],
-	[[11800,"Atletico Morelia","Jaiba Brava","local",2],[12000,"Atlante","Club Celaya","local",1.769],[12105,"Dorados","Mineros de Zacatecas","local",2.05],[11800,"Atletico San Luis","Tigres UANL","visitante",1.8],[12205,"Cruz Azul","Atlas de Guadalajara","visitante",3.5],"456,992"],
-	[[11800,"Atletico Morelia","Jaiba Brava","over",2.15],[11815,"Itabaiana SE","AD Carmopolis SE","over",2.2],[11830,"Central SC","ADJG Jaguar","over",2],[11900,"Porto Sport Club","Jacobina BA","over",2.15],[12000,"Atlante","Club Celaya","over",2],"406,780"],
-	[[11800,"Atletico Morelia","Jaiba Brava","mitad",1.48],[11815,"Itabaiana SE","AD Carmopolis SE","mitad",1.48],[11900,"Porto Sport Club","Jacobina BA","mitad",1.45],"31,761"],
-	[[11700,"Bangu RJ","Portuguesa RJ","visitante",1.95],"19.500"],
+	[[10600,"FC Felgueiras 1932","Pacos Ferreira","local",1.9],[10600,"Academica Viseu","FC Vizela","local",2.2],[10530,"Sivasspor","Alanyaspor","visitante",2],[10630,"Génova","Parma","visitante",2.4],[10630,"Salernitana","Sassuolo","visitante",1.769],"354,978"],
+	[[10530,"Keciorengucu","Kocaelispor","over",2],[10530,"Erzurum BB","Sanliurfaspor","over",2.1],[10600,"FC Felgueiras 1932","Pacos Ferreira","over",2.1],[10600,"Academica Viseu","FC Vizela","over",2.25],"198,450"],
+	[[10530,"Keciorengucu","Kocaelispor","mitad",1.42],[10530,"Erzurum BB","Sanliurfaspor","mitad",1.45],[10600,"FC Felgueiras 1932","Pacos Ferreira","mitad",1.444],[10600,"Academica Viseu","FC Vizela","mitad",1.5],"44,612"],
+	[[12105,"Dorados","Mineros de Zacatecas","local",2.05],[12205,"Cruz Azul","Atlas de Guadalajara","visitante",3.5],"71,750"],
+	[[12000,"Atlante","Club Celaya","local",1.76],"17.692"],
+	[[12000,"Atlante","Club Celaya","over",2],"20.000"],
+	[[11900,"Porto Sport Club","Jacobina BA","over",2.15],"21.500"],
+	[[11900,"Porto Sport Club","Jacobina BA","mitad",1.45],"14.500"],
 ]
