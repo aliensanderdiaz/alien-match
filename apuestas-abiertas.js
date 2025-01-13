1cec6011968689d73d7c4daf7993ab1f3335245c
aliensanderdiaz <aliensanderdiaz@gmail.com>
2025-01-12 20:41:42 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,6 +1,2 @@
 let APUESTAS_ABIERTAS = [
-	[[11045,"NAC Breda","Heerenveen","local",1.95],[11100,"Istanbul Basaksehir FK","Galatasaray","visitante",1.533],[11115,"Montpellier","Angers SCO","visitante",2.3],[11115,"Toulouse","Strasbourg","visitante",2.4],[11115,"Cesena","Cittadella","visitante",2.8],"462,134"],
-	[[11045,"NAC Breda","Heerenveen","over",2],[11115,"Cesena","Cittadella","over",2.5],"50,000"],
-	[[11115,"Cesena","Cittadella","mitad",1.54],"14.122"],
-	[[11015,"Elche","Real Zaragoza","over",2.375],[11015,"CF Cadiz","Levante","over",2.25],[11000,"Lamia","Atromitos","over",2.8],"149,625"],
 ]
