977b4954b54f617c170148942a4e4c081eaa54ca
aliensanderdiaz <aliensanderdiaz@gmail.com>
2025-01-16 07:30:07 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,2 +1,5 @@
 let APUESTAS_ABIERTAS = [
+	[[11330,"Athletic Bilbao","Osasuna","local",1.769],[11330,"Real Sociedad","Rayo Vallecano","local",1.8],[11245,"De Graafschap","Heracles Almelo","visitante",1.85],"5,892"],
+	[[11300,"Rio Claro SP","Ituano SP","over",2.15],[11300,"Maguary PE","Central SC","over",2.25],[11330,"Athletic Bilbao","Osasuna","over",2.25],[11330,"Real Sociedad","Rayo Vallecano","over",2.25],"24,490"],
+	[[11300,"Rio Claro SP","Ituano SP","mitad",1.45],[11300,"Maguary PE","Central SC","mitad",1.48],[11330,"Athletic Bilbao","Osasuna","mitad",1.45],[11330,"Real Sociedad","Rayo Vallecano","mitad",1.45],"45,120"],
 ]
--- ligas-faltantes-codes.js
+++ ligas-faltantes-codes.js
@@ -1,41 +1,8 @@
 let LIGAS_FALTANTES = [
-  'BCAT' ,
-  'BGOI' ,
-  'BMAR' ,
   'BMAT' ,
-  'BPARAI' ,
 
 ];
 
 let LIGAS_FALTANTES_BRASIL = [
-  '19457',
-  // Brazil - Campeonato Paulista A1
-  '60442',
-  // Brazil - Supercopa do Brasil
-  '1187789',
-  // Brazil - Copa Sao Paulo U20
-  '32464',
-  // Brazil - Campeonato Alagoano
-  '19791',
-  // Brasil - Campeonato Baiano
-  '19456',
-  // Brasil - Campeonato Carioca
-  '19788',
-  // Brasil - Campeonato Catarinense
-  '44672',
-  // Brazil - Campeonato Maranhense
-  '19790',
-  // Brasil - Campeonato Paranaense
-  '28509',
-  // Brazil - Campeonato Paraibano
-  '19792',
-  // Brasil - Campeonato Pernambucano
-  '55406',
-  // Brazil - Campeonato Piauiense
-  '19793',
-  // Brasil - Campeonato Potiguar
-  '47533',
-  // Brazil - Campeonato Sergipano
-  '44888',
-  // Brazil - Campeonato Matogrossense
+
 ]
