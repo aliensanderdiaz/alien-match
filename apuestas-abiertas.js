2abc9a72fc5435f1274ec847ab5a884c04352009
aliensanderdiaz <aliensanderdiaz@gmail.com>
2025-01-19 08:53:29 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,4 +1,3 @@
 let APUESTAS_ABIERTAS = [
-	[[10800,"Celta de Vigo","Athletic Bilbao","mitad",1.42],[10800,"Burgos CF","Deportivo La Coruna","mitad",1.533],[10800,"Atromitos","Olympiacos","mitad",1.45],[10800,"Portuguesa Santista SP","Capivariano SP","mitad",1.48],[10800,"Juventus SP","Rio Claro SP","mitad",1.571],[10800,"EC Democrata MG","Villa Nova MG","mitad",1.55],"68,286"],
-	[[10800,"Celta de Vigo","Athletic Bilbao","mitad",1.42],[10800,"Burgos CF","Deportivo La Coruna","mitad",1.5],[10830,"Almere City FC","Heracles Almelo","mitad",1.45],[10800,"Atromitos","Olympiacos","mitad",1.42],[10800,"Portuguesa Santista SP","Capivariano SP","mitad",1.48],[10800,"Juventus SP","Rio Claro SP","mitad",1.571],[10800,"EC Democrata MG","Villa Nova MG","mitad",1.55],"101,973"],
+	[[10900,"Palermo","Juve Stabia","mitad",1.45],[10900,"Catanzaro","SC Pisa","mitad",1.444],[10900,"Saint Etienne","Nantes","mitad",1.42],[10900,"Pacos Ferreira","CD Mafra","mitad",1.48],[10900,"Sport Club Aymores","Atletico MG","mitad",1.5],[10930,"Levadiakos","Asteras Tripolis","mitad",1.615],[10930,"Panetolikos FC","OFI Crete","mitad",1.55],"132,254"],
 ]
