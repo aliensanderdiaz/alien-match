cd1b084c95f2be2ec18498f2dbda38fbb508ba6d
aliensanderdiaz <aliensanderdiaz@gmail.com>
2025-01-26 21:53:21 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,4 +1,2 @@
 let APUESTAS_ABIERTAS = [
-	[[11115,"Cremonese","Modena FC","mitad",1.5],[11130,"FC St. Pauli","Union Berlin","mitad",1.444],[11230,"Malaga","Real Zaragoza","mitad",1.6],[11230,"Ferrol","Burgos CF","mitad",1.615],[11300,"FC Porto","CD Santa Clara","mitad",1.45],[11300,"Pumas Unam","Atlas de Guadalajara","mitad",1.42],[11300,"EC Democrata MG","Tombense MG","mitad",1.533],[11330,"Olympiacos","Panathinaikos","mitad",1.5],[11345,"Marica RJ","Sampaio Correa RJ","mitad",1.571],"416,742"],
-	[[11030,"Estoril","Vitoria Guimaraes","mitad",1.42],[11030,"FC Porto B","CD Tondela","mitad",1.48],"21,016"],
 ]
