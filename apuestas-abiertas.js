95c9e45238d5f15b7e2f00601490fabbbf3e4494
aliensanderdiaz <aliensanderdiaz@gmail.com>
2025-01-29 19:22:31 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,2 +1,6 @@
 let APUESTAS_ABIERTAS = [
+	[[12000,"Atlas de Guadalajara","Monterrey","mitad",1.42],[12000,"Atletico MG","America Mineiro MG","mitad",1.45],"12,148"],
+	[[11930,"Atlético Nacional","Atletico Bucaramanga","local",1.833],[11930,"Botafogo SP","Agua Santa SP","local",2.1],[11930,"Botafogo RJ","Fluminense RJ","local",2.15],[11935,"Portuguesa SP","Sao Paulo SP","visitante",2.05],"16,969"],
+	[[11930,"Atlético Nacional","Atletico Bucaramanga","over",2.55],[11930,"Botafogo SP","Agua Santa SP","over",2.55],[11930,"Botafogo RJ","Fluminense RJ","over",2.4],[11930,"SER Caxias Do Sul","Brasil de Pelotas RS","over",2.6],[11935,"Portuguesa SP","Sao Paulo SP","over",2.4],"97,381"],
+	[[11930,"Atlético Nacional","Atletico Bucaramanga","mitad",1.5],[11930,"Botafogo SP","Agua Santa SP","mitad",1.55],[11930,"Botafogo RJ","Fluminense RJ","mitad",1.5],[11930,"SER Caxias Do Sul","Brasil de Pelotas RS","mitad",1.55],[11935,"Portuguesa SP","Sao Paulo SP","mitad",1.533],"82,886"],
 ]
--- ligas-faltantes-codes.js
+++ ligas-faltantes-codes.js
@@ -1,13 +1,5 @@
 let LIGAS_FALTANTES = [
-   'BALA' ,
-   'BAMA' ,
-   'BBRA' ,
-   'BCAP' ,
-   'BCEA' ,
-   'BGOI' ,
-   'BMAR' ,
-   'BMAT' ,
-   'BSMAT' ,
+
 ];
 
 let LIGAS_FALTANTES_BRASIL = [
