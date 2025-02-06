352699f8aab184e51e48d006949fbebae3cd8566
aliensanderdiaz <aliensanderdiaz@gmail.com>
2025-02-06 13:44:32 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,3 +1,4 @@
 let APUESTAS_ABIERTAS = [
-	[[11500,"CA Aldosivi","CA Barracas Central","mitad",1.65],[10900,"CS Municipal Studentesc Iasi","FC Uta Arad","mitad",1.48],[11330,"Real Sociedad","Osasuna","mitad",1.45],[11330,"Petrolul Ploiesti","FCSB","mitad",1.571],[11400,"Madureira RJ","Sampaio Correa RJ","mitad",1.45],"80,682"],
+	[[11500,"CA Aldosivi","CA Barracas Central","mitad",1.65],[11700,"Sao Bernardo SP","Novorizontino SP","mitad",1.48],[11700,"EC Sao Jose","Ypiranga RS","mitad",1.571],[11700,"Desportiva Ferroviaria ES","Nova Venecia ES","mitad",1.48],[11700,"Concordia SC","CF Caravaggio SC","mitad",1.571],"89,248"],
+	[[11330,"Real Sociedad","Osasuna","mitad",1.45],[11330,"Petrolul Ploiesti","FCSB","mitad",1.615],[11400,"Madureira RJ","Sampaio Correa RJ","mitad",1.45],"33,963"],
 ]
