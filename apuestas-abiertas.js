6fef17221880fa3c6b2f4d5d2f6f9b29c0be43b9
aliensanderdiaz <aliensanderdiaz@gmail.com>
2025-02-09 09:10:39 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,4 +1,5 @@
 let APUESTAS_ABIERTAS = [
-	[[10800,"CD Alaves","Getafe","mitad",1.727],[10800,"Elche","CD Tenerife","mitad",1.571],[10730,"AFC Hermanstadt","Petrolul Ploiesti","mitad",1.571],[10745,"Juventud de Las Piedras","Danubio","mitad",1.55],"66,112"],
-	[[10800,"CD Alaves","Getafe","mitad",1.727],[10800,"Elche","CD Tenerife","mitad",1.571],[10900,"Reggiana","Cesena","mitad",1.571],[10900,"Spezia","Palermo","mitad",1.45],[10730,"AFC Hermanstadt","Petrolul Ploiesti","mitad",1.571],[10745,"Juventud de Las Piedras","Danubio","mitad",1.55],[10900,"Academica Viseu","Alverca","mitad",1.5],"225,962"],
+	[[11015,"Valencia","CD Leganes","mitad",1.45],[11015,"CF Cordoba","SD Huesca","mitad",1.5],[11015,"Burgos CF","Racing Santander","mitad",1.48],[11030,"Sporting Farense","Nacional Madeira","mitad",1.5],"48,285"],
+	[[11015,"Valencia","CD Leganes","mitad",1.45],[11015,"CF Cordoba","SD Huesca","mitad",1.5],[11015,"Burgos CF","Racing Santander","mitad",1.48],[11030,"Sporting Farense","Nacional Madeira","mitad",1.5],[11115,"Juve Stabia","Bari","mitad",1.55],"74,842"],
+	[[10900,"Reggiana","Cesena","mitad",1.571],[10900,"Academica Viseu","Alverca","mitad",1.5],"34,048"],
 ]
