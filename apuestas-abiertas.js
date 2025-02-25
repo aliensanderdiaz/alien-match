207a97e5fb13ccf2c6d2bb7f7df24d82ac09ece3
aliensanderdiaz <aliensanderdiaz@gmail.com>
2025-02-25 17:33:18 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,7 +1,5 @@
 let APUESTAS_ABIERTAS = [
-	[[11500,"O Elvas Cad","FC Tirsense","mitad",1.5],[11500,"Angers SCO","Stade Reims","mitad",1.444],"108,338 con $ 50,002"],
-	[[11330,"Moto Clube MA","Iape MA","mitad",1.48],[11445,"Wealdstone FC","Boston United","mitad",1.42],"210,160 con $ 100,000"],
-	[[11445,"Wigan","Huddersfield","mitad",1.533],[11445,"Gillingham","Fleetwood Town","mitad",1.533],"235,111 con $ 100,000"],
-	[[11445,"Birmingham City","Leyton Orient","mitad",1.42],[11445,"Tranmere","Accrington","mitad",1.45],"205,900 con $ 100,000"],
-	[[11445,"Cardiff City","Hull City","mitad",1.42],[11445,"Bromley FC","Bradford City","mitad",1.42],"201,640 con $ 100,000"],
+	[[11800,"Operario MS","Criciuma SC","mitad",0],"145,000 con $ 100,000"],
+	[[11700,"Maranhao MA","EC Vitoria","mitad",1.48],[11730,"Metropolitano Maringa PR","Juventude RS","mitad",1.45],"214,600 con $ 100,000"],
+	[[11445,"Wealdstone FC","Boston United","mitad",1.42],"210,160 con $ 100,000"],
 ]
