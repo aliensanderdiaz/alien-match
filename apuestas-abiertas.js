e04f29c4c1367c02462eedb39f2676db61450042
aliensanderdiaz <aliensanderdiaz@gmail.com>
2025-02-26 08:59:11 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,5 +1,3 @@
 let APUESTAS_ABIERTAS = [
-	[[10500,"Machida Zelvia","Tokyo Verdy","mitad",1.55],[10500,"Fagiano Okayama","Gamba Osaka","mitad",1.533],"200,284 con $ 84,271"],
-	[[10500,"Kashima Antlers","Albirex Niigata","mitad",1.48],[10500,"Kashiwa Reysol","Cerezo Osaka","mitad",1.444],"213,778 con $ 100,000"],
-	[[10500,"Avispa Fukuoka","Kawasaki Frontale","mitad",1.444],[10500,"FC Tokyo","Nagoya Grampus","mitad",1.55],"223,889 con $ 100,000"],
+	[[10930,"Asteras Tripolis","OFI Crete","mitad",0],"150,000 con $ 100,000"],
 ]
