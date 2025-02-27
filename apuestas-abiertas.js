3b26bc992773f240f2a4ff133abc21d704b690bf
aliensanderdiaz <aliensanderdiaz@gmail.com>
2025-02-26 21:10:48 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,3 +1,2 @@
 let APUESTAS_ABIERTAS = [
-	[[11200,"Pogon Szczecin","GKS Piast Gliwice","mitad",1.45],[11300,"Olympiacos","AEK Athens","mitad",1.48],[11300,"Juventus SP","Capivariano SP","mitad",1.48],[11300,"Rio Claro SP","Gremio Prudente SP","mitad",1.533],[11300,"Santo Andre SP","Oeste SP","mitad",1.533],"746,732 con $ 100,000"],
 ]
