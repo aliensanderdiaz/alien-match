f9fc4b5fc27191857b499a3e19646c270b6d5ef1
aliensanderdiaz <aliensanderdiaz@gmail.com>
2025-02-28 16:41:00 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,3 +1,4 @@
 let APUESTAS_ABIERTAS = [
-	[[11100,"Karpaty LVIV","LNZ Cherkasy","mitad",1.48],[11100,"FC Dynamo Makhachkala","Lokomotiv Moscow","mitad",1.5],"222,000 con $ 100,000"],
+	[[11500,"Real Valladolid","Las Palmas","mitad",1.48],[11500,"Sheffield Wednesday","Sunderland","mitad",1.42],[11500,"Arsenal de Sarandi","Club Atletico Atlanta","mitad",1.8],"378,288 con $ 100,000"],
+	[[11500,"Real Valladolid","Las Palmas","mitad",1.48],[11500,"Sheffield Wednesday","Sunderland","mitad",1.42],[11445,"Partick Thistle","Livingston","mitad",1.42],[11500,"Arsenal de Sarandi","Club Atletico Atlanta","mitad",1.8],"1,823,753 con $ 100,000"],
 ]
