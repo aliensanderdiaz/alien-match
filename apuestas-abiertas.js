d05235957e00664c107c95bcb96e3692035c94b7
aliensanderdiaz <aliensanderdiaz@gmail.com>
2025-03-02 09:43:11 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,6 +1,6 @@
 let APUESTAS_ABIERTAS = [
+	[[11000,"Huachipato","Colo Colo","mitad",0],"142,000 con $ 100,000"],
+	[[11030,"Moreirense","CF Estrela","mitad",1.5],[11045,"Sparta Rotterdam","Willem II Tilburg","mitad",1.42],"223,820 con $ 105,080"],
+	[[11015,"Real Oviedo","Deportivo La Coruna","mitad",1.48],[11030,"Boavista","CD Santa Clara","mitad",1.533],"238,462 con $ 105,080"],
 	[[10900,"FC Porto B","Maritimo","mitad",0],"122,997 con $ 85,152"],
-	[[10900,"Modena FC","Cosenza","mitad",1.42],[10830,"Zorya Lugansk","Rukh Vynnyky","mitad",1.48],"210,160 con $ 100,000"],
-	[[10900,"Génova","Empoli","mitad",1.45],[10900,"Catanzaro","Reggiana","mitad",1.5],"217,500 con $ 100,000"],
-	[[10800,"CD Leganes","Getafe","mitad",1.727],[10800,"CS Municipal Studentesc Iasi","Petrolul Ploiesti","mitad",1.615],"396,210 con $ 100,000"],
 ]
