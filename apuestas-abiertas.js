f092659ab231963eacafd11cbf246ea402cea6c6
aliensanderdiaz <aliensanderdiaz@gmail.com>
2025-03-02 08:29:16 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,7 +1,6 @@
 let APUESTAS_ABIERTAS = [
-	[[10730,"SV Waldhof Mannheim","TSV Alemannia Aachen","mitad",0],"144,444 con $ 100,000"],
-	[[10630,"Monza","Torino","mitad",1.48],[10530,"Boluspor","Erzurum BB","mitad",1.5],[10600,"Portimonense SC","Leixoes SC","mitad",1.444],[10600,"FK Khimki","Fakel Voronezh","mitad",1.48],[10700,"CS 2 de Mayo","Sportivo Trinidense","mitad",1.48],"702,388 con $ 100,000"],
-	[[10000,"Albirex Niigata","Cerezo Osaka","mitad",1.45],[10000,"Nagoya Grampus","Machida Zelvia","mitad",1.6],[10000,"Tokyo Verdy","Gamba Osaka","mitad",1.55],[10000,"Fagiano Okayama","Shimizu S-Pulse","mitad",1.5],[10000,"Daejeon Citizen","Suwon City FC","mitad",1.42],[10100,"Urawa Red Diamonds","Kashiwa Reysol","mitad",1.5],[10230,"Gangwon FC","Jeju United","mitad",1.42],"1,631,469 con $ 100,000"],
-	[[12210,"Pumas Unam","Chivas Guadalajara","mitad",0],"142,000 con $ 100,000"],
-	[[11930,"Talleres de Cordoba","CA Tigre","mitad",1.444],[11930,"Union Santa Fe","Gimnasia LP","mitad",1.6],[11930,"Temperley","Almirante Brown","mitad",1.615],[11930,"UCV FC","Academia Puerto Cabello","mitad",1.48],"552,533 con $ 100,000"],
+	[[10900,"FC Porto B","Maritimo","mitad",0],"122,997 con $ 85,152"],
+	[[10900,"Modena FC","Cosenza","mitad",1.42],[10830,"Zorya Lugansk","Rukh Vynnyky","mitad",1.48],"210,160 con $ 100,000"],
+	[[10900,"Génova","Empoli","mitad",1.45],[10900,"Catanzaro","Reggiana","mitad",1.5],"217,500 con $ 100,000"],
+	[[10800,"CD Leganes","Getafe","mitad",1.727],[10800,"CS Municipal Studentesc Iasi","Petrolul Ploiesti","mitad",1.615],"396,210 con $ 100,000"],
 ]
