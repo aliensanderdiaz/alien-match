ab01f5802478c8a601d8dcfaa42f3d5804d28dfe
aliensanderdiaz <aliensanderdiaz@gmail.com>
2025-03-05 09:15:16 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,2 +1,3 @@
 let APUESTAS_ABIERTAS = [
+	[[11315,"America SE","Guarany SE","mitad",1.42],[11400,"Ferrol","Burgos CF","mitad",1.7],[11445,"Livingston","Raith Rovers","mitad",1.48],[11500,"Deportivo Riestra","CA San Telmo","mitad",1.533],"547,817 con $ 100,000"],
 ]
