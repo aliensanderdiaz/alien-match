5a77cb61627d66ee64913b77397522f63fd5b231
aliensanderdiaz <aliensanderdiaz@gmail.com>
2025-05-05 07:40:05 -0500

    m

--- logica_partidos_wplay.js
+++ logica_partidos_wplay.js
@@ -1,4 +1,4 @@
-const MAXIMO_A_GANAR = 8000000 / 1000
+const MAXIMO_A_GANAR = 16_000_000 / 10000
 
 let PARTIDOS_BACKUP = [...PARTIDOS_OPTIMIZADOS]
 let indiceFavorito = 0
@@ -38,7 +38,7 @@ const ponerEventosEnLasAnclas = () => {
 }
 
 const elegirFavorito = function (indice, valor) {
-    console.log({ funcion: 'elegirFavorito', indice, valor})
+    console.log({ funcion: 'elegirFavorito', indice, valor })
     indiceFavorito = indice - 1
     PARTIDOS_OPTIMIZADOS[indiceFavorito]['favorito'] = valor
     const myModal = new bootstrap.Modal(document.getElementById('stakeModal'))
@@ -83,19 +83,19 @@ const dibujar = function (partidos = PARTIDOS_OPTIMIZADOS) {
         if (partido.mitadFavorito && partido.codigoWplay) {
             let exists = codeLiguesPreferidos.find(p => p.codigoWplay === partido.codigoWplay)
             if (!exists) {
-                codeLiguesPreferidos.push({ mitad: partido.mitad, codigoWplay: partido.codigoWplay})
+                codeLiguesPreferidos.push({ mitad: partido.mitad, codigoWplay: partido.codigoWplay })
             }
         }
 
         if (partido.mitad && partido.codigoWplay) {
             let exists = codeLigues.find(p => p.codigoWplay === partido.codigoWplay)
             if (!exists) {
-                codeLigues.push({ mitad: partido.mitad, codigoWplay: partido.codigoWplay})
+                codeLigues.push({ mitad: partido.mitad, codigoWplay: partido.codigoWplay })
             }
         } else if (partido.mitad === false && partido.codigoWplay) {
             let exists = codeLiguesNoHalf.find(p => p.codigoWplay === partido.codigoWplay)
             if (!exists) {
-                codeLiguesNoHalf.push({ mitad: partido.mitad, codigoWplay: partido.codigoWplay})
+                codeLiguesNoHalf.push({ mitad: partido.mitad, codigoWplay: partido.codigoWplay })
             }
         }
 
@@ -119,10 +119,10 @@ const dibujar = function (partidos = PARTIDOS_OPTIMIZADOS) {
             borrarAcumuladoFavorito = false
         }
 
-        if (partido.cuotaFavorito && partido.cuotaFavorito >= 1.74 ) {
+        if (partido.cuotaFavorito && partido.cuotaFavorito >= 1.74) {
             acumuladoFavorito = Math.floor(acumuladoFavorito * partido.cuotaFavorito * 100) / 100
             // console.log('FAVORITO')
-        // } else if ( partido.cuotaCualquiera && partido.cuotaCualquiera >= 1.42) {
+            // } else if ( partido.cuotaCualquiera && partido.cuotaCualquiera >= 1.42) {
             // console.log('CUALQUIERA')
             // acumuladoFavorito = Math.floor(acumuladoFavorito * partido.cuotaCualquiera * 100) / 100
         }
@@ -138,7 +138,7 @@ const dibujar = function (partidos = PARTIDOS_OPTIMIZADOS) {
             borrarAcumuladoOver = false
         }
 
-        if (partido.over && partido.over >= 2 ) {
+        if (partido.over && partido.over >= 2) {
             acumuladoOver = Math.floor(acumuladoOver * partido.over * 100) / 100
             // console.log('Over')
         }
@@ -153,10 +153,10 @@ const dibujar = function (partidos = PARTIDOS_OPTIMIZADOS) {
         let partido_string = `${partido.local} - ${partido.visitante}`
         // console.log({ partido_string, acumuladoEnteroFavorito })
 
-        let mitad = partido.mitad ? ` <a href="${ baseUrl + partido.codigoWplay + '?mkt_sort=GSH1' }" target="_blank" rel="noopener noreferrer">M</a>` : ` <a href="${ baseUrl + partido.codigoWplay + '?mkt_sort=OUH1' }" target="_blank" rel="noopener noreferrer">M</a>`
-        let local = partido.mitadFavorito ? ` <a href="${ baseUrl + partido.codigoWplay + '?mkt_sort=OU1H' }" target="_blank" rel="noopener noreferrer">L</a>`: ''
-        let visitante = partido.mitadFavorito ? ` <a href="${ baseUrl + partido.codigoWplay + '?mkt_sort=OU1A' }" target="_blank" rel="noopener noreferrer">V</a>`: ''
-        let over = ` <a href="${ baseUrl + partido.codigoWplay + '?mkt_sort=HCTG' }" target="_blank" rel="noopener noreferrer">O</a>`
+        let mitad = partido.mitad ? ` <a href="${baseUrl + partido.codigoWplay + '?mkt_sort=GSH1'}" target="_blank" rel="noopener noreferrer">M</a>` : ` <a href="${baseUrl + partido.codigoWplay + '?mkt_sort=OUH1'}" target="_blank" rel="noopener noreferrer">M</a>`
+        let local = partido.mitadFavorito ? ` <a href="${baseUrl + partido.codigoWplay + '?mkt_sort=OU1H'}" target="_blank" rel="noopener noreferrer">L</a>` : ''
+        let visitante = partido.mitadFavorito ? ` <a href="${baseUrl + partido.codigoWplay + '?mkt_sort=OU1A'}" target="_blank" rel="noopener noreferrer">V</a>` : ''
+        let over = ` <a href="${baseUrl + partido.codigoWplay + '?mkt_sort=HCTG'}" target="_blank" rel="noopener noreferrer">O</a>`
 
         // let cantidad_de_letras = partido_string.length
         html += `
@@ -164,13 +164,13 @@ const dibujar = function (partidos = PARTIDOS_OPTIMIZADOS) {
             <div class="col-1 border text-end">
                 <small>${indice}/${total} - ${partido.hora - 10000}</small>
                 <br>
-                <small class="${partido.apostar && partido.apostar === 'SI' ? 'bg-success':''}">${ partido.random ? partido.random : ''} - ${partido.random ? partido.apostar: ''}</small>
+                <small class="${partido.apostar && partido.apostar === 'SI' ? 'bg-success' : ''}">${partido.random ? partido.random : ''} - ${partido.random ? partido.apostar : ''}</small>
             </div>
             <div class="col-1 border">
                 <strong>
-                    <a href="${ baseUrl + partido.codigoWplay }" target="_blank" rel="noopener noreferrer">${partido.liga}</a>
+                    <a href="${baseUrl + partido.codigoWplay}" target="_blank" rel="noopener noreferrer">${partido.liga}</a>
                 </strong>
-                ${ mitad } ${ local } ${ visitante }    ${ over }    
+                ${mitad} ${local} ${visitante}    ${over}    
             </div>
             <div class="col-2 border">
                 <strong class="elegir-favorito ${partido.favorito === 'local' ? 'favorito-local' : ''}" onclick="elegirFavorito(${indice}, 'local')">
@@ -194,8 +194,8 @@ const dibujar = function (partidos = PARTIDOS_OPTIMIZADOS) {
                             ${partido.cuotaLocal.toFixed(2)}
                         </span>
                         <!-- <br> -->
-                        <span class="${ elegirClaseDeFavorito(partido.cuotaFavorito)}">
-                            ${ partido.favorito === 'local' && partido.localMitad > 1 ? partido.localMitad.toFixed(2) : ''}
+                        <span class="${elegirClaseDeFavorito(partido.cuotaFavorito)}">
+                            ${partido.favorito === 'local' && partido.localMitad > 1 ? partido.localMitad.toFixed(2) : ''}
                         </span>
 
                     </div>
@@ -210,8 +210,8 @@ const dibujar = function (partidos = PARTIDOS_OPTIMIZADOS) {
                             ${partido.cuotaVisitante.toFixed(2)}
                         </span>
                         <!-- <br> -->
-                        <span class="${ elegirClaseDeFavorito(partido.cuotaFavorito)}">
-                            ${ partido.favorito === 'visitante' && partido.visitanteMitad > 1 ? partido.visitanteMitad.toFixed(2): ''}
+                        <span class="${elegirClaseDeFavorito(partido.cuotaFavorito)}">
+                            ${partido.favorito === 'visitante' && partido.visitanteMitad > 1 ? partido.visitanteMitad.toFixed(2) : ''}
                         </span>
 
                     </div>
@@ -220,7 +220,7 @@ const dibujar = function (partidos = PARTIDOS_OPTIMIZADOS) {
                         <span>${partido.cuotaCualquiera && partido.cuotaCualquiera > 1 ? partido.cuotaCualquiera?.toFixed(2) : ''}</span>
                     </div>
 
-                    <div class="col border-end dato ${partido.over > 1 && partido.over < 1.74 ? 'bg-danger' : partido.over > 1.73 && partido.over < 2 ? 'bg-warning' :''}">
+                    <div class="col border-end dato ${partido.over > 1 && partido.over < 1.74 ? 'bg-danger' : partido.over > 1.73 && partido.over < 2 ? 'bg-warning' : ''}">
                         <span>${partido.over && partido.over > 1 ? partido.over?.toFixed(2) : ''}</span>
                     </div>
 
@@ -228,15 +228,15 @@ const dibujar = function (partidos = PARTIDOS_OPTIMIZADOS) {
                         <span>${partido.cuotaFavorito && partido.cuotaFavorito > 1 ? partido.cuotaFavorito?.toFixed(2) : ''}</span>
                     </div>
 
-                    <div class="col border-end dato ${ claseDistinta ? '':'claseDistinta' }">
+                    <div class="col border-end dato ${claseDistinta ? '' : 'claseDistinta'}">
                         <span>${acumuladoEntero}</span>
                     </div>
 
-                    <div class="col border-end dato ${ claseDistintaOver ? '':'claseDistinta' }">
+                    <div class="col border-end dato ${claseDistintaOver ? '' : 'claseDistinta'}">
                         <span>${acumuladoEnteroOver}</span>
                     </div>
 
-                    <div class="col border-end dato ${ claseDistintaFavorito ? '':'claseDistinta' }">
+                    <div class="col border-end dato ${claseDistintaFavorito ? '' : 'claseDistinta'}">
                         <span>${acumuladoEnteroFavorito}</span>
                     </div>
 
@@ -250,8 +250,8 @@ const dibujar = function (partidos = PARTIDOS_OPTIMIZADOS) {
             </div>
         </div>
                 `
-            }
-            // ${partido.rate}
+    }
+    // ${partido.rate}
 
     let links = []
     let codeStrings = ''
@@ -260,7 +260,7 @@ const dibujar = function (partidos = PARTIDOS_OPTIMIZADOS) {
         const element = codeLigues[index].codigoWplay;
         codeStrings += element
         codeStrings += '-'
-        if ((index + 1 ) % 5 === 0 || index === codeLigues.length - 1) {
+        if ((index + 1) % 5 === 0 || index === codeLigues.length - 1) {
             let codeStringsWithOutRaya = codeStrings.substring(0, codeStrings.length - 1)
             links.push(codeStringsWithOutRaya)
             codeStrings = ''
@@ -273,7 +273,7 @@ const dibujar = function (partidos = PARTIDOS_OPTIMIZADOS) {
         const element = codeLiguesNoHalf[index].codigoWplay;
         codeStrings += element
         codeStrings += '-'
-        if ((index + 1 ) % 5 === 0 || index === codeLiguesNoHalf.length - 1) {
+        if ((index + 1) % 5 === 0 || index === codeLiguesNoHalf.length - 1) {
             let codeStringsWithOutRaya = codeStrings.substring(0, codeStrings.length - 1)
             linksNoHalf.push(codeStringsWithOutRaya)
             codeStrings = ''
@@ -286,7 +286,7 @@ const dibujar = function (partidos = PARTIDOS_OPTIMIZADOS) {
         const element = codeLiguesPreferidos[index].codigoWplay;
         codeStrings += element
         codeStrings += '-'
-        if ((index + 1 ) % 5 === 0 || index === codeLiguesPreferidos.length - 1) {
+        if ((index + 1) % 5 === 0 || index === codeLiguesPreferidos.length - 1) {
             let codeStringsWithOutRaya = codeStrings.substring(0, codeStrings.length - 1)
             linksFavoritos.push(codeStringsWithOutRaya)
             codeStrings = ''
@@ -300,14 +300,14 @@ const dibujar = function (partidos = PARTIDOS_OPTIMIZADOS) {
         const element = codeLiguesAll[index].codigoWplay;
         codeStrings += element
         codeStrings += '-'
-        if ((index + 1 ) % 5 === 0 || index === codeLiguesAll.length - 1) {
+        if ((index + 1) % 5 === 0 || index === codeLiguesAll.length - 1) {
             let codeStringsWithOutRaya = codeStrings.substring(0, codeStrings.length - 1)
             linksAll.push(codeStringsWithOutRaya)
             codeStrings = ''
         }
     }
 
-    html+= `
+    html += `
     <span>
         Gol en la primera Mitad - SI (GSH1) - CNAT-601779 - UNAT-556801
     </span>
@@ -316,109 +316,109 @@ const dibujar = function (partidos = PARTIDOS_OPTIMIZADOS) {
 
     for (const link of links) {
 
-        html+= `
+        html += `
     <div class="row">
       <div class="col">
-        <a href="https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=GSH1&sb_type_ids=${ link }" target="_blank" rel="noopener noreferrer">${ link }</a>
+        <a href="https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=GSH1&sb_type_ids=${link}" target="_blank" rel="noopener noreferrer">${link}</a>
       </div>
     </div>
         `
     }
     html += '<hr>'
-    html+= `
+    html += `
     <span>
         PRIMERA MITAD MAS DE 0.5 (OUH1)
     </span>
     <br>
     `
     for (const link of linksNoHalf) {
-        html+= `
+        html += `
     <div class="row">
       <div class="col">
-        <a href="https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=OUH1&sb_type_ids=${ link }" target="_blank" rel="noopener noreferrer">${ link }</a>
+        <a href="https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=OUH1&sb_type_ids=${link}" target="_blank" rel="noopener noreferrer">${link}</a>
       </div>
     </div>
         `
     }
 
     html += '<hr>'
-    html+= `
+    html += `
     <span>
         AMBOS ANOTAN (BTSC)
     </span>
     <br>
     `
     for (const link of linksAll) {
-        html+= `
+        html += `
     <div class="row">
       <div class="col">
-        <a href="https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=BTSC&sb_type_ids=${ link }" target="_blank" rel="noopener noreferrer">${ link }</a>
+        <a href="https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=BTSC&sb_type_ids=${link}" target="_blank" rel="noopener noreferrer">${link}</a>
       </div>
     </div>
         `
     }
 
     html += '<hr>'
-    html+= `
+    html += `
     <span>
         OVER (HCTG)
     </span>
     <br>
     `
     for (const link of linksAll) {
-        html+= `
+        html += `
     <div class="row">
       <div class="col">
-        <a href="https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=HCTG&sb_type_ids=${ link }" target="_blank" rel="noopener noreferrer">${ link }</a>
+        <a href="https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=HCTG&sb_type_ids=${link}" target="_blank" rel="noopener noreferrer">${link}</a>
       </div>
     </div>
         `
     }
 
-    
+
     html += '<hr>'
-    html+= `
+    html += `
     <span>
         LOCAL - VISITANTE (OU1H - OU1A)
     </span>
     <br>
     `
     for (const link of linksFavoritos) {
-        html+= `
+        html += `
     <div class="row">
       <div class="col">
-        <a href="https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=OU1H&sb_type_ids=${ link }" target="_blank" rel="noopener noreferrer">${ link } - LOCAL</a>
+        <a href="https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=OU1H&sb_type_ids=${link}" target="_blank" rel="noopener noreferrer">${link} - LOCAL</a>
         ------
-        <a href="https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=OU1A&sb_type_ids=${ link }" target="_blank" rel="noopener noreferrer">${ link } - VISITANTE</a>
+        <a href="https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=OU1A&sb_type_ids=${link}" target="_blank" rel="noopener noreferrer">${link} - VISITANTE</a>
       </div>
     </div>
         `
     }
 
     html += '<hr>'
-    html+= `
+    html += `
     <span>
         TODOS POR FECHA - COPIAR CUOTAS
     </span>
     <br>
     `
     for (const link of linksAll) {
-        html+= `
+        html += `
     <div class="row">
       <div class="col">
-        <a href="https://local.wplay.co/es/type-coupon?coupon_group_by=SBTYPE&mkt_sort=MRES&sb_type_ids=${ link }" target="_blank" rel="noopener noreferrer">${ link }</a>
+        <a href="https://local.wplay.co/es/type-coupon?coupon_group_by=SBTYPE&mkt_sort=MRES&sb_type_ids=${link}" target="_blank" rel="noopener noreferrer">${link}</a>
       </div>
     </div>
         `
     }
-    
+
 
     // https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=GSH1&sb_type_ids=19296-19160-48352-19159-19328
 
     console.log({ links })
 
     html += '<hr>'
-    html+= `
+    html += `
     <span>
         AMISTOSOS
     </span>
@@ -452,9 +452,9 @@ const dibujarSM = function (partidos = PARTIDOS_OPTIMIZADOS) {
     let acumulado = 1
     for (const partido of partidos) {
 
-        let mitad = partido.mitad ? `<a href="${ baseUrlSm + partido.codigoWplay + '?mkt_sort=GSH1' }" target="_blank" rel="noopener noreferrer">M</a>` : ''
-        let local = partido.mitadFavorito ? ` <a href="${ baseUrlSm + partido.codigoWplay + '?mkt_sort=OU1H' }" target="_blank" rel="noopener noreferrer">L</a>`: ''
-        let visitante = partido.mitadFavorito ? ` <a href="${ baseUrlSm + partido.codigoWplay + '?mkt_sort=OU1A' }" target="_blank" rel="noopener noreferrer">V</a>`: ''
+        let mitad = partido.mitad ? `<a href="${baseUrlSm + partido.codigoWplay + '?mkt_sort=GSH1'}" target="_blank" rel="noopener noreferrer">M</a>` : ''
+        let local = partido.mitadFavorito ? ` <a href="${baseUrlSm + partido.codigoWplay + '?mkt_sort=OU1H'}" target="_blank" rel="noopener noreferrer">L</a>` : ''
+        let visitante = partido.mitadFavorito ? ` <a href="${baseUrlSm + partido.codigoWplay + '?mkt_sort=OU1A'}" target="_blank" rel="noopener noreferrer">V</a>` : ''
 
         if (partido.cuotaCualquiera) {
             acumulado = Math.floor(acumulado * partido.cuotaCualquiera * 100) / 100
@@ -472,8 +472,8 @@ const dibujarSM = function (partidos = PARTIDOS_OPTIMIZADOS) {
             </div>
             <div class="col-4 border ${partido.cuotaFavorito && partido.cuotaFavorito > 1 && partido.cuotaFavorito < 1.74 ? 'bg-warning' : ''} ${partido.cuotaCualquiera && partido.cuotaCualquiera > 1 && partido.cuotaCualquiera < 1.26 && partido.cuotaFavorito && partido.cuotaFavorito > 1 && partido.cuotaFavorito < 1.74 ? 'bg-danger' : ''}">
                 <small>
-                    <a href="${ baseUrlSm + partido.codigoWplay }" target="_blank" rel="noopener noreferrer">${partido.liga}</a>
-                    ${ mitad } ${ local } ${ visitante }
+                    <a href="${baseUrlSm + partido.codigoWplay}" target="_blank" rel="noopener noreferrer">${partido.liga}</a>
+                    ${mitad} ${local} ${visitante}
                 </small>
                 <br>
                 <small class="${partido.favorito === 'local' ? 'bg-secondary text-white' : ''}">${partido.local.substring(0, 10)}</small>
