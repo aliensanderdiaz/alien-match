312b5d756a1c7c408c2bba30bc99269174641464
aliensanderdiaz <aliensanderdiaz@gmail.com>
2025-05-10 18:12:40 -0500

    m

--- ligas-faltantes-codes.js
+++ ligas-faltantes-codes.js
@@ -1,27 +1,6 @@
 let LIGAS_FALTANTES = [
-    'S-JAP1' ,
-    'S-COR1' ,
-    'R-INGO' ,
-    'YE-REP1' ,
-    'YE-RUS1' ,
-    'YE-BIE1' ,
-    'YE-TUR2' ,
-    'R-ESC1' ,
-    'YE-GRE1' ,
-    'R-ING1' ,
-    'YE-POR1' ,
-    'FRA2' ,
-    'YE-SUI1' ,
-    'ZA-ARG1' ,
-    'ZA-EST1' ,
-    'ZA-BRA1' ,
-    'ZA-BRA2' ,
-    'FRA1' ,
-    'R-INGT' ,
-    'ZA-COL2' ,
-    'ZF-COL' ,
-    'ZA-VEN1' ,
-    'ZA-MEX1' ,
+
+ 'ZA-ECUC' ,
 ]
 
 LIGAS_FALTANTES = [...new Set(LIGAS_FALTANTES)];
