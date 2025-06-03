14f65d1033313ece82c80374d440d72ebd586d41
aliensanderdiaz <aliensanderdiaz@gmail.com>
2025-06-02 19:59:22 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,3 +1,2 @@
 let APUESTAS_ABIERTAS = [
-	[[11630,"Bahia BA","Sao Paulo SP","mitad",1.533],[11630,"Delfin","Independiente Del Valle","mitad",1.42],[11630,"Atletico GO","Goias GO","mitad",1.571],[11530,"Plaza Colonia","Liverpool Montevideo","mitad",1.45],[11600,"Estudiantes Rio Cuarto","CA Central Norte Salta","mitad",1.571],"779,619 con $ 100,000"],
 ]
