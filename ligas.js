1771fc5cffe0167340539203d01cf2028470854f
aliensanderdiaz <aliensanderdiaz@gmail.com>
2025-07-02 17:11:54 -0500

    2m

--- ligas.js
+++ ligas.js
@@ -1,11 +1,9 @@
 let LIGAS = [
-	{"liga":"ZA-ARG2","mitad":false,"mitadFavorito":false,"codigoWplay":19342},
-	{"liga":"ZA-BOL1","mitad":false,"mitadFavorito":false,"codigoWplay":19786},
-	{"liga":"ZA-BRA2","mitad":true,"mitadFavorito":true,"codigoWplay":19393},
+	{"liga":"ZA-ARGC","mitad":false,"mitadFavorito":true,"codigoWplay":19572},
+	{"liga":"ZA-BRA20","mitad":false,"mitadFavorito":false,"codigoWplay":26866},
 	{"liga":"ZA-CHIC","mitad":false,"mitadFavorito":false,"codigoWplay":19799},
-	{"liga":"ZA-ECU1","mitad":true,"mitadFavorito":true,"codigoWplay":19373},
-	{"liga":"MUNC","mitad":true,"mitadFavorito":true,"codigoWplay":31374},
+	{"liga":"ZA-ECU2","mitad":false,"mitadFavorito":false,"codigoWplay":30530},
+	{"liga":"ZA-EST2","mitad":false,"mitadFavorito":true,"codigoWplay":20075},
 	{"liga":"AMIE","codigoWplay":617783,"mitad":false,"mitadFavorito":true},
 	{"liga":"AMIC","codigoWplay":20183,"mitad":false,"mitadFavorito":true},
-	{"liga":"ZA-URU1","mitad":false,"mitadFavorito":true,"codigoWplay":19398},
 ]
