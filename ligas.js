b601f8cb2b11a516c7a6a8378bd64a4ae661347f
aliensanderdiaz <aliensanderdiaz@gmail.com>
2025-07-03 12:38:29 -0500

    m

--- ligas.js
+++ ligas.js
@@ -1,9 +1,8 @@
 let LIGAS = [
 	{"liga":"ZA-ARGC","mitad":false,"mitadFavorito":true,"codigoWplay":19572},
-	{"liga":"ZA-BRA20","mitad":false,"mitadFavorito":false,"codigoWplay":26866},
-	{"liga":"ZA-CHIC","mitad":false,"mitadFavorito":false,"codigoWplay":19799},
+	{"liga":"YE-BIE1","mitad":false,"mitadFavorito":false,"codigoWplay":19493},
+	{"liga":"ZA-BRA2","mitad":true,"mitadFavorito":true,"codigoWplay":19393},
 	{"liga":"ZA-ECU2","mitad":false,"mitadFavorito":false,"codigoWplay":30530},
+	{"liga":"ZA-EST1","mitad":true,"mitadFavorito":true,"codigoWplay":19302},
 	{"liga":"ZA-EST2","mitad":false,"mitadFavorito":true,"codigoWplay":20075},
-	{"liga":"AMIE","codigoWplay":617783,"mitad":false,"mitadFavorito":true},
-	{"liga":"AMIC","codigoWplay":20183,"mitad":false,"mitadFavorito":true},
 ]
