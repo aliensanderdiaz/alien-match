a1a665dda5a2f628d45a505a10b48264f54bbc11
aliensanderdiaz <aliensanderdiaz@gmail.com>
2025-07-06 19:27:08 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,3 +1,2 @@
 let APUESTAS_ABIERTAS = [
-	[[11800,"Loudoun United FC","Orange County Blues","mitad",1.3],[11830,"New York City","Toronto FC","mitad",1.3],[11935,"Atletico GO","CRB Maceio AL","mitad",1.48],[11530,"Vinotinto FC","Club Leones Del Norte","mitad",1.6],[11745,"Instituto AC Cordoba","Huracan","mitad",1.769],[11900,"Vargas Torres","Imbabura SC","mitad",1.42],"373,649 con $ 37,164"],
 ]
