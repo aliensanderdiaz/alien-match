fbe56693442f7f98ac469ede15ab4fd307f60a29
aliensanderdiaz <aliensanderdiaz@gmail.com>
2025-08-08 06:47:03 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,2 +1,4 @@
 let APUESTAS_ABIERTAS = [
+	[[10635,"Dalian Yingbo","Chengdu Rongcheng","mitad",1.533,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=OUH1&sb_type_ids=19485",19485],[10730,"FC Poltava","Veres Rivne","mitad",1.615,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=OUH1&sb_type_ids=19383",19383],"247,692 con $ 100,000"],
+	[[12010,"Boyaca Chico FC","La Equidad","mitad",1.55],"387,500 con $ 100,000"],
 ]
