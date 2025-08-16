37bf34cc5245074a068409b33c4b1edf97549a8f
aliensanderdiaz <aliensanderdiaz@gmail.com>
2025-08-16 13:58:51 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,3 +1,4 @@
 let APUESTAS_ABIERTAS = [
-	[[11130,"Wolverhampton Wanderers","Manchester City","mitad",1.3],[11215,"Platense","San Lorenzo","mitad",1.85],[11230,"Malaga","SD Eibar","mitad",1.571],[11230,"RCD Mallorca","Barcelona","mitad",1.2],[11200,"CA San Telmo","Colon de Santa Fe","mitad",1.615],[11205,"Chacarita Juniors","Temperley","mitad",1.7],"1,245,507 con $ 100,007"],
+	[[11410,"Gimnasia Y Esgrima Mendoza","Almirante Brown","mitad",1.533],[11415,"Cremonese","Palermo","mitad",1.4],[11400,"Groningen","Heerenveen","mitad",1.4],[11405,"Nice","Toulouse","mitad",1.4],[11400,"Colo Colo","Universidad Catolica","mitad",1.533],"645,190 con $ 100,007"],
+	[[11400,"Ceara CE","Bragantino SP","mitad",1.55],[11400,"Fluminense RJ","Fortaleza CE","mitad",1.42],[11400,"Remo PA","Botafogo SP","mitad",1.5],[11415,"Huracan","Argentinos Juniors","mitad",1.727],[11430,"CD Alaves","Levante","mitad",1.533],"874,397 con $ 100,000"],
 ]
