86555bfe422a4724d4e5907f0baf45e496d88f08
aliensanderdiaz <aliensanderdiaz@gmail.com>
2025-08-15 19:51:01 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,2 +1,4 @@
 let APUESTAS_ABIERTAS = [
+	[[11900,"Racing Club","CA Tigre","mitad",1.533],[11900,"Union La Calera","Deportes La Serena","mitad",1.48],[11910,"Ferro Carril Oeste","Club Atletico Colegiales","mitad",1.666],[11930,"Academia Anzoategui","Estudiantes de Merida","mitad",1.45],[12000,"Gimnasia y Tiro de Salta","CD Maipu","mitad",1.7],[12000,"Sport Boys Association","Deportivo Garcilaso","mitad",1.48],"1,379,927 con $ 100,007"],
+	[[11830,"North Carolina","Detroit City","mitad",1.42],"561,255 con $ 100,000"],
 ]
