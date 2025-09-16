7c89d45b62311026ebb0cf6256a4fe820b5783c3
aliensanderdiaz <aliensanderdiaz@gmail.com>
2025-09-16 06:44:49 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,3 +1,3 @@
 let APUESTAS_ABIERTAS = [
-	[[10500,"FC Tokyo","Tokyo Verdy","mitad",1.5,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=OUH1&sb_type_ids=19378",19378],[11130,"Verona","Cremonese","mitad",1.533,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=OUH1&sb_type_ids=19159",19159],[11100,"Dinamo Brest","Neman Grodno","mitad",1.42,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=OUH1&sb_type_ids=19493",19493],"326,600 con $ 100,000"],
+	[[11145,"Athletic Bilbao","Arsenal","mitad",1.42,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=OUH1&sb_type_ids=19161",19161],[11200,"Boulogne","SC Bastia","mitad",1.55,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=OUH1&sb_type_ids=19405",19405],"376,303 con $ 170,969"],
 ]
