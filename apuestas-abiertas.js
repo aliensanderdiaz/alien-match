12106228de051f836f1f4243983a79389ce66ea5
aliensanderdiaz <aliensanderdiaz@gmail.com>
2025-09-18 19:48:45 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,3 +1,2 @@
 let APUESTAS_ABIERTAS = [
-	[[11730,"Botafogo RJ","Mirassol SP","mitad",1.42],[11930,"River Plate","Palmeiras SP","mitad",1.769],[11700,"Bolivar","Atletico MG","mitad",1.55],[11900,"Vargas Torres","Imbabura SC","mitad",1.45],[11930,"Independiente Del Valle","Once Caldas","mitad",1.4],"466,401 con $ 59,001"],
 ]
