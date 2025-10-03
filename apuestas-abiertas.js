32f5eca767e243fcf01f0f6127d3405ed71efd26
aliensanderdiaz <aliensanderdiaz@gmail.com>
2025-10-03 18:02:02 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,2 +1,3 @@
 let APUESTAS_ABIERTAS = [
+	[[11915,"Argentinos Juniors","Central Cordoba","mitad",1.45],[11915,"Union Santa Fe","Aldosivi","mitad",1.48],[11930,"Deportivo Cali","Deportivo Pereira","mitad",1.533],[11930,"Coritiba PR","Botafogo SP","mitad",1.45],[11935,"CRB Maceio AL","Avai SC","mitad",1.45],[11745,"Rentistas","Atletico Fenix Montevideo","mitad",1.571],"1,087,169 con $ 100,000"],
 ]
--- ligas-faltantes-codes.js
+++ ligas-faltantes-codes.js
@@ -1,5 +1,21 @@
 let LIGAS_FALTANTES = [
- 'ZA-ECU2' ,
+ 'S-CHI1' ,
+ 'R-ESC1' ,
+ 'R-ESC2' ,
+ 'ZA-ARG2' ,
+ 'ZA-BRA1' ,
+ 'YE-RUS1' ,
+ 'R-INGO' ,
+ 'R-INGT' ,
+ 'R-INGN' ,
+ 'ITA2' ,
+ 'YE-REP1' ,
+ 'ESPC' ,
+ 'YE-GRE1' ,
+ 'YE-SUI1' ,
+ 'ZA-EST1' ,
+ 'ZA-EST2' ,
+ 'ZA-VEN1' ,
 ]
 
 LIGAS_FALTANTES = [...new Set(LIGAS_FALTANTES)];
