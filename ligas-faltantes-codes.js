729df53febf9b6d30ac10631f9abdaaf027a0b47
aliensanderdiaz <aliensanderdiaz@gmail.com>
2025-12-13 09:42:44 -0500

    m

--- ligas-faltantes-codes.js
+++ ligas-faltantes-codes.js
@@ -1,5 +1,18 @@
 let LIGAS_FALTANTES = [
-
+ 'R-ESC1' ,
+ 'YE-GRE1' ,
+ 'R-ING1' ,
+ 'R-INGO' ,
+ 'R-INGT' ,
+ 'YE-HOL1' ,
+ 'YE-POR1' ,
+ 'YE-SUI1' ,
+ 'ZA-ECU1' ,
+ 'ZA-BOL1' ,
+ 'ZA-COLC' ,
+ 'ZA-ARG1' ,
+ 'INTER' ,
+ 'YE-REP1' ,
 
 ]
 
