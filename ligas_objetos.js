767fbd65d7ced91850301cd3345c65dacc29f7c5
aliensanderdiaz <aliensanderdiaz@gmail.com>
2025-12-30 12:35:33 -0500

    m

--- ligas_objetos.js
+++ ligas_objetos.js
@@ -26,6 +26,14 @@ let LIGAS_OBJETOS = [
       mitad: true,
       favorito: true
   },
+  {
+      nombreFlashcore: 'ÁFRICA - Copa de África de Naciones',
+      "nombreWplay": 'Copa Africana de Naciones - Partidos',
+      "abreviado": 'CAFR',
+      "codigoWplay": 557799,
+      mitad: true,
+      favorito: true
+  },
     {
     nombreFlashcore: "ITALIA - Supercopa",
     nombreWplay: "SUPER COPA",
