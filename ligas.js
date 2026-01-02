350136d400143d9b441a4acbeb7adb50c0afa11b
aliensanderdiaz <aliensanderdiaz@gmail.com>
2026-01-01 22:38:28 -0500

    m

--- ligas.js
+++ ligas.js
@@ -1,12 +1,8 @@
 let LIGAS = [
-	{"liga":"R-ING1","mitad":true,"mitadFavorito":true,"codigoWplay":19157},
-	{"liga":"R-ING2","mitad":true,"mitadFavorito":true,"codigoWplay":19156},
-	{"liga":"R-INGO","mitad":true,"mitadFavorito":true,"codigoWplay":19326},
-	{"liga":"R-INGT","mitad":true,"mitadFavorito":true,"codigoWplay":19300},
-	{"liga":"I-AMI","mitad":false,"mitadFavorito":true,"codigoWplay":19287},
-	{"liga":"I-AMI23","mitad":false,"mitadFavorito":false,"codigoWplay":20182},
-	{"liga":"I-AMI21","mitad":false,"mitadFavorito":false,"codigoWplay":19345},
-	{"liga":"I-OLIH","codigoWplay":45631,"mitad":false,"mitadFavorito":true},
-	{"liga":"I-CUP","codigoWplay":51339,"mitad":false,"mitadFavorito":true},
-	{"liga":"CS20","codigoWplay":32567,"mitad":false,"mitadFavorito":true},
+	{"liga":"ZA-BRAC20","mitad":false,"mitadFavorito":false,"codigoWplay":1187789},
+	{"liga":"ESP1","mitad":true,"mitadFavorito":true,"codigoWplay":19160},
+	{"liga":"ESP2","mitad":true,"mitadFavorito":true,"codigoWplay":48352},
+	{"liga":"FRA1","mitad":true,"mitadFavorito":true,"codigoWplay":19327},
+	{"liga":"ITA1","mitad":true,"mitadFavorito":true,"codigoWplay":19159},
+	{"liga":"YE-POR1","mitad":true,"mitadFavorito":true,"codigoWplay":19211},
 ]
