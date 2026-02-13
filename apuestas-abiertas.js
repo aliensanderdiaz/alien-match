c3f2e900cb645d3665a56446974b51da34e1dc12
aliensanderdiaz <aliensanderdiaz@gmail.com>
2026-02-13 16:43:45 -0500

    m

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,13 +1,123 @@
 let APUESTAS_ABIERTAS = [
-	["229,394,039",[11230,"Fortuna Dusseldorf","SC Preussen Münster","local",1.8,"",19344],[11300,"FC Viktoria Köln","FC Erzgebirge Aue","local",1.9,"",19366],[11500,"Madureira RJ","Boavista RJ","local",2.4,"",19456],[11400,"Clermont Foot","Rodez Aveyron","local",2,"",19405],[11400,"Red Star Paris","Nancy","local",1.85,"",19405],[11400,"FC Oss","Jong AZ Alkmaar","local",1.8,"",19375],[11400,"RKC Waalwijk","Roda JC","local",1.8,"",19375],[11200,"Radomiak Radom","Korona Kielce","local",1.833,"",19336],[11545,"CD Tondela","Alverca","local",2.25,"",19211],[11600,"CD Everton Vina Del Mar","Huachipato","local",2,"",19303],[11500,"Elche","Osasuna","visitante",2.05,"",19160],[11400,"SC Amiens","Dunkerque","visitante",1.833,"",19405],[11400,"Laval","Annecy FC","visitante",2.375,"",19405],[11445,"Wrexham FC","Ipswich Town","visitante",1.85,"",19207],[11445,"Mechelen","Genk","visitante",1.85,"",19372],[11430,"GKS Katowice","Legia Warsaw","visitante",1.85,"",19336],[11300,"Petrolul Ploiesti","FC Arges Pitesti","visitante",2.5,"",46019],[11200,"Antalyaspor","Samsunspor","visitante",2,"",19363]],
-	["9,373,012",[11200,"Antalyaspor","Samsunspor","over",2.25,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=HCTG&sb_type_ids=19363",19363],[11430,"GKS Katowice","Legia Warsaw","over",2,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=HCTG&sb_type_ids=19336",19336],[11500,"Elche","Osasuna","over",2.1,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=HCTG&sb_type_ids=19160",19160],[11500,"Madureira RJ","Boavista RJ","over",2.5,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=HCTG&sb_type_ids=19456",19456],[11300,"Petrolul Ploiesti","FC Arges Pitesti","over",3,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=HCTG&sb_type_ids=46019",46019],[11330,"Santa Clara","Benfica","over",2.1,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=HCTG&sb_type_ids=19211",19211],[11545,"CD Tondela","Alverca","over",2.55,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=HCTG&sb_type_ids=19211",19211],[11600,"CD Everton Vina Del Mar","Huachipato","over",2.1,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=HCTG&sb_type_ids=19303",19303],[11400,"Laval","Annecy FC","over",2.8,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=HCTG&sb_type_ids=19405",19405],[11400,"Red Star Paris","Nancy","over",2.1,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=HCTG&sb_type_ids=19405",19405],[11400,"SC Amiens","Dunkerque","over",2,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=HCTG&sb_type_ids=19405",19405]],
-	["13,474",[11300,"Petrolul Ploiesti","FC Arges Pitesti","mitad",1.7,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=OUH1&sb_type_ids=46019",46019],[11500,"Madureira RJ","Boavista RJ","mitad",1.571,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=OUH1&sb_type_ids=19456",19456],[11400,"Laval","Annecy FC","mitad",1.6,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=OUH1&sb_type_ids=19405",19405],[11400,"Red Star Paris","Nancy","mitad",1.42,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=OUH1&sb_type_ids=19405",19405],[11545,"CD Tondela","Alverca","mitad",1.5,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=OUH1&sb_type_ids=19211",19211]],
-	["42,000",[11500,"Elche","Osasuna","over",2,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=HCTG&sb_type_ids=19160",19160],[11445,"Airdrieonians","St Johnstone","over",2,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=HCTG&sb_type_ids=19879",19879],[11500,"Madureira RJ","Boavista RJ","over",2.5,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=HCTG&sb_type_ids=19456",19456],[11600,"CD Everton Vina Del Mar","Huachipato","over",2.1,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=HCTG&sb_type_ids=19303",19303],[11600,"Estudiantes de Merida","Rayo Zuliano","over",2,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=HCTG&sb_type_ids=20080",20080]],
-	["29,869",[11545,"CD Tondela","Alverca","over",2.35,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=HCTG&sb_type_ids=19211",19211],[11430,"GKS Katowice","Legia Warsaw","over",2,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=HCTG&sb_type_ids=19336",19336],[11300,"Petrolul Ploiesti","FC Arges Pitesti","over",3.1,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=HCTG&sb_type_ids=46019",46019],[11445,"Wrexham FC","Ipswich Town","over",2.05,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=HCTG&sb_type_ids=19207",19207]],
-	["22,932",[11330,"Santa Clara","Benfica","over",2.1,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=HCTG&sb_type_ids=19211",19211],[11400,"Laval","Annecy FC","over",2.6,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=HCTG&sb_type_ids=19405",19405],[11400,"Red Star Paris","Nancy","over",2.1,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=HCTG&sb_type_ids=19405",19405],[11400,"SC Amiens","Dunkerque","over",2,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=HCTG&sb_type_ids=19405",19405]],
-	["10,137",[11330,"Santa Clara","Benfica","ambos",2.15,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=BTSC&sb_type_ids=19211",19211],[11400,"Laval","Annecy FC","ambos",2.05,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=BTSC&sb_type_ids=19405",19405],[11300,"Petrolul Ploiesti","FC Arges Pitesti","ambos",2.3,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=BTSC&sb_type_ids=46019",46019]],
-	["5,485",[11300,"Petrolul Ploiesti","FC Arges Pitesti","mitad",1.7,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=OUH1&sb_type_ids=46019",46019],[11500,"Madureira RJ","Boavista RJ","mitad",1.6,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=OUH1&sb_type_ids=19456",19456],[11600,"CD Everton Vina Del Mar","Huachipato","mitad",1.42,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=OUH1&sb_type_ids=19303",19303],[11600,"Estudiantes de Merida","Rayo Zuliano","mitad",1.42,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=OUH1&sb_type_ids=20080",20080]],
-	["4,626",[11330,"Santa Clara","Benfica","mitad",1.42,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=OUH1&sb_type_ids=19211",19211],[11400,"Laval","Annecy FC","mitad",1.55,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=OUH1&sb_type_ids=19405",19405],[11400,"Red Star Paris","Nancy","mitad",1.42,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=OUH1&sb_type_ids=19405",19405],[11545,"CD Tondela","Alverca","mitad",1.48,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=OUH1&sb_type_ids=19211",19211]],
-	["3,900",[11200,"Radomiak Radom","Korona Kielce","local",1.95,"",19336],[11200,"Antalyaspor","Samsunspor","visitante",2,"",19363]],
-	["4,725",[11200,"Antalyaspor","Samsunspor","over",2.25,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=HCTG&sb_type_ids=19363",19363],[11200,"Radomiak Radom","Korona Kielce","over",2.1,"https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=HCTG&sb_type_ids=19336",19336]],
+	["2,750",
+		[11930, "Godoy Cruz", "Ciudad de Bolivar", "mitad", 1.666, "https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=OUH1&sb_type_ids=19342", 19342],
+
+		[12000, "Gimnasia y Tiro de Salta", "Club Atletico Colegiales", "mitad", 1.65, "https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=OUH1&sb_type_ids=19342", 19342],
+
+		[11900, "Joinville SC", "Carlos Renaux SC", "mitad", 1.48, "https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=OUH1&sb_type_ids=19788", "19788"],
+
+		[11930, "Liverpool Montevideo", "Defensor Sporting", "mitad", 1.42, "https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=OUH1&sb_type_ids=19398", 19398],
+
+		[11830, "Palestino", "Universidad de Chile", "mitad", 1.42, "https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=OUH1&sb_type_ids=19303", 19303],
+
+		[11900, "Atletico Rafaela", "Club Almagro", "mitad", 1.8, "https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=OUH1&sb_type_ids=19342", 19342],
+
+		[11700, "Cerro Largo FC", "Montevideo Wanderers", "mitad", 1.5, "https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=OUH1&sb_type_ids=19398", 19398],
+
+		[11700, "Carabobo FC", "Monagas SC", "mitad", 1.42, "https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=OUH1&sb_type_ids=20080", 20080],
+
+		[11830, "Deportivo Recoleta", "Sportivo Luqueno", "mitad", 1.48, "https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=OUH1&sb_type_ids=19359", 19359],
+
+		[11830, "UCV FC", "Metropolitano SC", "mitad", 1.42, "https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=OUH1&sb_type_ids=20080", 20080],
+
+		[11800, "CA Independiente", "Atletico Lanus", "mitad", 1.571, "https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=OUH1&sb_type_ids=19296", 19296],
+
+		[12000, "América de Cali", "Independiente Santa Fe", "mitad", 1.55, "https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=OUH1&sb_type_ids=19311", 19311],
+
+		[12015, "Defensa y Justicia", "Velez Sarsfield", "mitad", 1.6, "https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=OUH1&sb_type_ids=19296", 19296],
+
+		[12015, "Union Santa Fe", "San Lorenzo", "mitad", 1.727, "https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=OUH1&sb_type_ids=19296", 19296],
+
+	],
+	["5,372",
+
+		[11700, "Cerro Largo FC", "Montevideo Wanderers", "local", 2.15, "", 19398],
+
+		[12000, "Alebrijes de Oaxaca", "Coyotes FC", "local", 2, "", 61826],
+
+		[11800, "CD Tapatio", "Jaiba Brava", "local", 2, "", 61826],
+
+		[11630, "Guarani Asuncion", "Sportivo San Lorenzo", "local", 1.75, "", 19359],
+
+		[11930, "Liverpool Montevideo", "Defensor Sporting", "local", 2, "", 19398],
+
+		[12015, "Union Santa Fe", "San Lorenzo", "local", 2.35, "", 19296],
+
+		[11800, "CA Independiente", "Atletico Lanus", "local", 2.1, "", 19296],
+
+		[12000, "América de Cali", "Independiente Santa Fe", "local", 2.05, "", 19311],
+
+		[12000, "Puebla", "Pumas UNAM", "local", 2.1, "", 19329],
+
+
+	],
+	["4,476",
+
+		[11830, "Palestino", "Universidad de Chile", "visitante", 2, "", 19303],
+
+		[11830, "Deportivo Recoleta", "Sportivo Luqueno", "visitante", 2.25, "", 19359],
+
+		[12015, "Defensa y Justicia", "Velez Sarsfield", "visitante", 2.25, "", 19296],
+
+
+	],
+	["6,731",
+
+		[11900, "Atletico Rafaela", "Club Almagro", "over", 3.35, "https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=HCTG&sb_type_ids=19342", 19342],
+
+		[11930, "Godoy Cruz", "Ciudad de Bolivar", "over", 2.85, "https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=HCTG&sb_type_ids=19342", 19342],
+
+		[12000, "Gimnasia y Tiro de Salta", "Club Atletico Colegiales", "over", 3.1, "https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=HCTG&sb_type_ids=19342", 19342],
+
+		[11700, "Carabobo FC", "Monagas SC", "over", 2.05, "https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=HCTG&sb_type_ids=20080", 20080],
+
+		[11830, "UCV FC", "Metropolitano SC", "over", 2.05, "https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=HCTG&sb_type_ids=20080", 20080],
+
+		[11900, "Joinville SC", "Carlos Renaux SC", "over", 2.2, "https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=HCTG&sb_type_ids=19788", "19788"],
+
+		[11930, "Liverpool Montevideo", "Defensor Sporting", "over", 2.15, "https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=HCTG&sb_type_ids=19398", 19398],
+
+		[12000, "América de Cali", "Independiente Santa Fe", "over", 2.3, "https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=HCTG&sb_type_ids=19311", 19311],
+
+		[12015, "Defensa y Justicia", "Velez Sarsfield", "over", 2.75, "https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=HCTG&sb_type_ids=19296", 19296],
+
+		[12015, "Union Santa Fe", "San Lorenzo", "over", 3.4, "https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=HCTG&sb_type_ids=19296", 19296],
+
+		[11700, "Cerro Largo FC", "Montevideo Wanderers", "over", 2.45, "https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=HCTG&sb_type_ids=19398", 19398],
+
+		[11800, "CA Independiente", "Atletico Lanus", "over", 2.7, "https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=HCTG&sb_type_ids=19296", 19296],
+
+		[11800, "CD Tapatio", "Jaiba Brava", "over", 2.1, "https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=HCTG&sb_type_ids=61826", 61826],
+
+		[11830, "Deportivo Recoleta", "Sportivo Luqueno", "over", 2.35, "https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=HCTG&sb_type_ids=19359", 19359],
+
+		[12000, "Puebla", "Pumas UNAM", "over", 2.1, "https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=HCTG&sb_type_ids=19329", 19329],
+
+
+	],
+	["21,769",
+		[11930, "Godoy Cruz", "Ciudad de Bolivar", "ambos", 2.5, "https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=BTSC&sb_type_ids=19342", 19342],
+
+		[12000, "Gimnasia y Tiro de Salta", "Club Atletico Colegiales", "ambos", 2.6, "https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=BTSC&sb_type_ids=19342", 19342],
+
+		[11700, "America de Cali (F)", "Junior de Barranquilla (F)", "ambos", 2, "https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=BTSC&sb_type_ids=48455", 48455],
+
+		[11730, "Treze PB", "Esporte de Patos", "ambos", 2.3, "https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=BTSC&sb_type_ids=28509", "28509"],
+
+		[11730, "Real Cartagena", "Atletico de Cali", "ambos", 2, "https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=BTSC&sb_type_ids=29812", 29812],
+
+		[11900, "Atletico Rafaela", "Club Almagro", "ambos", 2.5, "https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=BTSC&sb_type_ids=19342", 19342],
+
+		[11800, "CA Independiente", "Atletico Lanus", "ambos", 2.2, "https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=BTSC&sb_type_ids=19296", 19296],
+
+		[12000, "América de Cali", "Independiente Santa Fe", "ambos", 2, "https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=BTSC&sb_type_ids=19311", 19311],
+
+		[12015, "Defensa y Justicia", "Velez Sarsfield", "ambos", 2.15, "https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=BTSC&sb_type_ids=19296", 19296],
+
+		[12015, "Union Santa Fe", "San Lorenzo", "ambos", 2.55, "https://local.wplay.co/es/type-coupon?coupon_group_by=TIME&mkt_sort=BTSC&sb_type_ids=19296", 19296],
+
+
+
+	],
+
 ]
