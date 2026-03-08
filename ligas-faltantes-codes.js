a38c452897cf6754ffe4ce466508c2c08fd37035
aliensanderdiaz <aliensanderdiaz@gmail.com>
2026-03-07 22:36:11 -0500

    m

--- ligas-faltantes-codes.js
+++ ligas-faltantes-codes.js
@@ -1,10 +1,8 @@
 let LIGAS_FALTANTES = [
-    'BALA',    'BCAP',
-    'BCAR',    'BMIN',
-    'BPAU3',   'BPIA',
-    'BROR',    'BSMAT',
-    'BTOC',    'YE-RUS1',
-    'ZA-URU2'
+    'BALAC',   'BCAP',
+    'BPAU3',   'BRON',
+    'BSMAT',   'YE-RUS1',
+    'ZA-EST2'
 ]
 
 LIGAS_FALTANTES = [...new Set(LIGAS_FALTANTES)];
