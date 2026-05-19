07649acc0a9e0137ddc053b9c649b710bd3ddd92
aliensanderdiaz <aliensanderdiaz@gmail.com>
2026-05-19 18:21:25 -0500

    m

--- ligas.js
+++ ligas.js
@@ -1,12 +1,5 @@
 let LIGAS = [
-	{"liga":"ZA-ARG2","mitad":false,"mitadFavorito":false,"codigoWplay":19342},
-	{"liga":"ZA-BRA2","mitad":true,"mitadFavorito":true,"codigoWplay":2434787},
-	{"liga":"ZA-BRA20","mitad":false,"mitadFavorito":false,"codigoWplay":2434800},
-	{"liga":"ZA-COL2","mitad":false,"mitadFavorito":false,"codigoWplay":29812},
-	{"liga":"ZF-COL","mitad":false,"mitadFavorito":false,"codigoWplay":48455},
-	{"liga":"ZA-ECU1","mitad":true,"mitadFavorito":true,"codigoWplay":19373},
-	{"liga":"ESP2","mitad":true,"mitadFavorito":true,"codigoWplay":48352},
-	{"liga":"R-ING1","mitad":true,"mitadFavorito":true,"codigoWplay":19157},
-	{"liga":"YE-SUI1","mitad":true,"mitadFavorito":true,"codigoWplay":19305},
-	{"liga":"ZA-URU1","mitad":false,"mitadFavorito":true,"codigoWplay":19398},
+	{"liga":"ZA-ESTC","mitad":false,"mitadFavorito":true,"codigoWplay":19497},
+	{"liga":"ZA-LIBE","mitad":true,"mitadFavorito":true,"codigoWplay":19462},
+	{"liga":"ZA-SUDA","mitad":false,"mitadFavorito":true,"codigoWplay":19348},
 ]
