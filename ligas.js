d35eb436a2131f62956169148b850d5bd320cebd
aliensanderdiaz <aliensanderdiaz@gmail.com>
2026-07-06 20:35:44 -0500

    m

--- ligas.js
+++ ligas.js
@@ -1,9 +1,16 @@
 let LIGAS = [
+	{"liga":"ZA-BOL1","mitad":false,"mitadFavorito":false,"codigoWplay":19786},
 	{"liga":"ZA-BRA2","mitad":true,"mitadFavorito":true,"codigoWplay":2434787},
-	{"liga":"ZA-CHIC","mitad":false,"mitadFavorito":false,"codigoWplay":19799},
 	{"liga":"ZA-ECU1","mitad":true,"mitadFavorito":true,"codigoWplay":19373},
+	{"liga":"U-CHAMC","mitad":false,"mitadFavorito":true,"codigoWplay":61789},
+	{"liga":"U-CONFC","mitad":false,"mitadFavorito":true,"codigoWplay":1431783},
 	{"liga":"MUN","mitad":true,"mitadFavorito":true,"codigoWplay":296772},
+	{"liga":"I-AMI","mitad":false,"mitadFavorito":true,"codigoWplay":19287},
+	{"liga":"I-AMI23","mitad":false,"mitadFavorito":false,"codigoWplay":20182},
+	{"liga":"I-AMI21","mitad":false,"mitadFavorito":false,"codigoWplay":19345},
+	{"liga":"I-OLIH","codigoWplay":45631,"mitad":false,"mitadFavorito":true},
+	{"liga":"I-CUP","codigoWplay":51339,"mitad":false,"mitadFavorito":true},
+	{"liga":"CS20","codigoWplay":32567,"mitad":false,"mitadFavorito":true},
 	{"liga":"AMIE","codigoWplay":617783,"mitad":false,"mitadFavorito":true},
 	{"liga":"AMIC","codigoWplay":20183,"mitad":false,"mitadFavorito":true},
-	{"liga":"ZA-URU2","mitad":false,"mitadFavorito":false,"codigoWplay":26834},
 ]
